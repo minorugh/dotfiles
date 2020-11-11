d0446a5089809b6fc2e2a3aafc7aa5f616e7647d
Minoru Yamada <minorugh@users.noreply.github.com>
2020-11-11 20:56:19 +0900

    Fix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,11 +17,12 @@
 (defvar default-gc-cons-threshold gc-cons-threshold)
 (setq file-name-handler-alist nil)
 (setq gc-cons-threshold (* 1024 1024 100))
-(add-hook 'emacs-startup-hook
-		  (lambda ()
-			"Restore defalut values after startup."
-			(setq file-name-handler-alist default-file-name-handler-alist)
-			(setq gc-cons-threshold default-gc-cons-threshold)))
+(add-hook
+ 'emacs-startup-hook
+ (lambda ()
+   "Restore defalut values after startup."
+   (setq file-name-handler-alist default-file-name-handler-alist)
+   (setq gc-cons-threshold default-gc-cons-threshold)))
 
 
 (customize-set-variable
@@ -51,9 +52,10 @@
   :ensure t
   :config
   (custom-set-variables '(init-loader-show-log-after-init 'error-only))
-  (add-hook 'after-init-hook
-			(lambda ()
-			  (init-loader-load))))
+  (add-hook
+   'after-init-hook
+   (lambda ()
+	 (init-loader-load))))
 
 
 (provide 'init)
