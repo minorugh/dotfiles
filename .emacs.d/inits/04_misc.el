418e6ee24129e9935e896bb27b8f7075372ca89f
Minoru Yamada <minorugh@users.noreply.github.com>
2020-11-21 20:29:17 +0900

    Refactor

--- .emacs.d/inits/04_misc.el
+++ .emacs.d/inits/04_misc.el
@@ -4,18 +4,15 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(leaf popwin
-  :ensure t
+(leaf popwin :ensure t
   :global-minor-mode t)
 
 
-(leaf iedit
-  :ensure t
+(leaf iedit :ensure t
   :bind ("C-;" . iedit-mode))
 
 
-(leaf expand-region
-  :ensure t
+(leaf expand-region :ensure t
   :bind ("C-@" . er/expand-region))
 
 
@@ -27,51 +24,43 @@
   	:hook (flymake-mode-hook . flymake-diagnostic-at-point-mode)))
 
 
-(leaf key-chord
-  :ensure t
+(leaf key-chord :ensure t
   :global-minor-mode t
   :config
   (key-chord-define-global "df" 'counsel-descbinds)
   (key-chord-define-global "l;" 'init-loader-show-log))
 
 
-(leaf prescient
-  :ensure t
-  :global-minor-mode prescient-persist-mode
-  :init
+(leaf prescient :ensure t
+  :config
   (leaf company-prescient :ensure t
-    :after prescient company
-    :global-minor-mode t)
+	:after company
+	:global-minor-mode t)
   (leaf ivy-prescient :ensure t
-    :after prescient ivy
-    :global-minor-mode t))
+	:after ivy
+	:global-minor-mode t))
 
 
-(leaf quickrun
-  :ensure t
+(leaf quickrun :ensure t
   :bind ("<f5>" . quickrun))
 
 
-(leaf which-key
-  :ensure t
+(leaf which-key :ensure t
   :global-minor-mode t
   :config
   (setq which-key-max-description-length 40)
   (setq which-key-use-C-h-commands t))
 
 
-(leaf projectile
-  :ensure t
-  :global-minor-mode t
-  :init
+(leaf projectile :ensure t
+  :config
   (setq projectile-known-projects-file "~/Dropbox/emacs/projectile-bookmarks.eld")
   (leaf counsel-projectile
-    :ensure t
+	:ensure t
 	:global-minor-mode t))
 
 
-(leaf yasnippet
-  :ensure t
+(leaf yasnippet :ensure t
   :commands yas-global-mode
   :config
   (yas-global-mode)
@@ -80,13 +69,11 @@
   (leaf ivy-yasnippet :ensure t))
 
 
-(leaf restart-emacs
-  :ensure t
+(leaf restart-emacs :ensure t
   :bind ("C-x C-c" . restart-emacs))
 
 
-(leaf web-mode
-  :ensure t
+(leaf web-mode :ensure t
   :mode "\\.js?\\'" "\\.html?\\'" "\\.php?\\'")
 
 
