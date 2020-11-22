5846b9344d3d1202d291c9846eb3a658ec7bdbf2
Minoru Yamada <minorugh@users.noreply.github.com>
2020-11-22 22:58:53 +0900

    Rename

--- .emacs.d/inits/10_hydra-pinky.el
+++ .emacs.d/inits/10_hydra-pinky.el
@@ -9,7 +9,7 @@
  (defhydra hydra-pinky
    (:color red :hint nil)
    "
-  🐳 page:_SPC_:_b_:_;_  goto:_a_:_e_._j_._l_._w_  window:_o_:_0_:___  _d_iff:_n_:_p_  zoom:_-__/__+_  buffer:_[__:__]_  _s_wiper  _v_iew  exit:_,_"
+  🐳 page:_SPC_:_b_:_l_  goto:_j_._w_  window:_0_:_1_:_2_:_3_:_o_:_x_  _d_iff:_n_:_p_  zoom:_-__.__+_  buffer:_[__:__]_  _s_wiper"
    ;; move page
    ("SPC" scroll-up-command)
    ("f" scroll-up-command)
@@ -31,15 +31,16 @@
    ("<right>" forward-char)
    ("<left>" backward-char)
    ;; misc
-   ("i" View-exit :exit t)
-   ("," View-exit :exit t)
-   (";" recenter-top-bottom)
+   ("l" recenter-top-bottom)
    ;; window
    ("+" text-scale-increase)
    ("-" text-scale-decrease)
-   ("/" (text-scale-set 0))
+   ("." (text-scale-set 0))
    ("0" delete-window)
-   ("_" delete-other-windows)
+   ("1" delete-other-windows)
+   ("2" split-window-below)
+   ("3" split-window-right)
+   ("x" window-swap-states)
    ("d" vc-diff)
    ("n" diff-hl-next-hunk)
    ("p" diff-hl-previous-hunk)
@@ -48,17 +49,15 @@
    ("[" previous-buffer)
    ("]" next-buffer)
    ;; avy
-   ("l" avy-goto-line)
    ("w" avy-goto-word-1)
    ;; Others
    ("o" other-window-or-split)
    ("t" direx:jump-to-project-directory)
-   ("s" swiper)
-   ("v" view-mode)
-   ("." nil :color blue)))
+   ("s" swiper)))
 
 
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
-;;; 10_pinky.el ends here
+
+;;; 10_hydra-pinky.el ends here
