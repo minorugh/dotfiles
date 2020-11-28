5ee432a52c287de0582af2b6e9f09a10322c38bd
Minoru Yamada <minorugh@users.noreply.github.com>
2020-11-28 22:29:55 +0900

    Fix comment

--- .emacs.d/inits/99_my:template.el
+++ .emacs.d/inits/99_my:template.el
@@ -25,17 +25,20 @@
   (forward-line -5)
   (forward-char 18))
 
+
 (defun my:teirei-new-post ()
   "Open teirei file and insert template."
   (interactive)
   (find-file "~/Dropbox/GH/teirei/tex/teirei.txt")
   (my:minoru_sen))
 
+
 (defun my:swan-new-post ()
   "Open swan file and insert template."
   (interactive)
   (find-file "~/Dropbox/GH/swan/tex/swan.txt")
   (my:minoru_sen))
+
 
 (defun my:otibo-new-post ()
   "Open otibo file and insert template."
@@ -55,6 +58,7 @@
   (forward-line -1)
   (forward-char 15))
 
+
 (defun my:minoru_sen ()
   "Insert template."
   (interactive)
@@ -64,6 +68,7 @@
    (format-time-string "%Y年%-m月%-d日（参加者 名）\n"))
   (forward-line -1)
   (forward-char 15))
+
 
 (defun my:kinnei-new-post ()
   "Open kinnei file and insert template."
@@ -98,6 +103,7 @@
   (forward-line 4)
   (forward-char 18))
 
+
 (defun my:ap-new-post ()
   "Open ap file and insert template."
   (interactive)
@@ -111,6 +117,7 @@
    "-(\n<small>()</small><br>\n<-mi>\n-)\n=[\n= Feedback\n-[\n\n-]\n=]\n-elink\n")
   (forward-line -14)
   (forward-char 1))
+
 
 (defun my:apsh-new-post ()
   "Open apsh file and insert template."
@@ -134,11 +141,13 @@
   (forward-line -13)
   (forward-char 18))
 
+
 (defun my:haiku-note ()
   "Open haiku note file."
   (interactive)
   (find-file (format-time-string "~/Dropbox/howm/haiku/haikunote.%Y.md"))
   (goto-char (point-min)))
+
 
 (defun my:haiku-note-post ()
   "Insert template."
