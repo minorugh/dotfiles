1b0e278c45f1d1b5232cb5f992afd33956eac803
Minoru Yamada <minorugh@users.noreply.github.com>
2020-11-28 14:14:24 +0900

    Fix key bind

--- .emacs.d/inits/40_translate.el
+++ .emacs.d/inits/40_translate.el
@@ -6,7 +6,8 @@
 
 (leaf google-translate
   :ensure t
-  :bind ("C-c t" . google-translate-auto)
+  :bind (("C-t" . chromium-translate)
+		 ("C-c t" . google-translate-auto))
   :config
   (defun google-translate-auto ()
 	"Automatically recognize and translate Japanese and English."
@@ -36,31 +37,28 @@
   ;; Fix error of "Failed to search TKK"
   (defun google-translate--get-b-d1 ()
   	"Search TKK."
-  	(list 427110 1469889687)))
+  	(list 427110 1469889687))
 
-
-(bind-key
- "C-t"
- (defun chromium-translate ()
-   "Open google translate with chromium."
-   (interactive)
-   (if (use-region-p)
-	   (let ((string (buffer-substring-no-properties (region-beginning) (region-end))))
-		 (deactivate-mark)
-		 (if (string-match (format "\\`[%s]+\\'" "[:ascii:]")
-						   string)
-			 (browse-url (concat "https://translate.google.com/?source=gtx#en/ja/"
-								 (url-hexify-string string)))
-		   (browse-url (concat "https://translate.google.com/?source=gtx#ja/en/"
-							   (url-hexify-string string)))))
-	 (let ((string (read-string "Google Translate: ")))
-	   (if (string-match
-			(format "\\`[%s]+\\'" "[:ascii:]")
-			string)
-		   (browse-url
-			(concat "https://translate.google.com/?source=gtx#en/ja/" (url-hexify-string string)))
-		 (browse-url
-		  (concat "https://translate.google.com/?source=gtx#ja/en/" (url-hexify-string string))))))))
+  (defun chromium-translate ()
+	"Open google translate with chromium."
+	(interactive)
+	(if (use-region-p)
+		(let ((string (buffer-substring-no-properties (region-beginning) (region-end))))
+		  (deactivate-mark)
+		  (if (string-match (format "\\`[%s]+\\'" "[:ascii:]")
+							string)
+			  (browse-url (concat "https://translate.google.com/?source=gtx#en/ja/"
+								  (url-hexify-string string)))
+			(browse-url (concat "https://translate.google.com/?source=gtx#ja/en/"
+								(url-hexify-string string)))))
+	  (let ((string (read-string "Google Translate: ")))
+		(if (string-match
+			 (format "\\`[%s]+\\'" "[:ascii:]")
+			 string)
+			(browse-url
+			 (concat "https://translate.google.com/?source=gtx#en/ja/" (url-hexify-string string)))
+		  (browse-url
+		   (concat "https://translate.google.com/?source=gtx#ja/en/" (url-hexify-string string))))))))
 
 ;; Local Variables:
 ;; no-byte-compile: t
