6317ba668f30bbe65ee80b548caf93b3cc6d76d7
Minoru Yamada <minorugh@users.noreply.github.com>
2020-11-28 22:29:44 +0900

    Fix key bind

--- .emacs.d/inits/10_hydra-misc.el
+++ .emacs.d/inits/10_hydra-misc.el
@@ -25,38 +25,39 @@
   (hydra-browse
    (:hint nil :exit t)
    "
-  💰 Shop^        ^💭 SNS^        ^🔃 Repos^       ^🏠 GH^        ^🙌 Favorite^    ^📝 Others^    ^💣 Github^^      Google
+  💰 Shop^        ^💭 SNS^        ^🔃 Repos^       ^🏠 Blog^      ^🙌 Favorite^    ^📝 Others^   ^💣 Github^^       Google
   ^^^^^^^^^^----------------------------------------------------------------------------------------------------------------
-  _a_: Amazon      _t_: Twitter    _g_: github      _h_: HOME      _j_: Jorudan     _c_: Chrome    _1_: masasam     _5_: Keep
-  _r_: Rakuten     _u_: Youtube    _0_: gist        _b_: xsrv.jp   _n_: News        _p_: Pocket    _2_: abo-abo     _6_: Map
-  _y_: Yodobashi   _f_: Flickr     _d_: Dropbox     _e_: Essay     _w_: Weather     _q_: Qiita     _3_: blue        _7_: Drive
-  _k_: Kakaku      _l_: Tumblr     _x_: Xserver     _:_: Blog      _s_: SanyoBas    _,_: Slack     _4_: seagle      _8_: Photo"
+  _a_: Amazon      _t_: Twitter    _g_: Gdrive      _h_: GH        _j_: Jorudan     _k_: Keep      _1_: minorugh    _5_: Calendar
+  _r_: Rakuten     _u_: Youtube    _f_: Flickr      _b_: xsrv.jp   _n_: News        _p_: Pocket    _2_: gist        _6_: Map
+  _y_: Yodobashi   _i_: Instagram  _d_: Dropbox     _e_: Essay     _w_: Weather     _q_: Qiita     _3_: explore     _7_: Gmail
+  _K_: Kakaku      _l_: Tumblr     _x_: Xserver     _:_: Blog      _s_: SanyoBas    _,_: Slack     _4_: repo.new    _8_: Photo"
    ("a" (browse-url "https://www.amazon.co.jp/"))
    ("r" (browse-url "https://www.rakuten.co.jp/"))
    ("y" (browse-url "https://www.yodobashi.com/"))
-   ("k" (browse-url "http://kakaku.com/"))
+   ("K" (browse-url "http://kakaku.com/"))
    ("u" (browse-url "https://www.youtube.com/channel/UCnwoipb9aTyORVKHeTw159A/videos"))
    ("f" (browse-url "https://www.flickr.com/photos/minorugh/"))
-   ("g" (browse-url "https://github.com/minorugh/emacs.d"))
+   ("g" (browse-url "https://drive.google.com/drive/u/0/my-drive"))
    ("0" (browse-url "https://gist.github.com/minorugh"))
-   ("1" (browse-url "https://github.com/masasam/dotfiles/tree/master/.emacs\.d"))
-   ("2" (browse-url "https://github.com/abo-abo/hydra/wiki"))
-   ("3" (browse-url "https://github.com/blue0513?tab=repositories"))
-   ("4" (browse-url "https://github.com/seagle0128/.emacs\.d/tree/master/lisp"))
-   ("5" (browse-url "https://keep.google.com/u/0/"))
+   ("1" (browse-url "https://github.com/minorugh"))
+   ("2" (browse-url "https://gist.github.com/minorugh"))
+   ("3" (browse-url "https://github.com/explore"))
+   ("4" (browse-url "https://repo.new/"))
+   ("5" (browse-url "https://calendar.google.com/calendar/u/0/r?tab=wc"))
    ("6" (browse-url "https://www.google.co.jp/maps"))
-   ("7" (browse-url "https://drive.google.com/drive/u/0/my-drive"))
+   ("7" (browse-url "https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"))
    (":" (browse-url "http://blog.wegh.net/"))
    ("e" (browse-url "http://essay.wegh.net/"))
    ("b" (browse-url "https://minorugh.xsrv.jp/"))
-   ("s" (browse-url "http://www.sanyo-bus.co.jp/pdf/20191028tarusan_schedule.pdf"))
+   ("s" (browse-url "http://www.sanyo-bus.co.jp/pdf/20200401tarusan_schedule.pdf"))
+   ("i" (browse-url "https://www.instagram.com/"))
    ("j" (browse-url "https://www.jorudan.co.jp/"))
    ("n" (browse-url "https://news.yahoo.co.jp/"))
    ("x" (browse-url "https://www.xserver.ne.jp/login_server.php"))
    ("d" (browse-url "https://www.dropbox.com/home"))
    ("q" (browse-url "https://qiita.com/tags/emacs"))
    ("8" (browse-url "https://photos.google.com/?pageId=none"))
-   ("c" (browse-url "https://google.com"))
+   ("k" (browse-url "https://keep.google.com/u/0/"))
    ("l" (browse-url "https://minorugh.tumblr.com"))
    ("w" chromium-weather)
    ("h" chromium-homepage)
