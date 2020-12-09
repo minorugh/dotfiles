8d16825be67fdcff2364ff4eda231dc43c84329a
Minoru Yamada <minorugh@users.noreply.github.com>
2020-12-09 10:30:40 +0900

    Fix menu

--- .emacs.d/inits/10_hydra-menu.el
+++ .emacs.d/inits/10_hydra-menu.el
@@ -11,7 +11,7 @@
    "
   🐳 Quick Menu
   ---^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^--------------------------------------------------------------------------------------------------------
-   _d_ropbox   _e_macs.d^^   _i_nits   _x_srv.jp^^^^   GH:_h_   _<home>_   memo_,_list_@_   _g_ithub   _r_estart   _m_arkdown   _u_ndotree
+   _d_ropbox   _e_macs.d^^   _i_nits   _x_srv.jp^^^^   GH:_h_   _<home>_   howm_,_list_@_   _g_ithub   _r_estart   _m_arkdown   _u_ndotree
    init_l_og   git:_[_._]_   _t_ramp   _y_as:_n_:_v_   _a_g🐾   _f_lyerr   _s_earch-web^^   make:_k_   _c_ompile   _o_pen-url   dotfiles_._"
    ("a" counsel-ag)
    ("o" browse-url-at-point)
