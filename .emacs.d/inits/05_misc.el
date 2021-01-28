97e0c6974b59e8ce2f388cb3edb485f3976e86b2
minoru <minoru@e590.minorugh.com>
2021-01-29 08:25:41 +0900

    Fix

--- .emacs.d/inits/05_misc.el
+++ .emacs.d/inits/05_misc.el
@@ -3,15 +3,18 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(leaf popwin :ensure t
+(leaf popwin
+  :ensure t
   :global-minor-mode t)
 
 
-(leaf iedit :ensure t
+(leaf iedit
+  :ensure t
   :bind ("C-;" . iedit-mode))
 
 
-(leaf expand-region :ensure t
+(leaf expand-region
+  :ensure t
   :bind ("C-@" . er/expand-region))
 
 
@@ -23,44 +26,52 @@
   	:hook (flymake-mode-hook . flymake-diagnostic-at-point-mode)))
 
 
-(leaf key-chord :ensure t
+(leaf key-chord
+  :ensure t
   :global-minor-mode t
   :config
   (key-chord-define-global "df" 'counsel-descbinds)
   (key-chord-define-global "l;" 'init-loader-show-log))
 
 
-(leaf prescient :ensure t
+(leaf prescient
+  :ensure t
   :global-minor-mode prescient-persist-mode
   :init
-  (leaf company-prescient :ensure t
+  (leaf company-prescient
+	:ensure t
     :after prescient company
     :global-minor-mode t)
-  (leaf ivy-prescient :ensure t
+  (leaf ivy-prescient
+	:ensure t
     :after prescient ivy
     :global-minor-mode t))
 
 
-(leaf quickrun :ensure t
+(leaf quickrun
+  :ensure t
   :bind ("<f5>" . quickrun))
 
 
-(leaf which-key :ensure t
+(leaf which-key
+  :ensure t
   :global-minor-mode t
   :config
   (setq which-key-max-description-length 40)
   (setq which-key-use-C-h-commands t))
 
 
-(leaf projectile :ensure t
+(leaf projectile
+  :ensure t
   :global-minor-mode t
-  :init
-  (setq projectile-known-projects-file "~/.emacs.d/tmp/projectile-bookmarks.eld")
-  (leaf counsel-projectile :ensure t
-	:global-minor-mode t))
+  :config
+  (setq projectile-known-projects-file "~/.emacs.d/tmp/projectile-bookmarks.eld"))
+(leaf counsel-projectile :ensure t
+  :global-minor-mode t)
 
 
-(leaf yasnippet :ensure t
+(leaf yasnippet
+  :ensure t
   :commands yas-global-mode
   :config
   (yas-global-mode)
@@ -70,11 +81,13 @@
   (leaf ivy-yasnippet :ensure t))
 
 
-(leaf restart-emacs :ensure t
+(leaf restart-emacs
+  :ensure t
   :bind ("C-x C-c" . restart-emacs))
 
 
-(leaf web-mode :ensure t
+(leaf web-mode
+  :ensure t
   :mode "\\.js?\\'" "\\.html?\\'" "\\.php?\\'")
 
 
