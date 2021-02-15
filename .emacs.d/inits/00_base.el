626fa93958aa99d6fc9e7d1ba865ce2e5241739d
minoru <minoru@e590.minorugh.com>
2021-02-15 18:17:12 +0900

    Refactor

--- .emacs.d/inits/00_base.el
+++ .emacs.d/inits/00_base.el
@@ -52,12 +52,12 @@
       "Return a string giving the duration of the Emacs initialization."
       (interactive)
       (let ((str
-	     (format "%.3f seconds"
-		     (float-time
-		      (time-subtract after-init-time before-init-time)))))
-	(if (called-interactively-p 'interactive)
-	    (message "%s" str)
-	  str)))
+			 (format "%.3f seconds"
+					 (float-time
+					  (time-subtract after-init-time before-init-time)))))
+		(if (called-interactively-p 'interactive)
+			(message "%s" str)
+		  str)))
     (advice-add 'emacs-init-time :override #'ad:emacs-init-time)))
 
 
