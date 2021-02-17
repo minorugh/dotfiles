20f696fea45a51b07dba6fdbd85377111cbb9340
minoru <minoru@minorugh.com>
2021-02-17 18:53:30 +0900

    Add early-init.el for emacs-27.1

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,10 @@
+(push '(menu-bar-lines     . nil) default-frame-alist)
+(push '(tool-bar-lines     . nil) default-frame-alist)
+(push '(scroll-bar-mode    . nil) default-frame-alist)
+(push '(blink-cursor-mode  . nil) default-frame-alist)
+(push '(column-number-mode . nil) default-frame-alist)
+(setq frame-inhibit-implied-resize t)
+(setq site-run-file nil)
+(setq package-enable-at-startup nil)
+(provide 'early-init)
+;;; early-init.el ends here
