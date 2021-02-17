1cde5035edd2e9c94aa7e64a4863573cdd97ffbb
minoru <minoru@minorugh.com>
2021-02-18 08:54:21 +0900

    Fix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,13 +5,17 @@
 ;; (setq debug-on-error t)
 
 (when (version< emacs-version "27")
+  (set-frame-parameter nil 'fullscreen 'maximized)
+  (menu-bar-mode 0)
+  (tool-bar-mode 0)
+  (setq inhibit-splash-screen t)
+  (setq inhibit-startup-message t)
   (load (concat user-emacs-directory "early-init.el")))
 
 (package-initialize)
 (unless (package-installed-p 'leaf)
   (package-refresh-contents)
   (package-install 'leaf))
-
 
 (leaf leaf-keywords
   :ensure t
@@ -22,7 +26,6 @@
   (leaf-keywords-init)
   (setq load-prefer-newer t)
   (setq custom-file (locate-user-emacs-file "~/.emacs.d/tmp/custom.el")))
-
 
 (leaf init-loader
   :ensure t
