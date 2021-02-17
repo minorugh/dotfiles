7505f0e0568499959c8155289f0c67a7077a3303
minoru <minoru@minorugh.com>
2021-02-17 21:06:03 +0900

    Add early-init.el

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -6,14 +6,34 @@
 ;;
 ;;; Code:
 
-(push '(menu-bar-lines     . nil) default-frame-alist)
-(push '(tool-bar-lines     . nil) default-frame-alist)
-(push '(scroll-bar-mode    . nil) default-frame-alist)
-(push '(blink-cursor-mode  . nil) default-frame-alist)
-(push '(column-number-mode . nil) default-frame-alist)
+(defvar default-file-name-handler-alist file-name-handler-alist)
+(defvar default-gc-cons-threshold gc-cons-threshold)
+(setq file-name-handler-alist nil)
+(setq gc-cons-threshold (* 1024 1024 100))
+(add-hook 'emacs-startup-hook
+		  (lambda ()
+			"Restore defalut values after startup."
+			(setq file-name-handler-alist default-file-name-handler-alist)
+			(setq gc-cons-threshold default-gc-cons-threshold)))
+
+(menu-bar-mode -1)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(blink-cursor-mode  . 0) default-frame-alist)
+(push '(column-number-mode . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(fullscreen . maximized) default-frame-alist)
+
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
 (setq frame-inhibit-implied-resize t)
-(setq site-run-file nil)
+
+(customize-set-variable
+ 'package-archives '(("org"   . "https://orgmode.org/elpa/")
+					 ("melpa" . "https://melpa.org/packages/")
+ 					 ("gnu"   . "https://elpa.gnu.org/packages/")))
 (setq package-enable-at-startup nil)
+
 
 (provide 'early-init)
 
