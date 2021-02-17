0d08bb44b331d0615d07eea0aff09f6c775dfe0d
minoru <minoru@minorugh.com>
2021-02-17 19:44:47 +0900

    Fix comment

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,3 +1,11 @@
+;;; early-init.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Emacs27 introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+;;
+;;; Code:
+
 (push '(menu-bar-lines     . nil) default-frame-alist)
 (push '(tool-bar-lines     . nil) default-frame-alist)
 (push '(scroll-bar-mode    . nil) default-frame-alist)
@@ -6,5 +14,7 @@
 (setq frame-inhibit-implied-resize t)
 (setq site-run-file nil)
 (setq package-enable-at-startup nil)
+
 (provide 'early-init)
+
 ;;; early-init.el ends here
