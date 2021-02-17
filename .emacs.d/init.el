38852118e04e5269130a47363654591b0852e1ef
minoru <minoru@minorugh.com>
2021-02-17 21:06:32 +0900

    Move startup settings to early-init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,31 +4,9 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-;; Quiet start
-(set-frame-parameter nil 'fullscreen 'maximized)
-(scroll-bar-mode 0)
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
-(setq byte-compile-warnings '(cl-functions))
+(when (version< emacs-version "27")
+  (load (concat user-emacs-directory "early-init.el")))
 
-;; Speed up startup
-(defvar default-file-name-handler-alist file-name-handler-alist)
-(defvar default-gc-cons-threshold gc-cons-threshold)
-(setq file-name-handler-alist nil)
-(setq gc-cons-threshold (* 1024 1024 100))
-(add-hook 'emacs-startup-hook
-		  (lambda ()
-			"Restore defalut values after startup."
-			(setq file-name-handler-alist default-file-name-handler-alist)
-			(setq gc-cons-threshold default-gc-cons-threshold)))
-
-;; Packages
-(customize-set-variable
- 'package-archives '(("org"   . "https://orgmode.org/elpa/")
-					 ("melpa" . "https://melpa.org/packages/")
-					 ("gnu"   . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless (package-installed-p 'leaf)
   (package-refresh-contents)
