7fae50ac14d5d5589e62330877d7972ec6d61cbe
minoru <minoru@minorugh.com>
2021-02-18 08:39:23 +0900

    Fix

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -17,11 +17,11 @@
 			(setq gc-cons-threshold default-gc-cons-threshold)))
 
 
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(fullscreen . maximized) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(blink-cursor-mode . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-(push '(fullscreen . maximized) default-frame-alist)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 (setq frame-inhibit-implied-resize t)
