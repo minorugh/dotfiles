ad5dd402d3f060a454c2092e8cd5be4bc9e9bad3
minoru <minoru@minorugh.com>
2021-07-03 12:20:04 +0900

    Fix menu items

--- .emacs.d/inits/10_hydra-pinky.el
+++ .emacs.d/inits/10_hydra-pinky.el
@@ -20,8 +20,12 @@ If there are two or more windows, it will go to another window."
  (defhydra hydra-pinky
    (:color red :hint nil)
    "
-  🐳 page:_SPC_:_b_:_l_  goto:_j_._w_  window:_0_:_1_:_o_:_x_  _d_iff:_n_:_p_  zoom:_-__.__+_  buffer:_[__:__]_  _f_ile  _s_wiper"
+  🐳 page:_SPC_:_b_  👆:_h_._l_._j_._k_  window:_0_:_1_:_o_:_x_  _d_iff:_n_:_p_  zoom:_-__.__+_  buffer:_[__:__]_  _f_ile  _s_wiper"
    ;; move page
+   ("h" backward-char)
+   ("j" next-line)
+   ("k" previous-line)
+   ("l" forward-char)
    ("SPC" scroll-up-command)
    ("f" scroll-up-command)
    ("b" scroll-down-command)
@@ -29,7 +33,6 @@ If there are two or more windows, it will go to another window."
    ("<prior>" scroll-down-command)
    ("g" beginning-of-buffer)
    ("G" end-of-buffer)
-   ("l" recenter-top-bottom)
    ;; move line
    ("<down>" next-line)
    ("<up>" previous-line)
@@ -37,7 +40,7 @@ If there are two or more windows, it will go to another window."
    ("<left>" backward-char)
    ("a" seq-home)
    ("e" seq-end)
-   ("j" goto-line)
+   ;; ("j" goto-line)
    ("<down>" next-line)
    ("<up>" previous-line)
    ("<right>" forward-char)
@@ -60,8 +63,6 @@ If there are two or more windows, it will go to another window."
    (":" counsel-switch-buffer)
    ("[" winner-undo)
    ("]" winner-redo)
-   ;; avy
-   ("w" avy-goto-word-1)
    ;; Others
    ("f" counsel-find-file)
    ("s" swiper)))
