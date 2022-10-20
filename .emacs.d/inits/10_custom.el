aba1099bb9c93607872edbe34a3654a5b4167f1c
minorugh <minorugh@gmail.com>
2022-10-20 19:38:54 +0900

    Fix keybind

--- .emacs.d/inits/10_custom.el
+++ .emacs.d/inits/10_custom.el
@@ -9,7 +9,6 @@
 (leaf *user-custom-functions
   :bind	 (("<f3>" . thunar-open)
 		  ("<f4>" . terminal-open)
-		  ("<f6>" . gitk-open)
 		  ("<f8>" . toggle-menu-bar-mode-from-frame)
 		  ("C-x /" . my:delete-this-file)
 		  ("<muhenkan>" . minibuffer-keyboard-quit)
@@ -27,12 +26,6 @@
 	(let ((dir (directory-file-name default-directory)))
 	  ;; (shell-command (concat "gnome-terminal --maximize --working-directory " dir))))
 	  (shell-command (concat "gnome-terminal --working-directory " dir))))
-
-  (defun gitk-open ()
-	"Open gitk with current dir."
-	(interactive)
-	(shell-command "gitk &")
-	(delete-other-windows))
 
   (defun my:delete-file-if-no-contents ()
 	"If the file is empty, it will be deleted automatically."
