85ba12025c4d9cd94445b1877140a8436ef647db
minorugh <minorugh@gmail.com>
2022-11-07 22:09:44 +0900

    Add startup setting

--- .emacs.d/inits/00_base.el
+++ .emacs.d/inits/00_base.el
@@ -5,47 +5,51 @@
 
 (leaf cus-start
   :custom
-  `(;; Faster rendering by not corresponding to right-to-left language
-    (bidi-display-reordering . nil)
-    ;; Do not make a backup file like *.~
-    (make-backup-files . nil)
-    ;; Do not use auto save
-    (auto-save-default . nil)
-    (auto-save-list-file-prefix . nil)
-    ;; Do not create lock file
-    (create-lockfiles . nil)
-    ;; Open symbolic link directly
-    (vc-follow-symlinks . t)
-    ;; Do not distinguish uppercase and lowercase letters on completion
-    (completion-ignore-case . t)
-    (read-file-name-completion-ignore-case . t)
-    ;; Point keeps its screen position when scroll
-    (scroll-preserve-screen-position . t)
-    ;; All warning sounds and flash are invalid
-    (ring-bell-function . 'ignore)
-    ;; Turn off warning sound screen flash
-    (visible-bell . nil)
-    ;; Copy text with mouse range selection
-    (mouse-drag-copy-region . t)
-    ;; Deleted files go to the trash
-    (delete-by-moving-to-trash . t)
-    ;; Tab width default
-    (tab-width . 4)
-    ;; Limit the final word to a line break code (automatically correct)
-    (require-final-newline . t)
-    ;; Disallow adding new lines with newline at the end of the buffer
-    (next-line-add-newlines . nil)
-    ;; Make it easy to see when it is the same name file
-    (uniquify-buffer-name-style . 'post-forward-angle-brackets)
-    ;; It keeps going steadily the local mark ...  C-u C-SPC C-SPC
-    ;; It keeps going steadily the global mark ... C-x C-SPC C-SPC
-    (set-mark-command-repeat-pop . t)
-    ;; Use the X11 clipboard
-    (select-enable-clipboard  . t)
-    ;; change-default-file-location
-    (url-configuration-directory . "~/.emacs.d/tmp/url")
-    (bookmark-file . "~/.emacs.d/tmp/bookmarks")
-    ))
+  `(;; Startup setting
+	(inhibit-splash-screen . t)
+	(inhibit-startup-message . t)
+	(byte-compile-warnings . '(cl-functions))
+	;; Faster rendering by not corresponding to right-to-left language
+	(bidi-display-reordering . nil)
+	;; Do not make a backup file like *.~
+	(make-backup-files . nil)
+	;; Do not use auto save
+	(auto-save-default . nil)
+	(auto-save-list-file-prefix . nil)
+	;; Do not create lock file
+	(create-lockfiles . nil)
+	;; Open symbolic link directly
+	(vc-follow-symlinks . t)
+	;; Do not distinguish uppercase and lowercase letters on completion
+	(completion-ignore-case . t)
+	(read-file-name-completion-ignore-case . t)
+	;; Point keeps its screen position when scroll
+	(scroll-preserve-screen-position . t)
+	;; All warning sounds and flash are invalid
+	(ring-bell-function . 'ignore)
+	;; Turn off warning sound screen flash
+	(visible-bell . nil)
+	;; Copy text with mouse range selection
+	(mouse-drag-copy-region . t)
+	;; Deleted files go to the trash
+	(delete-by-moving-to-trash . t)
+	;; Tab width default
+	(tab-width . 4)
+	;; Limit the final word to a line break code (automatically correct)
+	(require-final-newline . t)
+	;; Disallow adding new lines with newline at the end of the buffer
+	(next-line-add-newlines . nil)
+	;; Make it easy to see when it is the same name file
+	(uniquify-buffer-name-style . 'post-forward-angle-brackets)
+	;; It keeps going steadily the local mark ...  C-u C-SPC C-SPC
+	;; It keeps going steadily the global mark ... C-x C-SPC C-SPC
+	(set-mark-command-repeat-pop . t)
+	;; Use the X11 clipboard
+	(select-enable-clipboard  . t)
+	;; change-default-file-location
+	(url-configuration-directory . "~/.emacs.d/tmp/url")
+	(bookmark-file . "~/.emacs.d/tmp/bookmarks")
+	))
 
 ;; Set default modes for startup hook
 (defun my:default-modes ()
