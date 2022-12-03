cc8543d8c042348d2c8435bc12f1b0a787ebd4a1
minorugh <minorugh@gmail.com>
2022-12-03 23:10:31 +0900

    Fix key bind

--- .emacs.d/inits/10_custom.el
+++ .emacs.d/inits/10_custom.el
@@ -11,7 +11,8 @@
 		  ("<f4>" . terminal-open)
 		  ("<f5>" . ssh-vim)
 		  ("<f8>" . toggle-menu-bar-mode-from-frame)
-		  ("C-x /" . my:delete-this-file)
+		  ("C-c /" . my:delete-this-file)
+		  ("C-c h" . chromium-tegaki)
 		  ("<muhenkan>" . minibuffer-keyboard-quit)
 		  ("C-c <left>" . winner-undo)
 		  ("C-c <right>" . winner-redo))
@@ -45,6 +46,7 @@
   ;;     (setq after-save-hook
   ;; 			(cons 'my:delete-file-if-no-contents after-save-hook)))
 
+
   (defun my:delete-this-file ()
 	"Delete the current file, and kill the buffer."
 	(interactive)
@@ -53,45 +55,49 @@
 	(when (yes-or-no-p (format "Really delete '%s'?"
 							   (file-name-nondirectory buffer-file-name)))
 	  (delete-file (buffer-file-name))
-	  (kill-this-buffer))))
+	  (kill-this-buffer)))
+
+  (defun chromium-tegaki ()
+	"Chromium tegaki site."
+	(interactive)
+	(browse-url "https://mojinavi.com/tegaki"))
+
+  ;; Automatically open root permission file with sudo
+  (leaf *sudo-open
+	:doc "https://ameblo.jp/grennarthmurmand1976/entry-12151018656.html"
+	:config
+	(defun file-root-p (filename)
+	  "Return t if file FILENAME created by root."
+	  (eq 0 (nth 2 (file-attributes filename))))
+
+	(defadvice find-file (around my:find-file activate)
+	  "Open FILENAME using tramp's sudo method if it's root permission."
+	  (if (and (file-root-p (ad-get-arg 0))
+			   (not (file-writable-p (ad-get-arg 0)))
+			   (y-or-n-p (concat (ad-get-arg 0)
+								 " is root permission. Open it as root? ")))
+		  (my:find-file-sudo (ad-get-arg 0))
+		ad-do-it))
+
+	(defun my:find-file-sudo (file)
+	  "Opens FILE with root privileges."
+	  (interactive "F")
+	  (set-buffer (find-file (concat "/sudo::" file)))))
 
 
-;; Automatically open root permission file with sudo
-(leaf *sudo-open
-  :doc "https://ameblo.jp/grennarthmurmand1976/entry-12151018656.html"
-  :config
-  (defun file-root-p (filename)
-	"Return t if file FILENAME created by root."
-	(eq 0 (nth 2 (file-attributes filename))))
-
-  (defadvice find-file (around my:find-file activate)
-	"Open FILENAME using tramp's sudo method if it's root permission."
-	(if (and (file-root-p (ad-get-arg 0))
-			 (not (file-writable-p (ad-get-arg 0)))
-			 (y-or-n-p (concat (ad-get-arg 0)
-							   " is root permission. Open it as root? ")))
-		(my:find-file-sudo (ad-get-arg 0))
-	  ad-do-it))
-
-  (defun my:find-file-sudo (file)
-	"Opens FILE with root privileges."
-	(interactive "F")
-	(set-buffer (find-file (concat "/sudo::" file)))))
-
-
-;; PS-printer
-(defalias 'ps-mule-header-string-charsets 'ignore)
-(setq ps-multibyte-buffer 'non-latin-printer
-	  ps-paper-type 'a4
-	  ps-font-size 9
-	  ;; ps-font-family 'Helvetica
-	  ps-font-family 'Courier
-	  ps-line-number-font 'Courier
-	  ps-printer-name nil
-	  ps-print-header nil
-	  ps-show-n-of-n t
-	  ps-line-number t
-	  ps-print-footer nil)
+  ;; PS-printer
+  (defalias 'ps-mule-header-string-charsets 'ignore)
+  (setq ps-multibyte-buffer 'non-latin-printer
+		ps-paper-type 'a4
+		ps-font-size 9
+		;; ps-font-family 'Helvetica
+		ps-font-family 'Courier
+		ps-line-number-font 'Courier
+		ps-printer-name nil
+		ps-print-header nil
+		ps-show-n-of-n t
+		ps-line-number t
+		ps-print-footer nil))
 
 
 ;; Local Variables:
