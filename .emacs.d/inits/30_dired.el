34dedd2b9c8a3b8e519db70b8470506c7083587a
minorugh <minorugh@gmail.com>
2022-12-03 11:34:56 +0900

    Add my:dired-sort

--- .emacs.d/inits/30_dired.el
+++ .emacs.d/inits/30_dired.el
@@ -109,6 +109,19 @@
 			 (length image-files)
 			 (mapconcat 'identity image-files " ")))))
 
+(defun my:dired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+	(after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding mark."
+  (my:dired-sort))
+
 
 ;; Local Variables:
 ;; no-byte-compile: t
