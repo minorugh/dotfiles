5a785fdc7f997d6efb75488dba55bcb65bfd6696
minorugh <minorugh@gmail.com>
2022-12-14 18:55:45 +0900

    Fix key bind

--- .emacs.d/inits/40_evil-mode.el
+++ .emacs.d/inits/40_evil-mode.el
@@ -8,23 +8,21 @@
   :if (display-graphic-p)
   :hook (prog-mode-hook . evil-local-mode)
   :chord ("::" . toggle-evil-local-mode)
-  :bind ((:key-translation-map
-		  ("<muhenkan>" . evil-escape-or-quit))
-		 (:evil-operator-state-map
-		  ("<muhenkan>" . evil-escape-or-quit))
-		 (:evil-normal-state-map
-		  ("M-." . hydra-quick/body)
-		  ("<home>" . open-dashboard)))
+  :bind (:evil-normal-state-map
+		 ("M-." . hydra-quick/body)
+		 ([home] . open-dashboard)
+		 ([muhenkan] . keyboard-quit))
   :config
   ;; Insert state overrides Emacs settings
   (setcdr evil-insert-state-map nil)
   (define-key evil-insert-state-map [escape] 'evil-normal-state)
+  (define-key key-translation-map [muhenkan] 'evil-escape-or-quit)
+  (define-key evil-operator-state-map [muheqkan] 'evil-escape-or-quit)
 
   (defun toggle-evil-local-mode ()
-	"Toggle on and off evil mode in local buffer."
+	"Toggle on and off evil local mode."
 	(interactive)
-	(if evil-local-mode
-		(evil-local-mode 0)
+	(if evil-local-mode (evil-local-mode 0)
 	  (evil-local-mode 1)))
 
   (defun evil-escape-or-quit (&optional prompt)
@@ -33,7 +31,7 @@
 	(cond
 	 ((or (evil-normal-state-p) (evil-insert-state-p) (evil-visual-state-p)
 		  (evil-replace-state-p) (evil-visual-state-p)) [escape])
-	 (t (kbd "<muhenkan>")))))
+	 ([muhenkan]))))
 
 
 ;; Local Variables:
