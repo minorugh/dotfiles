31c87aa5d1074adc906aeb9a770ae2511c799a78
minorugh <minorugh@gmail.com>
2022-12-14 22:04:33 +0900

    Fix keybind

--- .emacs.d/inits/06_key-chord.el
+++ .emacs.d/inits/06_key-chord.el
@@ -8,6 +8,7 @@
   :hook (after-init-hook . key-chord-mode)
   :chord (("df" . counsel-descbinds)
 		  ("l;" . init-loader-show-log)
+		  (";;" . view-mode)
 		  ("@@" . howm-list-all)
 		  ("jk" . open-junk-file))
   :custom
