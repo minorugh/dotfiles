ee909b7938430aa4b7524e907e84deb31d20d037
minorugh <minorugh@gmail.com>
2022-12-17 13:46:40 +0900

    Fix

--- .emacs.d/inits/40_evil-mode.el
+++ .emacs.d/inits/40_evil-mode.el
@@ -7,37 +7,11 @@
   :hook (after-init-hook  . evil-mode)
   :chord ("::" . toggle-evil-mode)
   :bind ((:evil-normal-state-map
-		  ("." . hydra-evil-pinky/body)
+		  ("." . hydra-pinky/body)
 		  ("C-e" . seq-end)
 		  ("M-." . hydra-quick/body)
 		  ([home] . open-dashboard)
 		  ([muhenkan] . keyboard-quit)))
-  :hydra
-  (hydra-evil-pinky
-   (:color red :hint nil)
-   "
-  :: _SPC_._b_._a_._e_  :_0_._1_._2_._o_._s_  :_-__.__+_  _d_eepl  _w_eblio  _k_oujien  _g_oogle  _c_hert
-   "
-   ;; web serch
-   ("d" gts-do-translate)
-   ("w" my:weblio)
-   ("k" my:koujien)
-   ("g" my:google)
-   ("c" chromium-vim-chert)
-   ;; move line
-   ("SPC" scroll-up-command)
-   ("b" scroll-down-command)
-   ("a" seq-home)
-   ("e" seq-end)
-   ;; window
-   ("+" text-scale-increase)
-   ("-" text-scale-decrease)
-   ("." (text-scale-set 0))
-   ("0" delete-window)
-   ("1" delete-other-windows)
-   ("2" split-window-below)
-   ("s" window-swap-states)
-   ("o" other-window-or-split))
   :init
   (setq evil-cross-lines t)
   (setq evil-undo-system 'undo-fu)
@@ -53,6 +27,7 @@
   (evil-set-initial-state 'fundamental-mode 'insert)
   (evil-set-initial-state 'text-mode 'insert)
   (evil-set-initial-state 'neotree-mode 'emacs)
+  (evil-set-initial-state 'dired-mode 'emacs)
   (evil-set-initial-state 'dashboard-mode 'emacs)
 
   (defun toggle-evil-mode ()
