5cc0d764db46c719574b0aae021ded5973f140fb
minorugh <minorugh@gmail.com>
2022-12-19 12:35:49 +0900

    Refactor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,20 +8,13 @@
 ;; (setq debug-on-error t)
 
 ;; Speed up startup
-(unless (or (daemonp) noninteractive init-file-debug)
-  (let ((old-file-name-handler-alist file-name-handler-alist))
-    (setq file-name-handler-alist nil)
-    (add-hook 'emacs-startup-hook
-              (lambda ()
-                "Recover file name handlers."
-                (setq file-name-handler-alist
-                      (delete-dups (append file-name-handler-alist
-                                           old-file-name-handler-alist)))))))
-;; Defer garbage collection further back in the startup process
+(defconst my:file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
 (add-hook 'emacs-startup-hook
           (lambda ()
-            "Recover GC values after startup."
-            (setq gc-cons-threshold 800000)))
+            "Recover file name handlers and GC values after startup."
+			(setq file-name-handler-alist my:file-name-handler-alist)
+			(setq gc-cons-threshold 800000)))
 
 ;; Package
 (eval-and-compile
