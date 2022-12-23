a8f13be2b1e855ebaf2f527ca2bfae477115c7c8
minorugh <minorugh@gmail.com>
2022-12-23 18:45:54 +0900

    Fix keybind

--- .emacs.d/inits/50_org-mode.el
+++ .emacs.d/inits/50_org-mode.el
@@ -5,27 +5,35 @@
 
 (leaf org
   :hook ((emacs-startup-hook . (lambda () (require 'org-protocol)))
+		 (org-mode-hook . (lambda () (electric-indent-local-mode -1)))
 		 ((org-capture-mode-hook org-agenda-mode-hook) . delete-other-windows))
   :chord (",," . org-capture)
   :bind ((:org-mode-map
-		  ([muhenkan] . view-mode)
 		  ("C-c i" . org-edit-special))
 		 ("C-c a" . org-agenda)
 		 ("C-c c" . org-capture)
 		 ("C-c k" . org-capture-kill)
 		 ("C-c i" . org-edit-src-exit))
-  :custom
-  `((org-log-done . 'time)
-	(timep-use-speed-commands . t)
-	(org-src-fontify-natively . t)
-	(org-startup-folded . 'content)
-	(org-agenda-files . '("~/Dropbox/howm/org/task.org"))
-	(org-agenda-span . 30))
   :config
+  (setq org-log-done 'time)
+  (setq org-use-speed-commands t)
+  (setq org-src-fontify-natively t)
+  (setq org-startup-folded 'content)
+  (setq org-startup-truncated nil)
+  (setq org-agenda-files '("~/Dropbox/howm/org/task.org"))
+  (setq org-refile-targets
+		(quote (("~/Dropbox/howm/org/archives.org" :level . 1)
+				("~/Dropbox/howm/org/remember.org" :level . 1)
+				("~/Dropbox/howm/org/memo.org" :level . 1)
+				("~/Dropbox/howm/org/task.org" :level . 1))))
+
+  ;; Capture template
   (setq org-capture-templates
 		'(("m" " Memo" entry (file+headline "~/Dropbox/howm/org/memo.org" "Memo")
 		   "* %? %U %i")
 		  ("i" " Idea" entry (file+headline "~/Dropbox/howm/org/idea.org" "Idea")
+		   "* %? %U %i")
+		  ("r" " Remember" entry (file+headline "~/Dropbox/howm/org/remember.org" "Remember")
 		   "* %? %U %i")
 		  ("t" " Task" entry (file+headline "~/Dropbox/howm/org/task.org" "TASK")
 		   "** TODO %?\n SCHEDULED: %^t \n")
@@ -34,13 +42,7 @@
 		  ("p" " Code capture" entry (file+headline "~/Dropbox/howm/org/capture.org" "Code")
 		   "* %^{Title} \nSOURCE: %:link\nCAPTURED: %U\n\n#+BEGIN_SRC\n%i\n#+END_SRC\n" :prepend t)
 		  ("L" " Link capture" entry (file+headline "~/Dropbox/howm/org/capture.org" "Link")
-		   "* [[%:link][%:description]] \nCAPTURED: %U\nREMARKS: %?" :prepend t)))
-
-  (setq org-refile-targets
-		(quote (("~/Dropbox/howm/org/archives.org" :level . 1)
-				("~/Dropbox/howm/org/remember.org" :level . 1)
-				("~/Dropbox/howm/org/memo.org" :level . 1)
-				("~/Dropbox/howm/org/task.org" :level . 1)))))
+		   "* [[%:link][%:description]] \nCAPTURED: %U\nREMARKS: %?" :prepend t))))
 
 
 ;; Local Variables:
