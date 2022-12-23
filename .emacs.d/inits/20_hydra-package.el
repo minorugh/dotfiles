5d169b8a55c0858937cf6887319a48af73917df0
minorugh <minorugh@gmail.com>
2022-12-23 18:44:13 +0900

    Fix

--- .emacs.d/inits/20_hydra-package.el
+++ .emacs.d/inits/20_hydra-package.el
@@ -24,39 +24,7 @@
   (defun select-elget-command ()
     "Narrow the only el-get command in `M-x'."
     (interactive)
-    (counsel-M-x "^el-get "))
-
-  :hydra
-  (hydra-pinky
-   (:color red :hint nil)
-   "
-  :_SPC_._b_._a_._e_  :_0_._1_._2_._o_._x_  :_[__:__]_  :_-__.__+_  :_s_wiper  :_d_eepl _w_eblio _k_oujien _g_oogle _c_hert
-   "
-   ;; web serch
-   ("d" gts-do-translate)
-   ("w" my:weblio)
-   ("k" my:koujien)
-   ("g" my:google)
-   ("c" chromium-vim-chert)
-   ("s" swiper)
-   ;; move line
-   ("SPC" scroll-up-command)
-   ("b" scroll-down-command)
-   ("a" seq-home)
-   ("e" seq-end)
-   ;; buffer
-   (":" counsel-switch-buffer)
-   ("[" winner-undo)
-   ("]" winner-redo)
-   ;; window
-   ("+" text-scale-increase)
-   ("-" text-scale-decrease)
-   ("." (text-scale-set 0))
-   ("0" delete-window)
-   ("1" delete-other-windows)
-   ("2" split-window-below)
-   ("x" window-swap-states)
-   ("o" other-window-or-split)))
+    (counsel-M-x "^el-get ")))
 
 
 ;; Local Variables:
