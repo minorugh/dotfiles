040c4449be6d69bb602fb0ee6d08d674239d5931
minorugh <minorugh@gmail.com>
2023-01-22 10:09:17 +0900

    Fix

--- .emacs.d/inits/30_hydra-make.el
+++ .emacs.d/inits/30_hydra-make.el
@@ -87,30 +87,34 @@
   (interactive)
   (compile "make clean"))
 
-
 (leaf package-utils
   :ensure t
+  :defer-config t
   :chord ("p@" . hydra-package/body)
   :hydra
   (hydra-package
    (:color red :hint nil)
    "
- 📦 Package: _m_elpa-list  _p_ackage-utils  _e_l-get
+ 📦 Package: _m_elpa  _i_nstall  upgrade:_l_ist._n_ame._a_ll  _r_emove  _e_l-get
 "
-   ("p" select-package-utils-command)
-   ("e" select-elget-command)
+   ("i" package-install)
+   ("l" package-utils-list-upgrades)
+   ("n" package-utils-upgrade-by-name)
+   ("r" my:remove-by-name)
+   ("a" package-utils-upgrade-all-and-restart)
    ("m" package-list-packages)
+   ("e" select-elget-command)
    ("<muhenkan>" nil))
-  :preface
-  (defun select-package-utils-command ()
-	"Narrow the only el-get command in `M-x'."
-	(interactive)
-	(counsel-M-x "^package-utils-"))
-
+  :config
   (defun select-elget-command ()
-	"Narrow the only el-get command in `M-x'."
-	(interactive)
-	(counsel-M-x "^el-get ")))
+    "Narrow the only el-get command in `M-x'."
+    (interactive)
+    (counsel-M-x "^el-get "))
+
+  (defun my:remove-by-name ()
+    "Narrow the only el-get command in `M-x'."
+    (interactive)
+    (counsel-M-x "purbn")))
 
 
 ;; Local Variables:
