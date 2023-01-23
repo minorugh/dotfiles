42d6b0470dfe7dd5731d5aca2cd3b5eaad66128c
minorugh <minorugh@gmail.com>
2023-01-23 11:54:28 +0900

    New

---
+++ .emacs.d/inits/20_hydra-pinky.el
@@ -0,0 +1,47 @@
+;;; 20_hydra-pinky.el --- Hydra confuguration for pinky. -*- lexical-binding: t; no-byte-compile: t -*-
+;;; Commentary:
+;;; Code:
+;;(setq debug-on-error t)
+
+(leaf *pinky
+  :chord ("::" . hydra-pinky/body)
+  :hydra
+  (hydra-pinky
+   (:color red :hint nil)
+   "
+  :_0_._1_._o_._x_  :_-__.__+_  _d_iff:_n_._p_  buffer:_[__:__]_  _f_ile  _s_wiper"
+   ;; window
+   ("+" text-scale-increase)
+   ("-" text-scale-decrease)
+   ("." (text-scale-set 0))
+   ("0" delete-window)
+   ("1" delete-other-windows)
+   ("2" split-window-below)
+   ("3" split-window-right)
+   ("x" window-swap-states)
+   ("o" other-window-or-split)
+   ;; diff-hl
+   ("d" vc-diff)
+   ("n" diff-hl-next-hunk)
+   ("p" diff-hl-previous-hunk)
+   ;; buffer
+   (":" counsel-switch-buffer)
+   ("[" winner-undo)
+   ("]" winner-redo)
+   ;; Others
+   ("f" counsel-find-file)
+   ("s" swiper)))
+
+
+(defhydra hydra-ctrl-x (ctl-x-map "" :pre (widen))
+  "ctrl-x"
+  ("0" delete-window)
+  ("1" delete-other-windows)
+  ("2" split-window-below)
+  ("3" split-window-right)
+  ("o" other-window-or-split)
+  ("S" window-swap-states))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 20_hydra-pinky.el ends here
