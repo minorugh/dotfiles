979af204feea27abbfcc586415d38daa8fc0c41f
minorugh <minorugh@gmail.com>
2023-05-08 16:14:22 +0900

    Fix

--- .emacs.d/inits/02_git.el
+++ .emacs.d/inits/02_git.el
@@ -8,7 +8,8 @@
   :ensure t
   :bind (("C-x g" . magit-status )
 		 ("M-g" . hydra-git/body))
-  :hook (magit-post-refresh-hook . diff-hl-magit-post-refresh)
+  :hook ((magit-post-refresh-hook . diff-hl-magit-post-refresh)
+		 (magit-blame-mode-hook . evil-emacs-state))
   :hydra
   (hydra-git
    (:color red :hint nil)
