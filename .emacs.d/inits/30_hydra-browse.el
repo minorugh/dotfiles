163f5f4a0246a0d94a565996f921c1eb139cd883
minorugh <minorugh@gmail.com>
2023-05-30 16:56:25 +0900

    Fix

--- .emacs.d/inits/30_hydra-browse.el
+++ .emacs.d/inits/30_hydra-browse.el
@@ -9,11 +9,11 @@
    (:hint nil :exit t)
    "
   ^ Shop^        ^ SNS^        ^🔃 Repos^       ^ Blog^       ^ Life^        ^ Social^    ^ Github^      oogle
-  ^^^^^^^^------------------------------------------------------------------------------------------------------------------
+  ^^^^^^^^───────────────────────────────────────────────────────────────────────────────────────────────────────────────────
   _A_: Amazon      _t_: Twitter    _d_: Dropbox     _g_: ghub.io    _j_: Jorudan     _k_: Keep      _1_: minorugh    _5_: Translate
-  _R_: Rakuten     _y_: Youtube    _f_: Flickr      _x_: xsrv.jp    _n_: News        _p_: Pocket    _2_: gist        _6_: Map
+  _R_: Rakuten     _y_: Youtube    _f_: Flickr      _m_: snap       _n_: News        _p_: Pocket    _2_: gist        _6_: Map
   _Y_: Yodobashi   _I_: Instagram  _G_: Gdrive      _e_: Essay      _w_: Weather     _q_: Qiita     _3_: explore     _7_: Earth
-  _K_: Kakaku      _T_: Tumblr     _X_: Xserver     _b_: Blog       _S_: SanyoBas    _s_: Slack     _4_: Centaur     _8_: Photo
+  _K_: Kakaku      _T_: Tumblr     _x_: Xserver     _b_: Blog       _S_: SanyoBas    _s_: Slack     _4_: Centaur     _8_: Photo
 "
    ("A" (browse-url "https://www.amazon.co.jp/"))
    ("R" (browse-url "https://www.rakuten.co.jp/"))
@@ -30,21 +30,21 @@
    ("5" (browse-url "https://translate.google.co.jp/?hl=ja"))
    ("6" (browse-url "https://www.google.co.jp/maps"))
    ("7" (browse-url "https://earth.google.com/web/"))
-   ("b" (browse-url "http://blog.wegh.net/"))
-   ("e" (browse-url "http://essay.wegh.net/"))
-   ("x" (browse-url "https://minorugh.xsrv.jp/"))
+   ("b" (browse-url "http://blog.gospel-haiku.com/"))
+   ("e" (browse-url "https://es.gospel-haiku.com/post/"))
+   ("m" (browse-url "https://snap.minorugh.com/"))
    ("S" (browse-url "http://www.sanyo-bus.co.jp/pdf/20201124tarusan_schedule.pdf"))
    ("I" (browse-url "https://www.instagram.com/"))
    ("j" (browse-url "https://www.jorudan.co.jp/"))
    ("n" (browse-url "https://news.yahoo.co.jp/"))
-   ("X" (browse-url "https://www.xserver.ne.jp/login_server.php"))
+   ("x" (browse-url "https://www.xserver.ne.jp/login_server.php"))
    ("d" (browse-url "https://www.dropbox.com/home"))
    ("q" (browse-url "https://qiita.com/minoruGH"))
    ("8" (browse-url "https://photos.google.com/?pageId=none"))
    ("k" (browse-url "https://keep.google.com/u/0/"))
    ("T" (browse-url "https://minorugh.tumblr.com"))
    ("w" (browse-url "https://tenki.jp/week/6/31/"))
-   ("g" (browse-url "https://minorugh.github.io/docs/config.html"))
+   ("g" (browse-url "https://minorugh.github.io/"))
    ("p" (browse-url "https://getpocket.com/a/queue/"))
    ("t" (browse-url "https://tweetdeck.twitter.com/"))
    ("s" (browse-url "https://emacs-jp.slack.com/messages/C1B73BWPJ/"))
