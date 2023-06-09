8b2062c83da62a34cd1550182ad3a96c44919d82
minorugh <minorugh@gmail.com>
2023-06-09 22:14:21 +0900

    Fix

--- .emacs.d/inits/05_evil.el
+++ .emacs.d/inits/05_evil.el
@@ -5,18 +5,22 @@
 
 (leaf evil
   :ensure t
-  :hook ((after-init-hook . evil-mode)
-		 (find-file-hook . my:evil-insert-state))
-  :bind (:evil-normal-state-map
-		 ("?" . chromium-vim-chert)
-		 ("SPC" . evil-insert)
-		 ("M-." . nil))
+  :hook (after-init-hook . evil-mode)
+  :bind ((:evil-normal-state-map
+		  ("?" . chromium-vim-chert)
+		  ("SPC" . evil-insert)
+		  ("M-." . nil))
+		 (:evil-emacs-state-map
+		  ([muhenkan] . evil-normal-state)))
   :init
   ;; Options for Evil, must be written bfore (require 'evil)
   (setq evil-insert-state-cursor '(bar . 4))
   (setq evil-cross-lines t)
   (setq evil-undo-system 'undo-fu)
   :config
+  (defalias 'evil-insert-state 'evil-emacs-state)
+  ;; (define-key evil-emacs-state-map (kbd "<escape>") 'evil-normal-state)
+
   ;; Use emacs key bindings in insert state
   (setcdr evil-insert-state-map nil)
 
@@ -41,6 +45,7 @@
   ;; Force evil-emacs-state into minor mode
   (add-hook 'org-capture-mode-hook 'evil-emacs-state)
   (add-hook 'magit-blame-mode-hook 'evil-emacs-state)
+  (add-hook 'find-file-hook 'evil-emacs-state)
 
   (defun evil-escape-or-quit (&optional prompt)
 	"If in evil state to ESC, else muhenkan key."
@@ -59,12 +64,6 @@
 	(evil-normal-state)
 	(if (use-region-p) (keyboard-quit)))
 
-  (defun my:evil-insert-state ()
-	"New files open in insert state."
-	(interactive)
-	(unless (file-exists-p buffer-file-name)
-	  (evil-insert-state)))
-
   (defun evil-swap-key (map key1 key2)
 	"Swap KEY1 and KEY2 in MAP."
 	(let ((def1 (lookup-key map key1))
@@ -77,7 +76,7 @@
   (defun ad:switch-to-buffer (&rest _arg)
 	"Set buffer for automatic insert-state."
 	(when (member (buffer-name) '("COMMIT_EDITMSG"))
-	  (evil-insert-state)))
+	  (evil-emacs-state)))
   (advice-add 'switch-to-buffer :after #'ad:switch-to-buffer)
 
   (defun chromium-vim-chert ()
