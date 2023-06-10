44e682eaea46450627cf28afc14ab47a5e30ca77
minorugh <minorugh@gmail.com>
2023-06-10 09:42:17 +0900

    Fix keybind

--- .emacs.d/inits/05_evil.el
+++ .emacs.d/inits/05_evil.el
@@ -4,9 +4,10 @@
   :ensure t
   :hook (after-init-hook . evil-mode)
   :bind ((:evil-normal-state-map
-		  ("?" . chromium-vim-chert)
 		  ("SPC" . evil-insert)
-		  ("M-." . nil))
+		  ("M-." . nil)
+		  ("?" . chromium-vim-chert)
+		  ([home] . open-dashboard))
 		 (:evil-emacs-state-map
 		  ([muhenkan] . my:evil-normal-state)
 		  ([escape] . my:evil-normal-state)))
@@ -27,7 +28,7 @@
 				  howm-mode
 				  git-timemachine-mode
 				  easy-hugo-mode))
-	(add-to-list 'evil-emacs-state-modes mode))
+    (add-to-list 'evil-emacs-state-modes mode))
 
   ;; Force evil-emacs-state into minor mode
   (add-hook 'org-capture-mode-hook 'evil-emacs-state)
@@ -36,37 +37,37 @@
 
   ;; User custom functions
   (defun my:evil-normal-state ()
-	"Turn off input-method then return to normal-state."
-	(interactive)
-	(if current-input-method (deactivate-input-method))
-	(evil-normal-state)
-	(if (use-region-p) (keyboard-quit)))
+    "Turn off input-method then return to normal-state."
+    (interactive)
+    (if current-input-method (deactivate-input-method))
+    (evil-normal-state)
+    (if (use-region-p) (keyboard-quit)))
 
   (defun my:evil-find-file ()
-	"New files open in emacs state."
-	(interactive)
-	(unless (file-exists-p buffer-file-name)
-	  (evil-emacs-state)))
+    "New files open in emacs state."
+    (interactive)
+    (unless (file-exists-p buffer-file-name)
+      (evil-emacs-state)))
 
   (defun evil-swap-key (map key1 key2)
-	"Swap KEY1 and KEY2 in MAP."
-	(let ((def1 (lookup-key map key1))
+    "Swap KEY1 and KEY2 in MAP."
+    (let ((def1 (lookup-key map key1))
 		  (def2 (lookup-key map key2)))
-	  (define-key map key1 def2)
-	  (define-key map key2 def1)))
+      (define-key map key1 def2)
+      (define-key map key2 def1)))
   (evil-swap-key evil-motion-state-map "j" "gj")
   (evil-swap-key evil-motion-state-map "k" "gk")
 
   (defun ad:switch-to-buffer (&rest _arg)
-	"Set buffer for automatic insert-state."
-	(when (member (buffer-name) '("COMMIT_EDITMSG"))
-	  (evil-emacs-state)))
+    "Set buffer for automatic insert-state."
+    (when (member (buffer-name) '("COMMIT_EDITMSG"))
+      (evil-emacs-state)))
   (advice-add 'switch-to-buffer :after #'ad:switch-to-buffer)
 
   (defun chromium-vim-chert ()
-	"Chromium vim chert sheet."
-	(interactive)
-	(browse-url "https://minorugh.github.io/emacs.d/vim-cheat.html")))
+    "Chromium vim chert sheet."
+    (interactive)
+    (browse-url "https://minorugh.github.io/emacs.d/vim-cheat.html")))
 
 
 ;; Local Variables:
