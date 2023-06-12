dd7a63239799b62420a812d4c97771cb0c218251
minorugh <minorugh@gmail.com>
2023-06-12 09:41:15 +0900

    Fix

--- .emacs.d/inits/12_dimmer.el
+++ .emacs.d/inits/12_dimmer.el
@@ -8,6 +8,7 @@
   :hook ((after-init-hook . dimmer-mode)
 		 (focus-out-hook . dimmer-off)
 		 (focus-in-hook . dimmer-on))
+  :bind ("C-q" . other-window-or-split)
   :config
   (setq dimmer-buffer-exclusion-regexps '("^ \\*which-key\\|^ \\*LV\\|^ \\*.*posframe.*buffer.*\\*$"))
   (setq dimmer-fraction 0.25)
@@ -19,66 +20,15 @@
 	(defun dimmer-on ()
       (dimmer-mode 1)
       (dimmer-process-all)))
-  ;; (add-hook 'focus-out-hook #'dimmer-off)
-  ;; (add-hook 'focus-in-hook #'dimmer-on))
-
-  ;; (defun my:toggle-dimmer ()
-  ;; 	(interactive)
-  ;; 	(unless (one-window-p)
-  ;; 	  (if (setq my:dimmer-mode (not my:dimmer-mode))
-  ;; 		  (dimmer-on)
-  ;; 		(dimmer-off))))
-  ;; (defun dimmer-off ()
-  ;; 	(dimmer-process-all)
-  ;; 	(dimmer-mode -1))
-  ;; (defun dimmer-on ()
-  ;; 	(when my:dimmer-mode
-  ;; 	  (dimmer-mode 1)
-  ;; 	  (dimmer-process-all))))
-
-
-;; Split window configuration with dimmer control
-  (leaf cus-window-command
-	:bind ("C-q" . other-window-or-split)
-	;; ("C-x 3" . my:split-window-right)
-	;; ("C-x 2" . my:split-window-below)
-	;; ("C-x 1" . my:delete-other-windows)
-	;; ("C-x 0" . my:delete-window))
-	:init
-	(defun other-window-or-split ()
-	  "If there is one window, open split window.
+  :init
+  ;; Split window configuration with dimmer control
+  (defun other-window-or-split ()
+	"If there is one window, open split window.
 If there are two or more windows, it will go to another window."
-	  (interactive)
-	  (when (one-window-p)
-		(split-window-horizontally)
-		;; (dimmer-mode 1)
-		)
-	  (other-window 1))
-
-	(defun my:split-window-right ()
-	  "With turn on dimmer."
-	  (interactive)
-	  (split-window-right)
-	  (dimmer-mode 1))
-
-	(defun my:split-window-below ()
-	  "With turn on dimmer."
-	  (interactive)
-	  (split-window-below)
-	  (dimmer-mode 1))
-
-	(defun my:delete-other-windows ()
-	  "With turn off dimmer."
-	  (interactive)
-	  (delete-other-windows)
-	  (dimmer-mode -1))
-
-	(defun my:delete-window ()
-	  "With turn off dimmer."
-	  (interactive)
-	  (delete-window)
-	  (when (one-window-p)
-		(dimmer-mode -1)))))
+	(interactive)
+	(when (one-window-p)
+	  (split-window-horizontally))
+	(other-window 1)))
 
 
 ;; Local Variables:
