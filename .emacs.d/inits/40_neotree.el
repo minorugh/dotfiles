c667da0fcddc6652123f1e0779924371f8c87e2c
minorugh <minorugh@gmail.com>
2023-06-21 11:29:58 +0900

    Fix keybind

--- .emacs.d/inits/40_neotree.el
+++ .emacs.d/inits/40_neotree.el
@@ -4,24 +4,28 @@
 ;; (setq debug-on-error t)
 
 (leaf neotree
+  :doc "Tree plugin like NerdTree for Vim"
+  :url "https://github.com/jaypei/emacs-neotree"
   :ensure t
-  :bind (("<f10>" . neotree-find)
+  :bind (("<f10>"    . neotree-find)
 		 (:neotree-mode-map
-		  ("RET" . neotree-enter-hide)
-		  ("a" . neotree-hidden-file-toggle)
-		  ("<left>" . neotree-select-up-node)
+		  ("RET"     . neotree-enter-hide)
+		  ("a"       . neotree-hidden-file-toggle)
+		  ("<left>"  . neotree-select-up-node)
 		  ("<right>" . neotree-change-root)
-		  ("<f10>" . neotree-toggle)))
+		  ("<f10>"   . neotree-toggle)))
   :custom
-  `((neo-keymap-style . 'concise)
-    (neo-create-file-auto-open . t))
+  (neo-keymap-style          . 'concise)
+  (neo-create-file-auto-open . t))
+
+(leaf all-the-icons
+  :doc "utility package to collect various Icon Fonts"
+  :url "https://github.com/domtronn/all-the-icons.el"
+  :ensure t
+  :if (display-graphic-p)
   :config
-  (leaf all-the-icons
-	:ensure t
-	:if (display-graphic-p)
-	:config
-	(unless (member "all-the-icons" (font-family-list))
-	  (all-the-icons-install-fonts t))))
+  (unless (member "all-the-icons" (font-family-list))
+	(all-the-icons-install-fonts t)))
 
 
 ;; Change neotree's font size
