5381628f3aa661084b1e7df7f0d49b53741f9199
minorugh <minorugh@gmail.com>
2023-06-22 08:55:26 +0900

    Fix doom-nano-modeline

--- .emacs.d/inits/08_ui.el
+++ .emacs.d/inits/08_ui.el
@@ -7,45 +7,33 @@
   :doc "Megapack of themes"
   :url "https://github.com/doomemacs/themes"
   :ensure t
-  ;; :hook (after-init-hook . (lambda () (load-theme 'doom-dracula t)))
-  :hook (after-init-hook . (lambda () (load-theme 'doom-nord t)))
+  :hook (after-init-hook . (lambda () (load-theme 'doom-dracula t)))
   :custom
-  `((doom-themes-enable-italic . nil)
-	(doom-themes-enable-bold . nil))
+  (doom-themes-enable-italic . nil)
+  (doom-themes-enable-bold   . nil)
   :config
   (doom-themes-neotree-config)
   (doom-themes-org-config))
 
 
-(leaf nano-modeline
-  :doc "Nice and consistent look theme"
-  :url "https://github.com/rougier/nano-emacs"
-  :el-get "rougier/nano-emacs"
-  :require nano-faces nano-modeline
-  :custom
-  (frame-background-mode . 'dark)
-  (nano-color-foreground . "#f8f8f2")
-  (nano-color-background . "#282a36")
-  (nano-color-highlight  . "#373844")
-  (nano-color-critical   . "#bd93f9")
-  (nano-color-salient    . "#0189cc")
-  (nano-color-strong     . "#e2e2dc")
-  (nano-color-popout     . "#f8f8f2")
-  (nano-color-subtle     . "#44475a")
-  (nano-color-faded      . "#6272a4")
+(leaf doom-nano-modeline
+  :doc "Modeline for Doom Emacs based on N Λ N O"
+  :url "https://github.com/ronisbr/doom-nano-modeline"
+  :el-get "ronisbr/doom-nano-modeline"
+  :hook (after-init-hook . doom-nano-modeline-mode)
   :custom-face
-  (region                    . '((t (:background "#6272a4"))))
-  (hl-line                   . '((t (:background "#3B4252" :extend t ))))
-  (vertical-border           . '((t (:background "#282a36" :foreground "#1E2029"))))
-  (mode-line                 . '((t (:background "#282a36"))))
-  (mode-line-inactive        . '((t (:background "#282a36"))))
-  (nano-face-header-salient  . '((t (:foreground "#282a36" :background "#0189cc"))))
-  (nano-face-header-popout   . '((t (:foreground "#282a36" :background "#f1fa8c"))))
-  (nano-face-header-critical . '((t (:foreground "#282a36" :background "#bd93f9"))))
-  (nano-face-header-faded    . '((t (:foreground "#282a36" :background "#6272a4"))))
-  (nano-face-subtle          . '((t (:foreground "#282a36" :background "#44475a"))))
-  (nano-face-header-default  . '((t (:foreground "#b0b8d1" :background "#44475a"))))
-  (nano-face-header-strong   . '((t (:foreground "#f8f8f2" :background "#44475a" :weight bold)))))
+  (region
+   . '((t (:background "#6272a4"))))
+  (hl-line
+   . '((t (:background "#3B4252" :extend t ))))
+  (doom-nano-modeline-active-face            . '((t (:inherit mode-line :background "#44475a" :weight bold))))
+  (doom-nano-modeline-cursor-position-face
+   . '((t (:foreground "#b0b8d1" :background "#44475a"))))
+  (doom-nano-modeline-evil-emacs-state-face  . '((t (:foreground "#ff7f50" :background "#6272a4" :weight bold))))
+  (doom-nano-modeline-evil-normal-state-face . '((t (:foreground "#adff2f" :background "#4682b4" :weight bold))))
+  (doom-nano-modeline-evil-visual-state-face . '((t (:foreground "#282a36" :background "#7383b5" :weight bold))))
+  (doom-nano-modeline-major-mode-face        . '((t (:foreground "#b0b8d1" :background "#44475a"))))
+  (doom-nano-modeline-vc-branch-name-face    . '((t (:foreground "#b0b8d1" :background "#44475a")))))
 
 
 (leaf cus-line-spacing
