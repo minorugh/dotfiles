53a1fc1201b02616b9665585bf573e83daf5b261
minorugh <minorugh@gmail.com>
2023-06-22 11:22:54 +0900

    Fix server-start

--- .emacs.d/inits/00_base.el
+++ .emacs.d/inits/00_base.el
@@ -12,58 +12,58 @@
 (leaf *generic-configurations
   :custom
   `(;; No startup screen appears
-	(inhibit-splash-screen . t)
-	;; Faster rendering by not corresponding to right-to-left language
-	(bidi-display-reordering . nil)
-	;; Do not make a backup file like *.~
-	(make-backup-files . nil)
-	;; Do not use auto save
-	(auto-save-default . nil)
-	(auto-save-list-file-prefix . nil)
-	;; Do not create lock file
-	(create-lockfiles . nil)
-	;; Open symbolic link directly
-	(vc-follow-symlinks . t)
-	;; Do not distinguish uppercase and lowercase letters on completion
-	(completion-ignore-case . t)
-	(read-file-name-completion-ignore-case . t)
-	;; Point keeps its screen position when scroll
-	(scroll-preserve-screen-position . t)
-	;; All warning sounds and flash are invalid
-	(ring-bell-function . 'ignore)
-	;; Turn off warning sound screen flash
-	(visible-bell . nil)
-	;; Copy text with mouse range selection
-	(mouse-drag-copy-region . t)
-	;; Deleted files go to the trash
-	(delete-by-moving-to-trash . t)
-	;; Tab width default
-	(tab-width . 4)
-	;; Limit the final word to a line break code (automatically correct)
-	(require-final-newline . t)
-	;; Disallow adding new lines with newline at the end of the buffer
-	(next-line-add-newlines . nil)
-	;; Make it easy to see when it is the same name file
-	(uniquify-buffer-name-style . 'post-forward-angle-brackets)
-	;; It keeps going steadily the local mark ...  C-u C-SPC C-SPC
-	;; It keeps going steadily the global mark ... C-x C-SPC C-SPC
-	(set-mark-command-repeat-pop . t)
-	;; Use the X11 clipboard
-	(select-enable-clipboard  . t)
-	;; change-default-file-location
-	(url-configuration-directory . "~/.emacs.d/tmp/url")
-	(bookmark-file . "~/.emacs.d/tmp/bookmarks"))
+    (inhibit-splash-screen . t)
+    ;; Faster rendering by not corresponding to right-to-left language
+    (bidi-display-reordering . nil)
+    ;; Do not make a backup file like *.~
+    (make-backup-files . nil)
+    ;; Do not use auto save
+    (auto-save-default . nil)
+    (auto-save-list-file-prefix . nil)
+    ;; Do not create lock file
+    (create-lockfiles . nil)
+    ;; Open symbolic link directly
+    (vc-follow-symlinks . t)
+    ;; Do not distinguish uppercase and lowercase letters on completion
+    (completion-ignore-case . t)
+    (read-file-name-completion-ignore-case . t)
+    ;; Point keeps its screen position when scroll
+    (scroll-preserve-screen-position . t)
+    ;; All warning sounds and flash are invalid
+    (ring-bell-function . 'ignore)
+    ;; Turn off warning sound screen flash
+    (visible-bell . nil)
+    ;; Copy text with mouse range selection
+    (mouse-drag-copy-region . t)
+    ;; Deleted files go to the trash
+    (delete-by-moving-to-trash . t)
+    ;; Tab width default
+    (tab-width . 4)
+    ;; Limit the final word to a line break code (automatically correct)
+    (require-final-newline . t)
+    ;; Disallow adding new lines with newline at the end of the buffer
+    (next-line-add-newlines . nil)
+    ;; Make it easy to see when it is the same name file
+    (uniquify-buffer-name-style . 'post-forward-angle-brackets)
+    ;; It keeps going steadily the local mark ...  C-u C-SPC C-SPC
+    ;; It keeps going steadily the global mark ... C-x C-SPC C-SPC
+    (set-mark-command-repeat-pop . t)
+    ;; Use the X11 clipboard
+    (select-enable-clipboard  . t)
+    ;; change-default-file-location
+    (url-configuration-directory . "~/.emacs.d/tmp/url")
+    (bookmark-file . "~/.emacs.d/tmp/bookmarks"))
   :config
   (defalias 'exit 'save-buffers-kill-emacs)
   (defalias 'yes-or-no-p 'y-or-n-p)
   (defun my:default-modes ()
-	"Set default modes for startup hook."
-	(interactive)
-	(winner-mode 1)
-	(global-auto-revert-mode 1)
-	(global-font-lock-mode 1)
-	(global-hl-line-mode 1)
-	(global-visual-line-mode 1))
+    "Set default modes for startup hook."
+    (interactive)
+    (winner-mode 1)
+    (global-auto-revert-mode 1)
+    (global-font-lock-mode 1)
+    (global-hl-line-mode 1)
+    (global-visual-line-mode 1))
   (add-hook 'after-init-hook 'my:default-modes)
 
   ;; Save the file specified code with basic utf-8 if it exist
@@ -72,22 +72,25 @@
 
   ;; Set font for main machine or other
   (if (string-match "e590" (shell-command-to-string "uname -n"))
-	  (add-to-list 'default-frame-alist '(font . "Cica-18"))
-	(add-to-list 'default-frame-alist '(font . "Cica-15")))
+      (add-to-list 'default-frame-alist '(font . "Cica-18"))
+    (add-to-list 'default-frame-alist '(font . "Cica-15")))
 
   :init
   (leaf exec-path-from-shell
-	:doc "Share PATH from shell environment variables"
-	:url "https://github.com/purcell/exec-path-from-shell"
-	:ensure t
-	:when (memq window-system '(mac ns x))
-	:hook (after-init-hook . exec-path-from-shell-initialize)
-	:custom
-	(exec-path-from-shell-check-startup-files . nil))
+    :doc "Share PATH from shell environment variables"
+    :url "https://github.com/purcell/exec-path-from-shell"
+    :ensure t
+    :when (memq window-system '(mac ns x))
+    :hook (after-init-hook . exec-path-from-shell-initialize)
+    :custom
+    (exec-path-from-shell-check-startup-files . nil))
 
-  (leaf *server
-	:doc "Use Emacs as a Server"
-	:hook (after-init-hook . server-mode))
+  (leaf *server-start
+    :doc "Server start for emacs-client"
+	:require server
+    :config
+    (unless (server-running-p)
+	  (add-hook 'after-init-hook 'server-start)))
 
   (leaf *recovery
 	:doc "Save place of cursor"
@@ -99,7 +102,7 @@
 	:hook (after-init-hook . savehist-mode)
 	:custom
 	`((savehist-file . "~/.emacs.d/tmp/history")
-	  (savehist-additional-variables . '(kill-ring))))
+      (savehist-additional-variables . '(kill-ring))))
 
   ;; recentf
   (leaf *recentf
@@ -107,9 +110,9 @@
 	:hook (after-init-hook . recentf-mode)
 	:custom
 	`((recentf-auto-cleanup . 'never)
-	  (recentf-exclude
-	   . '("\\.howm-keys" "Dropbox/backup" ".emacs.d/tmp/" ".emacs.d/elpa/" "/scp:"))
-	  (recentf-save-file . "~/.emacs.d/tmp/recentf"))))
+      (recentf-exclude
+       . '("\\.howm-keys" "Dropbox/backup" ".emacs.d/tmp/" ".emacs.d/elpa/" "/scp:"))
+      (recentf-save-file . "~/.emacs.d/tmp/recentf"))))
 
 
 ;; -----------------------------------------------------------------------------------------
