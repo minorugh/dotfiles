f6b3fe589c3082dd0ae27f4e9e3dfc5d9db4ec43
minorugh <minorugh@gmail.com>
2023-06-23 17:40:28 +0900

    Refactor

--- .emacs.d/inits/40_neotree.el
+++ .emacs.d/inits/40_neotree.el
@@ -3,6 +3,16 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
+(leaf all-the-icons
+  :doc "utility package to collect various Icon Fonts"
+  :url "https://github.com/domtronn/all-the-icons.el"
+  :ensure t
+  :if (display-graphic-p)
+  :config
+  (unless (member "all-the-icons" (font-family-list))
+	(all-the-icons-install-fonts t)))
+
+
 (leaf neotree
   :doc "Tree plugin like NerdTree for Vim"
   :url "https://github.com/jaypei/emacs-neotree"
@@ -23,43 +33,32 @@
   (defun neo-hide-nano-header ()
     "Hide nano header."
     (interactive)
-    (setq header-line-format "")))
+    (setq header-line-format ""))
 
+  ;; Change neotree's font size
+  ;; Tips from https://github.com/jaypei/emacs-neotree/issues/218
+  (defun neotree-text-scale ()
+	"Neotree text scale."
+	(interactive)
+	(text-scale-adjust 0)
+	(text-scale-decrease 0.8)
+	(message nil))
+  (add-hook 'neo-after-create-hook
+			(lambda (_)
+			  (call-interactively 'neotree-text-scale)))
 
-(leaf all-the-icons
-  :doc "utility package to collect various Icon Fonts"
-  :url "https://github.com/domtronn/all-the-icons.el"
-  :ensure t
-  :if (display-graphic-p)
-  :config
-  (unless (member "all-the-icons" (font-family-list))
-	(all-the-icons-install-fonts t)))
+  ;; neotree enter hide
+  ;; Tips from https://github.com/jaypei/emacs-neotree/issues/77
+  (defun neo-open-file-hide (full-path &optional arg)
+	"Neotree enter hide with FULL-PATH ARG."
+	(neo-global--select-mru-window arg)
+	(find-file full-path)
+	(neotree-hide))
 
-
-;; Change neotree's font size
-;; Tips from https://github.com/jaypei/emacs-neotree/issues/218
-(defun neotree-text-scale ()
-  "Neotree text scale."
-  (interactive)
-  (text-scale-adjust 0)
-  (text-scale-decrease 0.8)
-  (message nil))
-(add-hook 'neo-after-create-hook
-		  (lambda (_)
-			(call-interactively 'neotree-text-scale)))
-
-;; neotree enter hide
-;; Tips from https://github.com/jaypei/emacs-neotree/issues/77
-(defun neo-open-file-hide (full-path &optional arg)
-  "Neotree enter hide with FULL-PATH ARG."
-  (neo-global--select-mru-window arg)
-  (find-file full-path)
-  (neotree-hide))
-
-(defun neotree-enter-hide (&optional arg)
-  "Neotree enter hide with ARG."
-  (interactive "P")
-  (neo-buffer--execute arg 'neo-open-file-hide 'neo-open-dir))
+  (defun neotree-enter-hide (&optional arg)
+	"Neotree enter hide with ARG."
+	(interactive "P")
+	(neo-buffer--execute arg 'neo-open-file-hide 'neo-open-dir)))
 
 
 ;; Local Variables:
