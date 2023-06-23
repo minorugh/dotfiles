9086bb6adab4a5265daec1ec4835be96e3f3fe01
minorugh <minorugh@gmail.com>
2023-06-23 17:40:08 +0900

    Fix dashboard-items

--- .emacs.d/inits/01_dashboard.el
+++ .emacs.d/inits/01_dashboard.el
@@ -35,7 +35,7 @@
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t)
   (setq show-week-agenda-p t)
-  (setq dashboard-items '((recents  . 5)
+  (setq dashboard-items '((recents  . 8)
 						  (agenda   . 5)))
 
   ;; Set the footer
