92b6a171eef8edeb38de413f084af2f1d5cdba41
minorugh <minorugh@gmail.com>
2023-06-24 11:49:22 +0900

    Add pangu-spacing

--- .emacs.d/inits/06_mozc.el
+++ .emacs.d/inits/06_mozc.el
@@ -75,6 +75,19 @@
 	(delete-other-windows)))
 
 
+(leaf pangu-spacing
+  :doc "Put a space between Japanese and English"
+  :url "http://github.com/coldnew/pangu-spacing"
+  :ensure t
+  :hook ((markdown-mode-hook text-mode-hook) . pangu-spacing-mode)
+  :config
+  (setq pangu-spacing-real-insert-separtor t)
+  (setq pangu-spacing-include-regexp ;; alphabet only
+		(rx (or (and (or (group-n 3 (any "。，！？；：「」（）、"))
+						 (group-n 1 (or (category japanese))))))
+			(group-n 2 (in "a-zA-Z")))))
+
+
 ;; ---------------------------------------------------------------------
 ;;
 ;; Every time you start emacs on the submachine,
