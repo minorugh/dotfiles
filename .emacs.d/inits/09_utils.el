89c55e5b8801e96e617895999aaa8e20199b1619
minorugh <minorugh@gmail.com>
2023-06-24 22:47:03 +0900

    Fix

--- .emacs.d/inits/09_utils.el
+++ .emacs.d/inits/09_utils.el
@@ -106,6 +106,32 @@
 	(migemo-dictionary . "/usr/share/cmigemo/utf-8/migemo-dict")))
 
 
+(leaf *cus-counsel-ag
+  :doc "Fast full-text search"
+  :url "https://takaxp.github.io/init.html#org29c7b6b7"
+  :init
+  (defun ad:counsel-ag (f &optional initial-input initial-directory extra-ag-args ag-prompt caller)
+	(apply f (or initial-input
+				 (and (not (thing-at-point-looking-at "^\\*+"))
+					  (ivy-thing-at-point)))
+		   (unless current-prefix-arg
+			 (or initial-directory default-directory))
+		   extra-ag-args ag-prompt caller))
+  (with-eval-after-load "counsel"
+	(require 'thingatpt nil t)
+	(advice-add 'counsel-ag :around #'ad:counsel-ag)
+	;; Make search trigger even with 2 characters
+	(add-to-list 'ivy-more-chars-alist '(counsel-ag . 2))
+	(ivy-add-actions
+	 'counsel-ag
+	 '(("r" my:counsel-ag-in-dir "search in directory")))
+
+	(defun my:counsel-ag-in-dir (_arg)
+	  "Search again with new root directory."
+	  (let ((current-prefix-arg '(4)))
+		(counsel-ag ivy-text nil "")))))
+
+
 (leaf *cus-ps-print
   :doc "Print from Emacs via Postscript"
   :url "https://tam5917.hatenablog.com/entry/20120914/1347600433"
