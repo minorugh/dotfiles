91c31a2356ced1036adddfb55709a346f7acd14d
minorugh <minorugh@gmail.com>
2023-06-26 19:12:04 +0900

    Fix

--- .emacs.d/inits/06_mozc.el
+++ .emacs.d/inits/06_mozc.el
@@ -43,6 +43,7 @@
   (leaf pangu-spacing
 	:doc "Put a space between Japanese and English"
 	:url "http://github.com/coldnew/pangu-spacing"
+	:ensure t
 	:hook ((markdown-mode-hook text-mode-hook) . pangu-spacing-mode)
 	:config
 	(setq pangu-spacing-real-insert-separtor t)
