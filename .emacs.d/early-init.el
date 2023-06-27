c431c3de72a454d4794066b6a29c68e599eb2c60
minorugh <minorugh@gmail.com>
2023-06-27 22:24:41 +0900

    Refactor

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -28,10 +28,10 @@
 
 
 ;; Faster to disable these here (before they've been initialized)
+(push '(fullscreen . maximized) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-(push '(fullscreen . maximized) default-frame-alist)
 
 
 ;; Suppress flashing at startup
