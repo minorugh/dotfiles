e083804cbdf3064edf47f6ec55a0d1b9d4dfab25
minorugh <minorugh@gmail.com>
2023-06-29 15:24:31 +0900

    Fix

--- .emacs.d/inits/03_counsel.el
+++ .emacs.d/inits/03_counsel.el
@@ -3,28 +3,18 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(leaf counsel
-  :doc "Various completion functions using Ivy"
+;; -------------------------------------------------------------------
+;;
+;; completion functions using Ivy
+;;
+;; -------------------------------------------------------------------
+
+(leaf swiper
+  :doc "Isearch with an overview. Oh, man!"
   :url "https://github.com/abo-abo/swiper"
   :ensure t
-  :hook (after-init-hook . ivy-mode)
-  :bind (([remap isearch-forward]  . swiper-region)
-		 ([remap isearch-backward] . swiper-thing-at-point)
-		 ("C-:"     . counsel-switch-buffer)
-		 ("M-:"     . counsel-switch-buffer)
-		 ("M-x"     . counsel-M-x)
-		 ("M-y"     . counsel-yank-pop)
-		 ("C-x m"   . counsel-mark-ring)
-		 ("C-x C-b" . ibuffer)
-		 ("C-x C-f" . counsel-find-file)
-		 ("C-x C-r" . counsel-recentf))
-  :custom
-  `((search-default-mode             . nil)
-	(ivy-use-virtual-buffers         . t)
-	(ivy-use-selectable-prompt       . t)
-	(enable-recursive-minibuffers    . t)
-	(counsel-find-file-ignore-regexp . (regexp-opt completion-ignored-extensions))
-	(ivy-format-functions-alist      . '((t . my:ivy-format-function-arrow))))
+  :bind (("C-s" . swiper-region)
+		 ("C-r" . swiper-thing-at-point))
   :config
   (defun swiper-region ()
 	"If region is selected, `swiper-thing-at-point'.
@@ -33,31 +23,7 @@ If the region isn't selected, `swiper'."
 	(if (not (use-region-p))
 		(swiper)
 	  (swiper-thing-at-point)))
-
-  (defun my:ivy-format-function-arrow (cands)
-	"Transform into a string for minibuffer with CANDS."
-	(ivy--format-function-generic
-	 (lambda (str)
-	   (concat (if (display-graphic-p)
-				   "")
-			   (propertize " " 'display `(space :align-to 2))
-			   (ivy--add-face str 'ivy-current-match)))
-	 (lambda (str)
-	   (concat (propertize " " 'display `(space :align-to 2)) str))
-	 cands
-	 "\n"))
   :init
-  (leaf ivy-rich
-	:doc "More friendly display transformer for ivy"
-	:url "https://github.com/Yevgnen/ivy-rich"
-	:ensure t
-	:hook (after-init-hook . ivy-rich-mode))
-
-  (leaf amx
-	:ensure t
-	:custom `((amx-save-file . ,"~/.emacs.d/tmp/amx-items")
-			  (amx-history-length . 20)))
-
   (leaf swiper-migemo
 	:doc "Use ivy/counsel/swiper with migemo"
 	:url "https://github.com/tam17aki/swiper-migemo"
@@ -79,6 +45,51 @@ If the region isn't selected, `swiper'."
 	:custom
 	`((migemo-command    . "cmigemo")
 	  (migemo-dictionary . "/usr/share/cmigemo/utf-8/migemo-dict"))))
+
+
+(leaf counsel
+  :doc "Various completion functions using Ivy"
+  :url "https://github.com/abo-abo/swiper"
+  :ensure t
+  :hook (after-init-hook . ivy-mode)
+  :bind (("C-:"     . counsel-switch-buffer)
+		 ("M-:"     . counsel-switch-buffer)
+		 ("M-x"     . counsel-M-x)
+		 ("M-y"     . counsel-yank-pop)
+		 ("C-x m"   . counsel-mark-ring)
+		 ("C-x C-b" . ibuffer)
+		 ("C-x C-f" . counsel-find-file)
+		 ("C-x C-r" . counsel-recentf))
+  :custom
+  `((search-default-mode             . nil)
+	(ivy-use-virtual-buffers         . t)
+	(ivy-use-selectable-prompt       . t)
+	(enable-recursive-minibuffers    . t)
+	(counsel-find-file-ignore-regexp . (regexp-opt completion-ignored-extensions))
+	(ivy-format-functions-alist      . '((t . my:ivy-format-function-arrow))))
+  :init
+  (leaf ivy-rich
+	:doc "More friendly display transformer for ivy"
+	:url "https://github.com/Yevgnen/ivy-rich"
+	:ensure t
+	:hook (after-init-hook . ivy-rich-mode))
+  (leaf amx
+	:ensure t
+	:custom `((amx-save-file . ,"~/.emacs.d/tmp/amx-items")
+			  (amx-history-length . 20)))
+  :config
+  (defun my:ivy-format-function-arrow (cands)
+	"Transform into a string for minibuffer with CANDS."
+	(ivy--format-function-generic
+	 (lambda (str)
+	   (concat (if (display-graphic-p)
+				   "")
+			   (propertize " " 'display `(space :align-to 2))
+			   (ivy--add-face str 'ivy-current-match)))
+	 (lambda (str)
+	   (concat (propertize " " 'display `(space :align-to 2)) str))
+	 cands
+	 "\n")))
 
 
 ;; Local Variables:
