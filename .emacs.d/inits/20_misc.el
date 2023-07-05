b3195c602972050db8f20c7575fb3895b1cbe608
minorugh <minorugh@gmail.com>
2023-07-05 14:52:05 +0900

    Fix

--- .emacs.d/inits/20_misc.el
+++ .emacs.d/inits/20_misc.el
@@ -33,13 +33,6 @@
   :hook ((emacs-lisp-mode-hook css-mode-hook) . aggressive-indent-mode))
 
 
-(leaf quickrun
-  :doc "Qick executes editing buffer"
-  :url "https://github.com/emacsorphanage/quickrun"
-  :bind ([f6]  . quickrun)
-  :ensure t)
-
-
 (leaf restart-emacs
   :doc "Restart emacs from within emacs"
   :url "https://github.com/iqbalansari/restart-emacs"
@@ -51,6 +44,24 @@
   :doc "Open root parmission files as sudo user"
   :url "https://github.com/nflath/sudo-edit"
   :ensure t)
+
+
+(leaf which-key
+  :doc "Displays available keybindings in popup"
+  :url "https://github.com/justbur/emacs-which-key"
+  :ensure t
+  :hook (after-init-hook . which-key-mode)
+  :custom ((which-key-max-description-length . 40)))
+
+
+(leaf key-chord
+  :doc "Mapping a pair of simultaneously pressed keys"
+  :url "https://github.com/emacsorphanage/key-chord"
+  :ensure t
+  :hook (after-init-hook . key-chord-mode)
+  :chord (("df" . counsel-descbinds)
+		  ("l;" . init-loader-show-log))
+  :custom (key-chord-two-keys-delay . 0.1))
 
 
 ;; Local Variables:
