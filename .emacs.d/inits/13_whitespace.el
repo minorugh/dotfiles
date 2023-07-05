2013daf126b0dd1dd6633e054a9f33a8061511d1
minorugh <minorugh@gmail.com>
2023-07-05 09:47:15 +0900

    New file

---
+++ .emacs.d/inits/13_whitespace.el
@@ -0,0 +1,31 @@
+;;; 13_whitespace.el --- Witespace configurations. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+;; (setq debug-on-error t)
+
+(leaf whitespace
+  :doc "Highligh trailing whitespace"
+  :hook (prog-mode-hook . my:enable-trailing-mode)
+  :bind ("C-c C-c" . my:cleanup-for-spaces)
+  :custom
+  (show-trailing-whitespace . nil)
+  :init
+  (defun my:enable-trailing-mode ()
+	"Show tail whitespace."
+	(setq show-trailing-whitespace t))
+
+  (defun my:cleanup-for-spaces ()
+	"Remove contiguous line breaks at end of line + end of file."
+	(interactive)
+	(delete-trailing-whitespace)
+	(save-excursion
+	  (save-restriction
+		(widen)
+		(goto-char (point-max))
+		(delete-blank-lines)))))
+
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+;;; 13_whitespace.el ends here
