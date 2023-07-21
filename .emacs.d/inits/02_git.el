a970a6a0eb67e73e38b3579aa743fc46b97c97be
minorugh <minorugh@gmail.com>
2023-07-22 07:39:46 +0900

    Fix

--- .emacs.d/inits/02_git.el
+++ .emacs.d/inits/02_git.el
@@ -4,72 +4,56 @@
 ;; (setq debug-on-erro t)
 
 (leaf diff-hl
+  :doc "Highlight uncommitted changes"
+  :url "https://github.com/dgutov/diff-hl"
   :ensure t
   :init
   (add-hook 'after-init-hook 'global-diff-hl-mode)
   (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
-  (unless (window-system) (diff-hl-margin-mode))
   :config
   (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 
+(leaf git-timemachine
+  :doc "Git time machine"
+  :url "https://gitlab.com/pidu/git-timemachine"
+  :ensure t)
+
+
+(leaf browse-at-remote
+  :doc "Open github page from Emacs"
+  :url "https://github.com/rmuslimov/browse-at-remote"
+  :ensure t
+  :custom	(browse-at-remote-prefer-symbolic . nil))
+
+
 (leaf magit
+  :doc "A Git Porcelain inside Emacs"
+  :url "https://github.com/magit/magit"
   :ensure t
   :bind ("C-x g" . magit-status)
   :custom
   (transient-history-file . "~/.emacs.d/tmp/transient-history")
   ;; Do not split window
-  (magit-display-buffer-function . 'magit-display-buffer-fullframe-status-v1)
-  :init
-  (leaf git-timemachine	:ensure t)
-  (leaf browse-at-remote
-	:ensure t
-	:custom	(browse-at-remote-prefer-symbolic . nil))
+  (magit-display-buffer-function . 'magit-display-buffer-fullframe-status-v1))
+
+
+(leaf *gitk
+  :doc ""
+  :url ""
   :config
   (defun gitk-open ()
-    "Open gitk with current dir."
-    (interactive)
-    (shell-command "gitk &")
-    (delete-other-windows))
+	"Open gitk with current dir."
+	(interactive)
+	(shell-command "gitk &")
+	(delete-other-windows))
 
   (defun git-gui-open ()
-    "Open gitk with current dir."
-    (interactive)
-    (shell-command "git gui &")
-    (delete-other-windows)))
-
-
-(leaf *define-gist-commands
-  :doc "Gist upload from current buffer or region"
-  :config
-  (defun gist-description ()
-    "Add gist description."
-    (shell-quote-argument (read-from-minibuffer "Add gist description: ")))
-
-  (defun gist-filename ()
-    "The character string entered in minibuffer is used as file-name.
-If enter is pressed without file-name, that's will be buffer-file-neme."
-    (interactive)
-    (let ((file (file-name-nondirectory (buffer-file-name (current-buffer)))))
-      (read-from-minibuffer (format "File name (%s): " file) file)))
-
-  (defun gist-region-or-buffer ()
-    "If region is selected, post from the region.
-If region isn't selected, post from the buffer."
-    (interactive)
-    (let ((file (buffer-file-name)))
-      (if (not (use-region-p))
-		  (compile (concat "gist -od " (gist-description) " " file))
-		(compile (concat "gist -oPd " (gist-description) " -f " (gist-filename)))))
-    (delete-other-windows))
-
-  (defun dired-do-gist ()
-    "Dired-get-filename do gist and open in browser."
-    (interactive)
-    (let ((file (dired-get-filename nil t)))
-      (compile (concat "gist -od " (gist-description) " " file)))
-    (delete-other-windows)))
+	"Open gitk with current dir."
+	(interactive)
+	(shell-command "git gui &")
+	(delete-other-windows)))
 
 
 ;; Local Variables:
