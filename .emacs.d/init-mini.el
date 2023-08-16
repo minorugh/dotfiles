735db19954652e715b7321927fa810c7ec46dae3
minorugh <minorugh@gmail.com>
2023-08-17 07:05:16 +0900

    Fix font-size

--- .emacs.d/init-mini.el
+++ .emacs.d/init-mini.el
@@ -26,7 +26,7 @@
 (prefer-coding-system 'utf-8)
 
 ;; font
-(add-to-list 'default-frame-alist '(font . "Cica-20"))
+(add-to-list 'default-frame-alist '(font . "Cica-21"))
 
 ;;; Faster rendering by not corresponding to right-to-left language
 (setq-default bidi-display-reordering nil)
