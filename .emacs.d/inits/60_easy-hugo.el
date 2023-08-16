8c01629190866a99958586210eb4ad80a3d82a6e
minorugh <minorugh@gmail.com>
2023-08-17 07:05:04 +0900

    Fix

--- .emacs.d/inits/60_easy-hugo.el
+++ .emacs.d/inits/60_easy-hugo.el
@@ -20,47 +20,9 @@
   (setq easy-hugo-sshdomain "xsrv")
   (setq easy-hugo-root "/home/minorugh/minorugh.com/public_html/snap/")
   (setq easy-hugo-previewtime "300")
-  ;; Load other blog settings & custom configuration for evil-mode
-  (setq easy-hugo-bloglist
-		'(;; blog2 setting
-		  ((easy-hugo-basedir . "~/src/github.com/minorugh/emacs.d/hugo/")
-		   (easy-hugo-url . "https://minorugh.github.io/emacs.d")
-		   (easy-hugo-postdir . "content/startup")
-		   (easy-hugo-preview-url . "http://localhost:1313/emacs.d/"))
-		  ;; blog3 setting
-		  ((easy-hugo-basedir . "~/src/github.com/minorugh/minorugh.github.io/")
-		   (easy-hugo-url . "https://minorugh.github.io")
-		   (easy-hugo-postdir . "content/posts"))
-		  ;; blog4 setting
-		  ((easy-hugo-basedir . "~/Dropbox/GH/gg/")
-		   (easy-hugo-url . "https://gg.gospel-haiku.com")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/gospel-haiku.com/public_html/gg/"))
-		  ;; blog5 setting
-		  ((easy-hugo-basedir . "~/Dropbox/GH/blog/")
-		   (easy-hugo-url . "https://blog.gospel-haiku.com")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/gospel-haiku.com/public_html/blog/"))
-		  ;; blog6 setting
-		  ((easy-hugo-basedir . "~/Dropbox/GH/es/")
-		   (easy-hugo-url . "https://es.gospel-haiku.com")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/gospel-haiku.com/public_html/es/"))
-		  ;; blog7 setting
-		  ((easy-hugo-basedir . "~/Dropbox/myama.site/bible/")
-		   (easy-hugo-url . "https://bible.myama.site")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/myama.site/public_html/bible/"))
-		  ;; blog8 setting
-		  ((easy-hugo-basedir . "~/Dropbox/myama.site/tube/")
-		   (easy-hugo-url . "https://tube.myama.site")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/myama.site/public_html/tube/"))
-		  ;; blog9 setting
-		  ((easy-hugo-basedir . "~/Dropbox/myama.site/ryo/")
-		   (easy-hugo-url . "https://ryo.myama.site")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/myama.site/public_html/ryo/"))))
+  ;; Load temolate for other blog settings
+  (require 'my:bloglist)
+  :config
   ;; Customize for my help menu
   (setq easy-hugo-help-line 5
 		easy-hugo-help "
@@ -70,7 +32,7 @@
   , .. Prev postdir     . .. Next postdir    ; .. Select blog      v .. Open view mode
   N .. No help [tab]    s .. Sort time       u .. Sort Publish     e .. Edit easy-hugo
 ")
-  :config
+  ;; Load custom configuration for evil-mode
   (require 'evil-easy-hugo)
   (leaf popup :ensure t)
   (leaf request
