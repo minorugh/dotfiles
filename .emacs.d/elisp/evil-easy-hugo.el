e047c02f013f525fe507c2e43232d2388d224436
minorugh <minorugh@gmail.com>
2023-08-16 10:45:14 +0900

    rename

--- .emacs.d/elisp/evil-easy-hugo.el
+++ .emacs.d/elisp/evil-easy-hugo.el
@@ -1,4 +1,4 @@
-;;; cus-easy-hugo.el --- User custom configurations for evil-mode. -*- lexical-binding: t -*-
+;;; evi-easy-hugo.el --- User custom configurations for evil-mode. -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
 ;; Rewrite easy-hugo-newpost configuration for `evil-mode'.
@@ -56,55 +56,11 @@ POST-FILE needs to have and extension '.md' or '.org' or '.ad' or '.rst' or '.mm
 				  (string-equal file-ext "org"))
 		 (insert (easy-hugo--org-headers (file-name-base post-file))))
 	   (goto-char (point-max))
-	   (save-buffer))))
+	   (save-buffer)))))
 
 
-  (setq easy-hugo-bloglist
-		'(;; blog2 setting
-		  ((easy-hugo-basedir . "~/src/github.com/minorugh/emacs.d/hugo/")
-		   (easy-hugo-url . "https://minorugh.github.io/emacs.d")
-		   (easy-hugo-postdir . "content/startup")
-		   (easy-hugo-preview-url . "http://localhost:1313/emacs.d/"))
-		  ;; blog3 setting
-		  ((easy-hugo-basedir . "~/src/github.com/minorugh/minorugh.github.io/")
-		   (easy-hugo-url . "https://minorugh.github.io")
-		   (easy-hugo-postdir . "content/posts"))
-		  ;; blog4 setting
-		  ((easy-hugo-basedir . "~/Dropbox/GH/gg/")
-		   (easy-hugo-url . "https://gg.gospel-haiku.com")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/gospel-haiku.com/public_html/gg/"))
-		  ;; blog5 setting
-		  ((easy-hugo-basedir . "~/Dropbox/GH/blog/")
-		   (easy-hugo-url . "https://blog.gospel-haiku.com")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/gospel-haiku.com/public_html/blog/"))
-		  ;; blog6 setting
-		  ((easy-hugo-basedir . "~/Dropbox/GH/es/")
-		   (easy-hugo-url . "https://es.gospel-haiku.com")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/gospel-haiku.com/public_html/es/"))
-		  ;; blog7 setting
-		  ((easy-hugo-basedir . "~/Dropbox/myama.site/bible/")
-		   (easy-hugo-url . "https://bible.myama.site")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/myama.site/public_html/bible/"))
-		  ;; blog8 setting
-		  ((easy-hugo-basedir . "~/Dropbox/myama.site/tube/")
-		   (easy-hugo-url . "https://tube.myama.site")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/myama.site/public_html/tube/"))
-		  ;; blog9 setting
-		  ((easy-hugo-basedir . "~/Dropbox/myama.site/ryo/")
-		   (easy-hugo-url . "https://ryo.myama.site")
-		   (easy-hugo-sshdomain . "xsrv")
-		   (easy-hugo-root . "/home/minorugh/myama.site/public_html/ryo/"))))
-
-  )
-
-
-(provide 'cus-easy-hugo)
+(provide 'evil-easy-hugo)
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars)
 ;; End:
-;;; cus-easy-hugo.el ends here
+;;; evil-easy-hugo.el ends here
