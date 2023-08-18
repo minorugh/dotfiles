bd415ace12c76af29b15a5f016d67ed89b1fff23
minorugh <minorugh@gmail.com>
2023-08-18 09:57:19 +0900

    Add server

--- .emacs.d/inits/00_base.el
+++ .emacs.d/inits/00_base.el
@@ -57,6 +57,11 @@
 	:custom
 	(exec-path-from-shell-check-startup-files . nil))
 
+  ;; Server Start
+  (eval-and-compile (require 'server))
+  (unless (server-running-p)
+	(server-start))
+
   ;; Change to short command
   (defalias 'yes-or-no-p #'y-or-n-p)
   (defalias 'exit 'save-buffers-kill-emacs)
@@ -67,7 +72,7 @@
 
   ;; Fonts
   (if (string-match "e590" (shell-command-to-string "uname -n"))
-	  (add-to-list 'default-frame-alist '(font . "Cica-18"))
+	  (add-to-list 'default-frame-alist '(font . "Cica-20"))
 	(add-to-list 'default-frame-alist '(font . "Cica-15")))
 
   ;; Recentf
