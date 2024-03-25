c5bd60eec7c1dee1e92b18018ca065215aacc25f
minorugh <minorugh@gmail.com>
2024-03-25 16:01:32 +0900

    Fix

--- .emacs.d/inits/20_make.el
+++ .emacs.d/inits/20_make.el
@@ -3,64 +3,62 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(leaf *cus-compilation
-  :config
-  (add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
-  (setq compilation-scroll-output t)
-  (setq compilation-always-kill t)
-  (setq compilation-finish-functions 'compile-autoclose)
-  (defun compile-autoclose (buffer string)
-	"Automatically close the compilation."
-	(cond ((string-match "compilation" (buffer-name buffer))
-		   (string-match "finished" string)
-		   (delete-other-windows)
-		   (message "Compile successful."))
-		  (t (message "Compilation exited abnormally: %s" string)))))
+;; custom-compilation
+(add-to-list 'auto-mode-alist '("\\.mak\\'" . makefile-mode))
+(setq compilation-scroll-output t)
+(setq compilation-always-kill t)
+(setq compilation-finish-functions 'compile-autoclose)
+(defun compile-autoclose (buffer string)
+  "Automatically close the compilation."
+  (cond ((string-match "compilation" (buffer-name buffer))
+		 (string-match "finished" string)
+		 (delete-other-windows)
+		 (message "Compile successful."))
+		(t (message "Compilation exited abnormally: %s" string))))
 
 
-(leaf *make-function
-  :config
-  (defun my:make-k ()
-	"Make k."
-	(interactive)
-	(compile "make -k"))
+;; user make function
+(defun my:make-k ()
+  "Make k."
+  (interactive)
+  (compile "make -k"))
 
-  (defun my:make-draft ()
-	"Make kinnei draft."
-	(interactive)
-	(compile "make df"))
+(defun my:make-draft ()
+  "Make kinnei draft."
+  (interactive)
+  (compile "make df"))
 
-  (defun my:make-upsftp ()
-	"Make upfstp."
-	(interactive)
-	(compile "make up"))
+(defun my:make-upsftp ()
+  "Make upfstp."
+  (interactive)
+  (compile "make up"))
 
-  (defun my:make-move ()
-	"Make move."
-	(interactive)
-	(compile "make mv"))
+(defun my:make-move ()
+  "Make move."
+  (interactive)
+  (compile "make mv"))
 
-  (defun my:make-bklog ()
-	"Make bklog."
-	(interactive)
-	(compile "make bk"))
+(defun my:make-bklog ()
+  "Make bklog."
+  (interactive)
+  (compile "make bk"))
 
-  (defun my:make-git ()
-	"Make git."
-	(interactive)
-	(compile "make git"))
+(defun my:make-git ()
+  "Make git."
+  (interactive)
+  (compile "make git"))
 
-  (defun my:make-sort ()
-	"Make sort for filelist."
-	(interactive)
-	(compile "make sort")
-	(find-file "~/Dropbox/GH/upsftp/filelist.txt")
-	(goto-char (point-min)))
+(defun my:make-sort ()
+  "Make sort for filelist."
+  (interactive)
+  (compile "make sort")
+  (find-file "~/Dropbox/GH/upsftp/filelist.txt")
+  (goto-char (point-min)))
 
-  (defun my:make-clean ()
-	"Make clean."
-	(interactive)
-	(compile "make clean")))
+(defun my:make-clean ()
+  "Make clean."
+  (interactive)
+  (compile "make clean"))
 
 
 ;; Local Variables:
