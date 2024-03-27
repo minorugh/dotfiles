8ea53063798440499dc26dfb8dcbb95a3ffd8455
minorugh <minorugh@gmail.com>
2024-03-27 09:05:19 +0900

    Fix font-size

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -58,7 +58,7 @@
 ;; Fonts
 (if (string-match "e590" (shell-command-to-string "uname -n"))
 	(add-to-list 'default-frame-alist '(font . "Cica-21"))
-  (add-to-list 'default-frame-alist '(font . "Cica-15")))
+  (add-to-list 'default-frame-alist '(font . "Cica-18")))
 
 
 (provide 'early-init)
