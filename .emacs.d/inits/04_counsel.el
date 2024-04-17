5b5918d4e07b5d10861c2c37997861dbc6c63390
minorugh <minorugh@gmail.com>
2024-04-17 17:21:26 +0900

    Fix

--- .emacs.d/inits/04_counsel.el
+++ .emacs.d/inits/04_counsel.el
@@ -31,17 +31,17 @@
 	  (amx-history-length . 20)))
   :config
   (defun my:ivy-format-function-arrow (cands)
-	"Transform into a string for minibuffer with CANDS."
-	(ivy--format-function-generic
-	 (lambda (str)
+    "Transform into a string for minibuffer."
+    (ivy--format-function-generic
+     (lambda (str)
 	   (concat (if (display-graphic-p)
 				   "")
 			   (propertize " " 'display `(space :align-to 2))
 			   (ivy--add-face str 'ivy-current-match)))
-	 (lambda (str)
+     (lambda (str)
 	   (concat (propertize " " 'display `(space :align-to 2)) str))
-	 cands
-	 "\n"))
+     cands
+     "\n"))
 
   (defun ad:counsel-ag (f &optional initial-input initial-directory extra-ag-args ag-prompt caller)
 	"Fast full-text search. see https://takaxp.github.io/init.html#org29c7b6b7"
@@ -51,19 +51,16 @@
 		   (unless current-prefix-arg
 			 (or initial-directory default-directory))
 		   extra-ag-args ag-prompt caller))
-  (with-eval-after-load "counsel"
-	(require 'thingatpt nil t)
-	(advice-add 'counsel-ag :around #'ad:counsel-ag)
-	;; Make search trigger even with 2 characters
-	(add-to-list 'ivy-more-chars-alist '(counsel-ag . 2))
-	(ivy-add-actions
-	 'counsel-ag
-	 '(("r" my:counsel-ag-in-dir "search in directory")))
-
-	(defun my:counsel-ag-in-dir (_arg)
-	  "Search again with new root directory."
-	  (let ((current-prefix-arg '(4)))
-		(counsel-ag ivy-text nil "")))))
+  (advice-add 'counsel-ag :around #'ad:counsel-ag)
+  ;; Make search trigger even with 2 characters
+  (add-to-list 'ivy-more-chars-alist '(counsel-ag . 2))
+  (ivy-add-actions
+   'counsel-ag
+   '(("r" my:counsel-ag-in-dir "search in directory")))
+  (defun my:counsel-ag-in-dir (_arg)
+	"Search again with new root directory."
+	(let ((current-prefix-arg '(4)))
+	  (counsel-ag ivy-text nil ""))))
 
 
 ;; Local Variables:
