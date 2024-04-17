1456ad7e90c139b22ea20115fac66f63c72d17a8
minorugh <minorugh@gmail.com>
2024-04-17 17:22:24 +0900

    Fix

--- .emacs.d/inits/10_writing.el
+++ .emacs.d/inits/10_writing.el
@@ -55,15 +55,15 @@
   :hook (mail-mode-hook . darkroom-mode))
 
 
-(leaf pangu-spacing
-  :ensure t
-  :hook ((markdown-mode-hook text-mode-hook) . pangu-spacing-mode)
-  :config
-  (setq pangu-spacing-real-insert-separtor t)
-  (setq pangu-spacing-include-regexp ;; alphabet only
-		(rx (or (and (or (group-n 3 (any "。，！？；：「」（）、"))
-						 (group-n 1 (or (category japanese))))))
-			(group-n 2 (in "a-zA-Z")))))
+;; (leaf pangu-spacing
+;;   :ensure t
+;;   :hook ((markdown-mode-hook text-mode-hook) . pangu-spacing-mode)
+;;   :config
+;;   (setq pangu-spacing-real-insert-separtor t)
+;;   (setq pangu-spacing-include-regexp ;; alphabet only
+;; 		(rx (or (and (or (group-n 3 (any "。，！？；：「」（）、"))
+;; 						 (group-n 1 (or (category japanese))))))
+;; 			(group-n 2 (in "a-zA-Z")))))
 
 
 ;; Local Variables:
