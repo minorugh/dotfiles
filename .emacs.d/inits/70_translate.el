f7a845cc774f2f29944ede5497a798f4de37e965
minorugh <minorugh@gmail.com>
2024-04-26 22:26:19 +0900

    Fix

--- .emacs.d/inits/70_translate.el
+++ .emacs.d/inits/70_translate.el
@@ -44,7 +44,9 @@
   :doc "Display translation results in mini-buffer & copy to clipboard"
   :url "https://gist.github.com/masatoi/ec90d49331e40983427025f8167d01ee"
   :require my:deepl-api
-  :bind ("C-c d" . deepl-translate))
+  :bind ("C-c d" . deepl-translate)
+  :init
+  (load "~/Dropbox/backup/emacs/api/deepl-api.el"))
 
 
 ;; Local Variables:
