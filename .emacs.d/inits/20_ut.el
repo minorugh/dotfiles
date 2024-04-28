a3ce56f34ef4459a3bb4625b3fc5eb879706c080
minorugh <minorugh@gmail.com>
2024-04-28 23:21:21 +0900

    Fix

--- .emacs.d/inits/20_ut.el
+++ .emacs.d/inits/20_ut.el
@@ -3,51 +3,51 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(leaf which-key :ensure t
-  :hook (after-init-hook . which-key-mode)
-  :custom (which-key-max-description-length . 40))
+(leaf which-key
+  :ensure t
+  :custom (which-key-max-description-length . 40)
+  :hook after-init-hook)
 
-
-(leaf counsel-tramp :ensure t
+(leaf counsel-tramp
+  :ensure t
   :custom
   `((tramp-persistency-file-name . ,"~/.emacs.d/tmp/tramp")
 	(tramp-default-method        . "scp")
 	(counsel-tramp-custom-connections
 	 . '(/scp:xsrv:/home/minorugh/gospel-haiku.com/public_html/))))
 
-
-(leaf sequential-command :ensure t
+(leaf sequential-command
+  :ensure t
   :config
   (leaf sequential-command-config
 	:hook (after-init-hook . sequential-command-setup-keys)))
 
-
-(leaf counsel-web :ensure t
+(leaf counsel-web
+  :ensure t
   :bind ("s-w" . counsel-web-search)
   :custom
   `((counsel-web-search-action . #'browse-url)
 	(counsel-web-engine . 'google)
 	(counsel-web-search-dynamic-update . t)))
 
-
-(leaf quickrun :ensure t
+(leaf quickrun
+  :ensure t
   :bind ([f6] . quickrun))
 
-
-(leaf key-chord :ensure t
-  :hook (after-init-hook . key-chord-mode)
+(leaf key-chord
+  :ensure t
   :custom (key-chord-two-keys-delay . 0.1)
   :chord (("df" . counsel-descbinds)
-		  ("l;" . init-loader-show-log)))
+		  ("l;" . init-loader-show-log))
+  :hook after-init-hook)
 
-
-(leaf iedit :ensure t
+(leaf iedit
+  :ensure t
   :bind ([insert] . iedit-mode))
 
-
-(leaf expand-region :ensure t
+(leaf expand-region
+  :ensure t
   :bind ("C-@" . er/expand-region))
-
 
 (leaf *cus-ps-print
   :url "https://tam5917.hatenablog.com/entry/20120914/1347600433"
