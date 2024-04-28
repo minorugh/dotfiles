64babccb8edc4deac0b1899e53419b0d463a364d
minorugh <minorugh@gmail.com>
2024-04-28 20:58:17 +0900

    Fix

--- .emacs.d/inits/20_misc.el
+++ .emacs.d/inits/20_misc.el
@@ -3,34 +3,39 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-(leaf projectile :ensure t
-  :hook (after-init-hook . projectile-mode)
-  :custom
-  (projectile-known-projects-file . "~/.emacs.d/tmp/projectile.eld"))
+(leaf projectile
+  :ensure t
+  :custom (projectile-known-projects-file . "~/.emacs.d/tmp/projectile.eld")
+  :hook after-init-hook)
 
 
-(leaf prescient :ensure t
-  :hook (after-init-hook . prescient-persist-mode)
+(leaf prescient
+  :ensure t
   :custom `((prescient-aggressive-file-save . t)
 			(prescient-save-file . "~/.emacs.d/tmp/prescient-save"))
+  :hook (after-init-hook . prescient-persist-mode)
   :config
   (leaf ivy-prescient :ensure t :global-minor-mode t)
   (leaf company-prescient :ensure t :global-minor-mode t))
 
 
-(leaf popwin :ensure t
-  :hook (after-init-hook . popwin-mode))
+(leaf popwin
+  :ensure t
+  :hook after-init-hook)
 
 
-(leaf posframe :ensure t
+(leaf posframe
+  :ensure t
   :when window-system)
 
 
-(leaf aggressive-indent :ensure t
+(leaf aggressive-indent
+  :ensure t
   :hook ((emacs-lisp-mode-hook css-mode-hook) . aggressive-indent-mode))
 
 
-(leaf imenu-list :ensure t
+(leaf imenu-list
+  :ensure t
   :bind ([f2]  . imenu-list-smart-toggle)
   :custom `((imenu-list-auto-resize . t)
 			(imenu-list-position    . 'left))
@@ -40,19 +45,12 @@
     :hook (css-mode-hook . counsel-css-imenu-setup)))
 
 
-(leaf web-mode :ensure t
+(leaf web-mode
+  :ensure t
   :mode ("\\.js?\\'" "\\.html?\\'" "\\.php?\\'")
   :custom `((web-mode-markup-indent-offset . 2)
 			(web-mode-css-indent-offset . 2)
 			(web-mode-code-indent-offset . 2)))
-
-
-;; (leaf flycheck :ensure t
-;;   :hook (prog-mode-hook . flycheck-mode)
-;;   :bind (:flycheck-mode-map
-;;          ("M-n" . flycheck-next-error)
-;;          ("M-p" . flycheck-previous-error))
-;;   :custom ((flycheck-emacs-lisp-initialize-packages . t)))
 
 
 ;; Local Variables:
