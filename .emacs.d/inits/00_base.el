2a43f82eb7c7389d82ae534caf8f0e01e7e9b71d
minorugh <minorugh@gmail.com>
2024-04-28 15:23:00 +0900

    Fix

--- .emacs.d/inits/00_base.el
+++ .emacs.d/inits/00_base.el
@@ -6,50 +6,50 @@
 (leaf *generic-configuration
   :custom
   `(;; No startup screen appears
-	(inhibit-splash-screen . t)
-	;; Faster rendering by not corresponding to right-to-left language
-	(bidi-display-reordering . nil)
-	;; Do not make a backup file like *.~
-	(make-backup-files . nil)
-	;; Do not use auto save
-	(auto-save-default . nil)
-	(auto-save-list-file-prefix . nil)
-	;; Do not create lock file
-	(create-lockfiles . nil)
-	;; Open symbolic link directly
-	(vc-follow-symlinks . t)
-	;; Do not distinguish uppercase and lowercase letters on completion
-	(completion-ignore-case . t)
-	(read-file-name-completion-ignore-case . t)
-	;; Point keeps its screen position when scroll
-	(scroll-preserve-screen-position . t)
-	;; All warning sounds and flash are invalid
-	(ring-bell-function . 'ignore)
-	;; Turn off warning sound screen flash
-	(visible-bell . nil)
-	;; Copy text with mouse range selection
-	(mouse-drag-copy-region . t)
-	;; Deleted files go to the trash
-	(delete-by-moving-to-trash . t)
-	;; Tab width default
-	(tab-width . 4)
-	;; Limit the final word to a line break code (automatically correct)
-	(require-final-newline . t)
-	;; Disallow adding new lines with newline at the end of the buffer
-	(next-line-add-newlines . nil)
-	;; Make it easy to see when it is the same name file
-	(uniquify-buffer-name-style . 'post-forward-angle-brackets)
-	;; It keeps going steadily the local mark ...  C-u C-SPC C-SPC
-	;; It keeps going steadily the global mark ... C-x C-SPC C-SPC
-	(set-mark-command-repeat-pop . t)
-	;; Use the X11 clipboard
-	(select-enable-clipboard  . t)
-	;; Disable warnings at initialization
-	(warning-minimum-level . :emergency)
-	;; change-default-file-location
-	(request-storage-directory . "~/.emacs.d/tmp/request")
-	(url-configuration-directory . "~/.emacs.d/tmp/url")
-	(bookmark-file . "~/.emacs.d/tmp/bookmarks"))
+    (inhibit-splash-screen . t)
+    ;; Faster rendering by not corresponding to right-to-left language
+    (bidi-display-reordering . nil)
+    ;; Do not make a backup file like *.~
+    (make-backup-files . nil)
+    ;; Do not use auto save
+    (auto-save-default . nil)
+    (auto-save-list-file-prefix . nil)
+    ;; Do not create lock file
+    (create-lockfiles . nil)
+    ;; Open symbolic link directly
+    (vc-follow-symlinks . t)
+    ;; Do not distinguish uppercase and lowercase letters on completion
+    (completion-ignore-case . t)
+    (read-file-name-completion-ignore-case . t)
+    ;; Point keeps its screen position when scroll
+    (scroll-preserve-screen-position . t)
+    ;; All warning sounds and flash are invalid
+    (ring-bell-function . 'ignore)
+    ;; Turn off warning sound screen flash
+    (visible-bell . nil)
+    ;; Copy text with mouse range selection
+    (mouse-drag-copy-region . t)
+    ;; Deleted files go to the trash
+    (delete-by-moving-to-trash . t)
+    ;; Tab width default
+    (tab-width . 4)
+    ;; Limit the final word to a line break code (automatically correct)
+    (require-final-newline . t)
+    ;; Disallow adding new lines with newline at the end of the buffer
+    (next-line-add-newlines . nil)
+    ;; Make it easy to see when it is the same name file
+    (uniquify-buffer-name-style . 'post-forward-angle-brackets)
+    ;; It keeps going steadily the local mark ...  C-u C-SPC C-SPC
+    ;; It keeps going steadily the global mark ... C-x C-SPC C-SPC
+    (set-mark-command-repeat-pop . t)
+    ;; Use the X11 clipboard
+    (select-enable-clipboard  . t)
+    ;; Disable warnings at initialization
+    (warning-minimum-level . :emergency)
+    ;; change-default-file-location
+    (request-storage-directory . "~/.emacs.d/tmp/request")
+    (url-configuration-directory . "~/.emacs.d/tmp/url")
+    (bookmark-file . "~/.emacs.d/tmp/bookmarks"))
   :config
   ;; Change to short command
   (defalias 'yes-or-no-p #'y-or-n-p)
@@ -76,25 +76,12 @@
 
   ;;Server
   (defun restart-server ()
-	"Server Start."
-	(interactive)
-	(eval-and-compile (require 'server))
-	(unless (server-running-p)
-	  (server-start)))
-  (add-hook 'emacs-startup-hook 'restart-server)
-
-  ;; Emacs init time
-  (defun ad:emacs-init-time ()
-	"Advice `emacs-init-time'."
-	(interactive)
-	(let ((str
-		   (format "%.3f seconds"
-				   (float-time
-					(time-subtract after-init-time before-init-time)))))
-	  (if (called-interactively-p 'interactive)
-		  (message "%s" str)
-		str)))
-  (advice-add 'emacs-init-time :override #'ad:emacs-init-time))
+    "Server Start."
+    (interactive)
+    (eval-and-compile (require 'server))
+    (unless (server-running-p)
+      (server-start)))
+  (add-hook 'emacs-startup-hook 'restart-server))
 
 
 ;; Local Variables:
