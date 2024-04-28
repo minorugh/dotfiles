60b0d65d5a0c1656a632b0d5ac082ee57285ca9a
minorugh <minorugh@gmail.com>
2024-04-28 15:22:44 +0900

    Fix

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -7,7 +7,7 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-;; Move garbage collection further back in the startup process
+;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
 
@@ -27,12 +27,14 @@
 ;; In noninteractive sessions, prioritize non-byte-compiled source files to
 ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
 ;; to skip the mtime checks on every *.elc file.
-(setq load-prefer-newer noninteractive)
+(setq load-prefer-newer t)
 
+;; Encoding
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
 
 ;; Inhibit resizing frame
 (setq frame-inhibit-implied-resize t)
-
 
 ;; Faster to disable these here (before they've been initialized)
 (push '(fullscreen . maximized) default-frame-alist)
@@ -44,7 +46,6 @@
 ;; Prevent flashing of unstyled modeline at startup
 (setq-default mode-line-format nil)
 
-
 ;; Suppress flashing at startup
 (when (file-directory-p "~/.emacs.d/elpa/")
   (setq inhibit-message t)
@@ -55,12 +56,6 @@
 			  (redisplay)))
   (custom-set-faces
    '(default ((t (:background "#282a36"))))))
-
-
-;; Encoding
-(set-language-environment "Japanese")
-(prefer-coding-system 'utf-8)
-
 
 ;; Fonts
 (if (string-match "P1" (shell-command-to-string "uname -n"))
