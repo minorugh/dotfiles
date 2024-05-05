3b41c58606f1762b76515a3a5be250b3a1e1d68b
minorugh <minorugh@gmail.com>
2024-05-06 06:56:52 +0900

    Fix

--- .emacs.d/inits/08_evil.el
+++ .emacs.d/inits/08_evil.el
@@ -7,17 +7,17 @@
   :doc "The extensible vi layer for Emacs"
   :ensure t
   :hook ((after-init-hook . evil-mode)
-		 (find-file-hook  . my:evil-find-file))
+	 (find-file-hook  . my:evil-find-file))
   :bind ((:evil-normal-state-map
-		  ("C-e"  . seq-end) ;; sequential-command
-		  ("SPC"  . evil-insert)
-		  ("C-@"  . er/expand-region)
-		  ("M-."  . nil)
-		  ("?"    . vim-cheat-sheet)
-		  ([home] . open-dashboard))
-		 (:evil-emacs-state-map
-		  ([muhenkan] . my:return-to-normal-state)
-		  ([escape]   . my:return-to-normal-state)))
+	  ("C-e"  . seq-end) ;; sequential-command
+	  ("SPC"  . evil-insert)
+	  ("C-@"  . er/expand-region)
+	  ("M-."  . nil)
+	  ("?"    . vim-cheat-sheet)
+	  ([home] . open-dashboard))
+	 (:evil-emacs-state-map
+	  ([muhenkan] . my:return-to-normal-state)
+	  ([escape]   . my:return-to-normal-state)))
   :init
   (setq evil-cross-lines t)
   (setq evil-undo-system 'undo-fu)
@@ -25,46 +25,46 @@
   (defalias 'evil-insert-state 'evil-emacs-state)
   ;; Force evil-emacs-state for major modes
   (dolist (mode '(lisp-interaction-mode
-				  fundamental-mode dashboard-mode dired-mode
-				  neotree-mode howm-mode yatex-mode mail-mode
-				  git-timemachine-mode easy-hugo-mode markdown-mode))
-	(add-to-list 'evil-emacs-state-modes mode))
+		  fundamental-mode dashboard-mode dired-mode
+		  neotree-mode howm-mode yatex-mode mail-mode
+		  git-timemachine-mode easy-hugo-mode markdown-mode))
+    (add-to-list 'evil-emacs-state-modes mode))
 
   ;; Force evil-emacs-state for minor modes
   (add-hook 'org-capture-mode-hook 'evil-emacs-state)
   (add-hook 'magit-blame-mode-hook 'evil-emacs-state)
 
   (defun vim-cheat-sheet ()
-	"Vim cheat sheet online."
-	(interactive)
-	(browse-url "https://minorugh.github.io/emacs.d/vim-cheat.html"))
+    "Vim cheat sheet online."
+    (interactive)
+    (browse-url "https://minorugh.github.io/emacs.d/vim-cheat.html"))
 
   (defun my:return-to-normal-state ()
-	"Turn off input-method then return to normal-state."
-	(interactive)
-	(if (use-region-p)(keyboard-escape-quit))
-	(if current-input-method (deactivate-input-method))
-	(evil-normal-state)
-	(message "-- NORMAL --"))
+    "Turn off input-method then return to normal-state."
+    (interactive)
+    (if (use-region-p)(keyboard-escape-quit))
+    (if current-input-method (deactivate-input-method))
+    (evil-normal-state)
+    (message "-- NORMAL --"))
 
   (defun my:evil-find-file ()
-	"New files open in insert-state."
-	(interactive)
-	(unless (file-exists-p buffer-file-name)
+    "New files open in insert-state."
+    (interactive)
+    (unless (file-exists-p buffer-file-name)
       (evil-insert-state)))
 
   ( defun evil-swap-key (map key1 key2)
-	"Swap KEY1 and KEY2 in MAP."
-	(let ((def1 (lookup-key map key1))
-		  (def2 (lookup-key map key2)))
-	  (define-key map key1 def2)
-	  (define-key map key2 def1)))
+    "Swap KEY1 and KEY2 in MAP."
+    (let ((def1 (lookup-key map key1))
+	  (def2 (lookup-key map key2)))
+      (define-key map key1 def2)
+      (define-key map key2 def1)))
   (evil-swap-key evil-motion-state-map "j" "gj")
   (evil-swap-key evil-motion-state-map "k" "gk")
 
   (defun ad:switch-to-buffer (&rest _arg)
-	"Set buffer for automatic `evil-insert-state'."
-	(when (member (buffer-name) '("COMMIT_EDITMSG"))
+    "Set buffer for automatic `evil-insert-state'."
+    (when (member (buffer-name) '("COMMIT_EDITMSG"))
       (evil-insert-state)))
   (advice-add 'switch-to-buffer :after #'ad:switch-to-buffer))
 
