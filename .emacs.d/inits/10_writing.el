400100e712ba1f42e2de833ec3f656fa967fad1b
minorugh <minorugh@gmail.com>
2024-05-06 06:57:42 +0900

    Fix

--- .emacs.d/inits/10_writing.el
+++ .emacs.d/inits/10_writing.el
@@ -30,27 +30,27 @@
   :doc "Remove visual distractions and focus on writing"
   :ensure t
   :bind (([f8] . my:darkroom-in)
-		 (:darkroom-mode-map
-		  ([f8] . my:darkroom-out)))
+	 (:darkroom-mode-map
+	  ([f8] . my:darkroom-out)))
   :config
   (defun my:darkroom-in ()
-	"Enter to the `darkroom-mode'."
-	(interactive)
-	(diff-hl-mode 0)
-	(display-line-numbers-mode 0)
-	(darkroom-tentative-mode 1)
-	(toggle-frame-fullscreen)
-	(setq-local line-spacing .2)
-	(evil-emacs-state))
+    "Enter to the `darkroom-mode'."
+    (interactive)
+    (diff-hl-mode 0)
+    (display-line-numbers-mode 0)
+    (darkroom-tentative-mode 1)
+    (toggle-frame-fullscreen)
+    (setq-local line-spacing .2)
+    (evil-emacs-state))
 
   (defun my:darkroom-out ()
-	"Returns from `darkroom-mode' to the previous state."
-	(interactive)
-	(darkroom-tentative-mode 0)
-	(display-line-numbers-mode 1)
-	(toggle-frame-fullscreen)
-	(setq-local line-spacing 0)
-	(evil-normal-state)))
+    "Returns from `darkroom-mode' to the previous state."
+    (interactive)
+    (darkroom-tentative-mode 0)
+    (display-line-numbers-mode 1)
+    (toggle-frame-fullscreen)
+    (setq-local line-spacing 0)
+    (evil-normal-state)))
 
 
 (leaf mail-mode
