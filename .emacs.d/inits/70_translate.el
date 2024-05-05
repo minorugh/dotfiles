f2dc59a8b3025baaed61654cf7a2be8a5752893e
minorugh <minorugh@gmail.com>
2024-05-06 07:01:47 +0900

    Fix

--- .emacs.d/inits/70_translate.el
+++ .emacs.d/inits/70_translate.el
@@ -9,33 +9,33 @@
   :bind ("C-t" . google-translate-auto)
   :config
   (defun google-translate-auto ()
-	"Automatically recognize and translate Japanese and English."
-	(interactive)
-	(if (use-region-p)
-		(let ((string (buffer-substring-no-properties (region-beginning) (region-end))))
-		  (deactivate-mark)
-		  (if (string-match (format "\\`[%s]+\\'" "[:ascii:]")
-							string)
-			  (google-translate-translate
-			   "en" "ja"
-			   string)
-			(google-translate-translate
-			 "ja" "en"
-			 string)))
-	  (let ((string (read-string "Google Translate: ")))
-		(if (string-match
-			 (format "\\`[%s]+\\'" "[:ascii:]")
-			 string)
-			(google-translate-translate
-			 "en" "ja"
-			 string)
-		  (google-translate-translate
-		   "ja" "en"
-		   string)))))
+    "Automatically recognize and translate Japanese and English."
+    (interactive)
+    (if (use-region-p)
+	(let ((string (buffer-substring-no-properties (region-beginning) (region-end))))
+	  (deactivate-mark)
+	  (if (string-match (format "\\`[%s]+\\'" "[:ascii:]")
+			    string)
+	      (google-translate-translate
+	       "en" "ja"
+	       string)
+	    (google-translate-translate
+	     "ja" "en"
+	     string)))
+      (let ((string (read-string "Google Translate: ")))
+	(if (string-match
+	     (format "\\`[%s]+\\'" "[:ascii:]")
+	     string)
+	    (google-translate-translate
+	     "en" "ja"
+	     string)
+	  (google-translate-translate
+	   "ja" "en"
+	   string)))))
 
   (defun google-translate--get-b-d1 ()
-	"Fix error of `Failed to search TKK`."
-	(list 427110 1469889687)))
+    "Fix error of `Failed to search TKK`."
+    (list 427110 1469889687)))
 
 
 ;; Deeple translate configurations
