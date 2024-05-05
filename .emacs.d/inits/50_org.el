93dbf5dc9a264bd419b8f7d4c1dff6e0f579e6f6
minorugh <minorugh@gmail.com>
2024-05-06 07:00:38 +0900

    Fix

--- .emacs.d/inits/50_org.el
+++ .emacs.d/inits/50_org.el
@@ -6,11 +6,11 @@
 (leaf org
   :chord ((",," . org-capture))
   :bind ((("C-c a"  . org-agenda)
-		  ("C-c c"  . org-capture)
-		  ("C-c k"  . org-capture-kill)
-		  ("C-c i"  . org-edit-src-exit)
-		  (:org-mode-map
-		   ("C-c i" . org-edit-special))))
+	  ("C-c c"  . org-capture)
+	  ("C-c k"  . org-capture-kill)
+	  ("C-c i"  . org-edit-src-exit)
+	  (:org-mode-map
+	   ("C-c i" . org-edit-special))))
   :config
   (setq org-log-done 'time)
   (setq org-use-speed-commands t)
@@ -19,24 +19,24 @@
   (setq org-startup-truncated nil)
   (setq org-agenda-files '("~/Dropbox/howm/org/task.org"))
   (setq org-refile-targets
-		(quote (("~/Dropbox/howm/org/archives.org" :level . 1)
-				("~/Dropbox/howm/org/remember.org" :level . 1)
-				("~/Dropbox/howm/org/memo.org" :level     . 1)
-				("~/Dropbox/howm/org/task.org" :level     . 1)))))
+	(quote (("~/Dropbox/howm/org/archives.org" :level . 1)
+		("~/Dropbox/howm/org/remember.org" :level . 1)
+		("~/Dropbox/howm/org/memo.org" :level     . 1)
+		("~/Dropbox/howm/org/task.org" :level     . 1)))))
 
 
 ;; Capture template
 (setq org-capture-templates
-	  '(("m" " Memo" entry (file+headline "~/Dropbox/howm/org/memo.org" "Memo")
-		 "* %? %U %i")
-		("i" " Idea" entry (file+headline "~/Dropbox/howm/org/idea.org" "Idea")
-		 "* %? %U %i")
-		("r" " Remember" entry (file+headline "~/Dropbox/howm/org/remember.org" "Remember")
-		 "* %? %U %i")
-		("t" " Task" entry (file+headline "~/Dropbox/howm/org/task.org" "TASK")
-		 "** TODO %?\n SCHEDULED: %^t \n")
-		("e" " Experiment" entry (file+headline "~/Dropbox/howm/org/experiment.org" "Experiment")
-		 "* %? %i\n#+BEGIN_SRC perl\n\n#+END_SRC\n\n%U")))
+      '(("m" " Memo" entry (file+headline "~/Dropbox/howm/org/memo.org" "Memo")
+	 "* %? %U %i")
+	("i" " Idea" entry (file+headline "~/Dropbox/howm/org/idea.org" "Idea")
+	 "* %? %U %i")
+	("r" " Remember" entry (file+headline "~/Dropbox/howm/org/remember.org" "Remember")
+	 "* %? %U %i")
+	("t" " Task" entry (file+headline "~/Dropbox/howm/org/task.org" "TASK")
+	 "** TODO %?\n SCHEDULED: %^t \n")
+	("e" " Experiment" entry (file+headline "~/Dropbox/howm/org/experiment.org" "Experiment")
+	 "* %? %i\n#+BEGIN_SRC perl\n\n#+END_SRC\n\n%U")))
 
 
 (leaf calendar
