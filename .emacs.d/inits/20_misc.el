ea0b0600dac168bd4a93e4011294bb66f4c1f51e
minorugh <minorugh@gmail.com>
2024-05-18 05:42:42 +0900

    Fix

--- .emacs.d/inits/20_misc.el
+++ .emacs.d/inits/20_misc.el
@@ -43,25 +43,23 @@
 (leaf flycheck
   :doc "On-the-fly syntax checking"
   :ensure t
-  :bind (("M-n" . flycheck-next-error)
-	 ("M-p" . flycheck-previous-error))
   :config
   (setq flycheck-emacs-lisp-initialize-packages t)
   :hook  prog-mode-hook)
 
 
-  (leaf imenu-list
-    :doc "Show imenu entries in a separate buffer"
+(leaf imenu-list
+  :doc "Show imenu entries in a separate buffer"
+  :ensure t
+  :bind ([f2]  . imenu-list-smart-toggle)
+  :config
+  (setq imenu-list-auto-resize t)
+  (setq imenu-list-position    'left)
+  :preface
+  (leaf counsel-css
+    :doc "stylesheet-selector-aware swiper"
     :ensure t
-    :bind ([f2]  . imenu-list-smart-toggle)
-    :config
-    (setq imenu-list-auto-resize t)
-    (setq imenu-list-position    'left)
-    :preface
-    (leaf counsel-css
-      :doc "stylesheet-selector-aware swiper"
-      :ensure t
-      :hook (css-mode-hook . counsel-css-imenu-setup)))
+    :hook (css-mode-hook . counsel-css-imenu-setup)))
 
 
 (leaf web-mode
