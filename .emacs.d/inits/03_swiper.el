8031897de9a204ef12ff8e4ecbf0055298a0586d
minorugh <minorugh@gmail.com>
2024-05-19 07:13:35 +0900

    Fix

--- .emacs.d/inits/03_swiper.el
+++ .emacs.d/inits/03_swiper.el
@@ -7,35 +7,34 @@
   :doc "Isearch with an overview"
   :ensure t
   :bind (("C-s" . swiper-region)
-		 ("C-c r" . swiper-thing-at-point))
+	 ("C-c r" . swiper-thing-at-point))
   :config
   (defun swiper-region ()
     "If region is selected, `swiper-thing-at-point'.
 If the region isn't selected, `swiper'."
     (interactive)
     (if (not (use-region-p))
-		(swiper)
+	(swiper)
       (swiper-thing-at-point)))
-
+  :preface
   (leaf swiper-migemo
-	:doc "ivy/counsel/swiper with migemo"
-	:el-get "tam17aki/swiper-migemo"
-	:hook (after-init-hook . global-swiper-migemo-mode)
-	:config
-	(add-to-list 'swiper-migemo-enable-command 'counsel-rg)
-	(setq migemo-options '("--quiet" "--nonewline" "--emacs"))
-	(migemo-kill)
-	(migemo-init)))
+    :doc "ivy/counsel/swiper with migemo"
+    :el-get "tam17aki/swiper-migemo"
+    :hook (after-init-hook . global-swiper-migemo-mode)
+    :config
+    (add-to-list 'swiper-migemo-enable-command 'counsel-rg)
+    (setq migemo-options '("--quiet" "--nonewline" "--emacs"))
+    (migemo-kill)
+    (migemo-init))
 
-
-(leaf migemo
-  :doc "Japanese incremental search through dynamic pattern expansion"
-  :ensure t
-  :if (executable-find "cmigemo")
-  :hook (after-init-hook . migemo-init)
-  :config
-  (setq migemo-command    "cmigemo")
-  (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict"))
+  (leaf migemo
+    :doc "Japanese incremental search through dynamic pattern expansion"
+    :ensure t
+    :if (executable-find "cmigemo")
+    :hook (after-init-hook . migemo-init)
+    :config
+    (setq migemo-command    "cmigemo")
+    (setq migemo-dictionary "/usr/share/cmigemo/utf-8/migemo-dict")))
 
 
 ;; Local Variables:
