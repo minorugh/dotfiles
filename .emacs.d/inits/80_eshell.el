cbaa87bfdee87f55d521badf14d92dc29f4fc6da
minorugh <minorugh@gmail.com>
2024-05-22 08:01:14 +0900

    Fix

--- .emacs.d/inits/80_eshell.el
+++ .emacs.d/inits/80_eshell.el
@@ -18,12 +18,12 @@
 	  (list "ex" "exit"))))
   (push '("*eshell*" :height 0.5) popwin:special-display-config)
   :config
-  (setq eshell-directory-name ,"~/.emacs.d/tmp/eshell")
+  (setq eshell-directory-name "~/.emacs.d/tmp/eshell")
   (setq eshell-cmpl-ignore-case t)
   (setq eshell-ask-to-save-history (quote always))
   (setq eshell-cmpl-cycle-completions t)
   (setq eshell-cmpl-cycle-cutoff-length 5)
-  (setq eshell-history-file-name ,"~/Dropbox/backup/zsh/.zsh_history")
+  (setq eshell-history-file-name "~/Dropbox/backup/zsh/.zsh_history")
   (setq eshell-hist-ignoredups t)
   (custom-set-variables
    '(eshell-prompt-function
