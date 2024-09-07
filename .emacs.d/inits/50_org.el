666431076432c0c97806cf8483b19a64454ccb2d
minorugh <minorugh@gmail.com>
2024-09-07 15:27:06 +0900

    Fix org-agenda-span for month

--- .emacs.d/inits/50_org.el
+++ .emacs.d/inits/50_org.el
@@ -17,12 +17,8 @@
   (setq org-src-fontify-natively t)
   (setq org-startup-folded 'content)
   (setq org-startup-truncated nil)
+  (setq org-agenda-span 'month)
   (setq org-agenda-files '("~/Dropbox/howm/org/task.org"))
-  ;; (setq org-refile-targets
-  ;; 	(quote (("~/Dropbox/howm/org/archives.org" :level . 1)
-  ;; 		("~/Dropbox/howm/org/remember.org" :level . 1)
-  ;; 		("~/Dropbox/howm/org/memo.org" :level     . 1)
-  ;; 		("~/Dropbox/howm/org/task.org" :level     . 1))))
 
   ;; Capture template
   (defun my:howm-create-file ()
@@ -47,21 +43,25 @@
 	  ("," " Task" entry (file+headline "~/Dropbox/howm/org/task.org" "TASK")
 	   "** TODO %?\n SCHEDULED: %^t \n"))))
 
-
 (leaf calendar
+  :defvar calendar-holidays japanese-holidays
   :bind (("<f7>"  . calendar)
 	 (:calendar-mode-map
 	  ("<f7>" . calendar-exit)))
   :config
-  (leaf japanese-holidays :ensure t
-    :require t
-    :hook '((calendar-today-visible-hook   . japanese-holiday-mark-weekend)
-	    (calendar-today-invisible-hook . japanese-holiday-mark-weekend)
-	    (calendar-today-visible-hook   . calendar-mark-today))
-    :config
-    (setq calendar-holidays
-	  (append japanese-holidays holiday-local-holidays holiday-other-holidays))
-    (setq calendar-mark-holidays-flag t)))
+  (with-eval-after-load 'japanese-holidays
+    (setq calendar-holidays (append japanese-holidays holiday-local-holidays))))
+
+(leaf japanese-holidays :ensure t
+  :after calendar
+  :require t
+  :hook '((calendar-today-visible-hook   . japanese-holiday-mark-weekend)
+	  (calendar-today-invisible-hook . japanese-holiday-mark-weekend)
+	  (calendar-today-visible-hook   . calendar-mark-today))
+  :config
+  (setq calendar-holidays
+	(append japanese-holidays holiday-local-holidays holiday-other-holidays))
+  (setq calendar-mark-holidays-flag t))
 
 
 ;; Local Variables:
