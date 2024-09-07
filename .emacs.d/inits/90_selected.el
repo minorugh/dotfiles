4f4fe05cd15db56a81b2fc62c9b348b4fd2ada19
minorugh <minorugh@gmail.com>
2024-09-07 15:28:43 +0900

    Add my:deepl-traslate

--- .emacs.d/inits/90_selected.el
+++ .emacs.d/inits/90_selected.el
@@ -13,6 +13,7 @@
 	 ("s" . swiper-thing-at-point)
 	 ("d" . deepl-translate)
 	 ("t" . google-translate-auto)
+	 ("T" . my:deepl-translate)
 	 ("w" . my:weblio)
 	 ("g" . my:google-this))
   :config
