1d50f4e299f6eff84d6f9aa2dffed30ff7903eb5
minorugh <minorugh@gmail.com>
2024-09-16 19:15:32 +0900

    Fix emacs-init-time

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -58,6 +58,17 @@
 ;; Custom files
 (setq custom-file "~/.emacs.d/tmp/custom.el")
 
+(defun emacs-init-time ()
+  "Overwrite `emacs-init-time'."
+  (interactive)
+  (let ((str
+	 (format "%.3f seconds"
+		 (float-time
+		  (time-subtract after-init-time before-init-time)))))
+    (if (called-interactively-p 'interactive)
+	(message "%s" str)
+      str)))
+
 
 (provide 'early-init)
 ;; Local Variables:
