e17affca061c411012afa878ba4d19e371f63b95
minorugh <minorugh@gmail.com>
2024-09-16 15:17:15 +0900

    Fix

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -27,12 +27,10 @@
 (prefer-coding-system 'utf-8)
 
 ;; Faster to disable these here (before they've been initialized)
-(push '(fullscreen . maximized) default-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
+(push '(fullscreen . maximized) default-frame-alist)
 
 ;; Prevent flashing of unstyled modeline and headerline at startup
 (setq-default mode-line-format nil)
