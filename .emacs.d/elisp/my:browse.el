d682cacfa3bae99e408ebea94f1095f7beae0263
minorugh <minorugh@gmail.com>
2024-09-19 09:07:05 +0900

    Fix

--- .emacs.d/elisp/my:browse.el
+++ .emacs.d/elisp/my:browse.el
@@ -12,44 +12,44 @@
    "
   ^ Shop^       ^ SNS^        ^🔃 Repos^    ^ Blog^     ^ Life^      ^ Social^  ^ Github^    oogle
   ^^^^^^^^^^^^^^^^^──────────────────────────────────────────────────────────────────────────────────────────────────
-  _A_: Amazon     _t_: Twitter    _d_: Dropbox  _g_: ghub.io  _j_: Jorudan   _k_: Keep    _1_: minorugh  _5_: Mail
-  _R_: Rakuten    _y_: Youtube    _f_: Flickr   _m_: snap     _n_: News      _p_: Pocket  _2_: gist      _6_: Map
-  _Y_: Yodobashi  _I_: Instagram  _G_: Gdrive   _e_: Essay    _w_: Weather   _q_: Qiita   _3_: masasam   _7_: Earth
-  _K_: Kakaku     _T_: Tumblr     _x_: Xserver  _b_: Blog     _S_: SanyoBas  _s_: Slack   _4_: Centaur   _8_: Photo
+  _a_: Amazon     _x_: Twitter    _D_: Dropbox  _g_: ghub.io  _j_: Jorudan   _K_: Keep    _1_: minorugh  _c_: Calendar
+  _r_: Rakuten    _u_: Youtube    _F_: Flickr   _S_: snap     _n_: News      _p_: Pocket  _2_: gist      _m_: Mail
+  _y_: Yodobashi  _i_: Instagram  _G_: Gdrive   _E_: Essay    _w_: Weather   _q_: Qiita   _3_: masasam   _M_: Maps
+  _k_: Kakaku     _T_: Tumblr     _X_: Xserver  _B_: Blog     _b_: SanyoBas  _s_: Slack   _4_: Centaur   _P_: Photo
 "
-   ("A" (browse-url "https://www.amazon.co.jp/"))
-   ("R" (browse-url "https://www.rakuten.co.jp/"))
-   ("Y" (browse-url "https://www.yodobashi.com/"))
-   ("K" (browse-url "http://kakaku.com/"))
-   ("y" (browse-url "https://www.youtube.com/channel/UCnwoipb9aTyORVKHeTw159A/videos"))
-   ("f" (browse-url "https://www.flickr.com/photos/minorugh/"))
+   ("a" (browse-url "https://www.amazon.co.jp/"))
+   ("r" (browse-url "https://www.rakuten.co.jp/"))
+   ("y" (browse-url "https://www.yodobashi.com/"))
+   ("k" (browse-url "http://kakaku.com/"))
+   ("u" (browse-url "https://www.youtube.com/channel/UCnwoipb9aTyORVKHeTw159A/videos"))
+   ("F" (browse-url "https://www.flickr.com/photos/minorugh/"))
    ("G" (browse-url "https://drive.google.com/drive/u/0/my-drive"))
    ("0" (browse-url "https://gist.github.com/minorugh"))
    ("1" (browse-url "https://github.com/minorugh"))
    ("2" (browse-url "https://gist.github.com/minorugh"))
    ("3" (browse-url "https://github.com/masasam"))
    ("4" (browse-url "https://github.com/seagle0128/.emacs.d"))
-   ("5" (browse-url "https://mail.google.com/mail/"))
-   ("6" (browse-url "https://www.google.co.jp/maps"))
-   ("7" (browse-url "https://earth.google.com/web/"))
-   ("b" (browse-url "http://blog.gospel-haiku.com/"))
-   ("e" (browse-url "https://es.gospel-haiku.com/post/"))
-   ("m" (browse-url "https://snap.minorugh.com/"))
-   ("S" (browse-url "https://www.sanyo-bus.co.jp/highway/maiko.html"))
-   ("I" (browse-url "https://www.instagram.com/"))
+   ("c" (browse-url "https://calendar.google.com/calendar/r"))
+   ("m" (shell-command "sylpheed"))
+   ("M" (browse-url "https://www.google.co.jp/maps"))
+   ("B" (browse-url "http://blog.gospel-haiku.com/"))
+   ("E" (browse-url "https://es.gospel-haiku.com/post/"))
+   ("S" (browse-url "https://snap.minorugh.com/"))
+   ("b" (browse-url "https://www.sanyo-bus.co.jp/highway/maiko.html"))
+   ("i" (browse-url "https://www.instagram.com/"))
    ("j" (browse-url "https://www.jorudan.co.jp/"))
    ("n" (browse-url "https://news.yahoo.co.jp/"))
-   ("x" (browse-url "https://www.xserver.ne.jp/login_server.php"))
-   ("d" (browse-url "https://www.dropbox.com/home"))
+   ("X" (browse-url "https://www.xserver.ne.jp/login_server.php"))
+   ("D" (browse-url "https://www.dropbox.com/home"))
    ("q" (browse-url "https://qiita.com/minoruGH"))
-   ("8" (browse-url "https://photos.google.com/?pageId=none"))
-   ("k" (browse-url "https://keep.google.com/u/0/"))
+   ("P" (browse-url "https://photos.google.com/?pageId=none"))
+   ("K" (browse-url "https://keep.google.com/u/0/"))
    ("T" (browse-url "https://minorugh.tumblr.com"))
    ("w" (browse-url "https://tenki.jp/week/6/31/"))
    ("g" (browse-url "https://minorugh.github.io/"))
    ("p" (browse-url "https://getpocket.com/a/queue/"))
-   ("t" (browse-url "https://twitter.com/gospelhaiku"))
-   ("s" (browse-url "https://emacs-jp.slack.com/messages/C1B73BWPJ/"))
+   ("x" (browse-url "https://twitter.com/gospelhaiku"))
+   ("s" (shell-command "slack"))
    ("<muhenkan>" nil)
    ("." nil))
   :config
