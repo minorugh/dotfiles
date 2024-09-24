05c58f2ac5aa65cdd02fe1e86a42b00ac4567041
minorugh <minorugh@gmail.com>
2024-09-24 22:44:21 +0900

    Fix

--- .emacs.d/inits/41_dired-ut.el
+++ .emacs.d/inits/41_dired-ut.el
@@ -33,31 +33,7 @@ see https://gist.github.com/kobapan/28908b564b610bd3e6f3fae78637ac8b"
 see https://riptutorial.com/git/example/18336/gitk-and-git-gui"
     (interactive)
     (compile "gitk")
-    (delete-other-windows))
-
-  ;; -------------------------------------------------
-  ;; 書き込み権限のないファイル/ディレクトリを開くときに、自動的にsudoで開く
-  ;; sudoで開き直すときは、M+x sudo
-  ;; -------------------------------------------------
-  ;; (defun find-file--sudo (orig-fun &optional filename &rest r)
-  ;;   (if (and (not (file-writable-p filename)) ; 書き込み権限がなかったら
-  ;;            (y-or-n-p (concat filename " is read-only. Open it as root? "))) ; y だったら
-  ;; 	(sudo filename) ; /sudo:: で開く
-  ;;     (apply orig-fun `(,filename)) )) ; その他通常のfind-fileで開く
-  ;; (advice-add 'find-file :around #'find-file--sudo)
-
-  ;; (defun sudo (&optional file)
-  ;;   "Open read-only FILE with sudo."
-  ;;   (interactive)
-  ;;   (if file ; find-fileから呼ばれたら
-  ;; 	(find-file (concat "/sudo::" file)) ; /sudo:: で開く
-  ;;     (let ((pos (point)))
-  ;; 	(find-alternate-file ; /sudo:: で開き直す
-  ;; 	 (concat "/sudo::" (or (buffer-file-name) list-buffers-directory)))
-  ;; 	(goto-char pos))) ; カーソル位置復元
-  ;;   (rename-buffer (concat "sudo:" (buffer-name))))
-					; バッファ名の先頭にsudo:を付ける
-  )
+    (delete-other-windows)))
 
 
 ;; Local Variables:
