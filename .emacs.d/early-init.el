413c94c9bb4c0e288acef55541058ed5e9af8017
minorugh <minorugh@gmail.com>
2024-09-24 21:11:59 +0900

    Fix

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -7,17 +7,32 @@
 ;;; Code:
 ;; (setq debug-on-error t)
 
-;; Always load newest byte code
-(setq load-prefer-newer t)
-
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
+
+;; Prevent unwanted runtime compilation for native-comp users
+(setq native-comp-jit-compilation nil)
 
 ;; For slightly faster startup
 (setq package-enable-at-startup nil)
 
-;; Write any customizations to a temp file so they are discarded.
-(setq custom-file "~/.emacs.d/tmp/custom.el")
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Set language & System encoding
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;; Font
+(if (string-match "P1" (shell-command-to-string "uname -n"))
+    (add-to-list 'default-frame-alist '(font . "Cica-20"))
+  (add-to-list 'default-frame-alist '(font . "Cica-18")))
+
+;; Inhibit resizing frame
+(setq frame-inhibit-implied-resize t)
+(setq frame-resize-pixelwise t)
 
 ;; Disable warnings at initialization
 (eval-and-compile
@@ -30,11 +45,6 @@
 
 ;; Default frame settings.
 (push '(fullscreen . maximized) default-frame-alist)
-(push '(ns-transparent-titlebar . t) default-frame-alist)
-
-;; Inhibit resizing frame
-(setq frame-inhibit-implied-resize t)
-(setq frame-resize-pixelwise t)
 
 ;; Prevent flashing of unstyled modeline and headerline at startup
 (setq-default mode-line-format nil)
@@ -44,12 +54,14 @@
 (setq inhibit-splash-screen t)
 
 ;; Do not display anything until Emacs is fully started
+;; If there is no elpa directory nothing suppresses
 (when (file-directory-p "~/.emacs.d/elpa/")
   (add-hook 'window-setup-hook
 	    (lambda ()
 	      (setq inhibit-redisplay nil)
 	      (setq inhibit-message nil)
 	      (redisplay)))
+  (setq inhibit-redisplay t)
   (setq inhibit-message t)
   (custom-set-faces '(default ((t (:background "#282a36"))))))
 
