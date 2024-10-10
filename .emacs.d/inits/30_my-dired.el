7d8207c367073715f83e74f5dfd875036eeb47aa
Minoru Yamada <minorugh@gmail.com>
2024-10-11 05:55:23 +0900

    Fix

---
+++ .emacs.d/inits/30_my-dired.el
@@ -0,0 +1,71 @@
+;;; 30_my-dired.el --- Hydra quick dired configurations. -*- no-byte-compile: t; -*-
+;;; Commentary:
+;;; Code:
+;; (setq debug-on-error t)
+
+(leaf * quick-dired
+  :doc "Slection menu for quick access"
+  :bind ("M-." . hydra-dired/body)
+  :hydra
+  (hydra-dired
+   (:hint nil :exit t)
+   "
+   Quick.dired
+  _d_ropbox  _e_macs.d^^  _i_nits  root_/_  _s_rc  _._files  scale_+_  _z_illa  make._c__k__g_|_b__m__u_  _p_ages
+  _r_estart  magit_[__]_  _t_ramp  GH.._h_  _o_rg  _<home>_  _f_lychk  _P_assx  howm._,__;__@_|md_v_^^^^  _x_serv
+  "
+   ("i" my:inits-dir)
+   ("+" text-scale-adjust)
+   ("o" my:org-dir)
+   ("t" counsel-tramp)
+   ("q" my:tramp-quit)
+   ("<home>" my:home-dir)
+   ("d" my:dropbox)
+   ("D" chromium-dropbox)
+   ("." my:dotfiles-dir)
+   ("i" my:inits-dir)
+   ("e" my:emacs-dir)
+   ("h" my:gh-dir)
+   ("r" restart-emacs)
+   ("v" markdown-preview)
+   ("@" howm-list-all)
+   ("," my:howm-create-memo)
+   (";" my:howm-create-tech)
+   ("c" make-commit)
+   ("k" my:make-k)
+   ("g" my:make-git)
+   ("P" keepassxc)
+   ("p" my:pages-dir)
+   ("b" my:make-bklog)
+   ("m" my:make-move)
+   ("u" my:make-upsftp)
+   ("/" my:root-dir)
+   ("f" flycheck-list-errors)
+   ("_" delete-other-windows)
+   ("[" hydra-git/body)
+   ("]" my:magit-status)
+   ("s" my:scr-dir)
+   ("z" filezilla)
+   ("x" (browse-url "https://www.xserver.ne.jp/login_server.php"))
+   ("M-." hydra-work/body)
+   ("<muhenkan>" nil))
+  :init
+  (defun keepassxc ()
+    "Open keepassxc with auto passwd input."
+    (delete-other-windows))
+
+  (defun my:magit-status ()
+    "Display message if magit in dashboard."
+    (interactive)
+    (if (string= "*dashboard*" (buffer-name))
+	(message "Can't magit in Dashboard！")
+      (magit-status)))
+
+  (defun filezilla ()
+    "Open filezilla."
+    (interactive)
+    (compile "filezilla -s")
+    (delete-other-windows)))
+
+
+;;; 30_my-dired.el ends here
---
+++ .emacs.d/inits/40_neotree.el
@@ -0,0 +1,62 @@
+;;; 40_neotree.el --- Neotree configurations. -*- no-byte-compile: t; -*-
+;;; Commentary:
+;;; Code:
+;; (setq debug-on-error t)
+
+(leaf neotree :ensure t
+  :doc "Tree plugin like NerdTree for Vim"
+  :after projectile
+  :commands neotree-find
+  :bind (("<f10>" . my:neotree-find)
+	 (:neotree-mode-map
+	  ("RET"     . neotree-enter-hide)
+	  ("a"       . neotree-hidden-file-toggle)
+	  ("<left>"  . neotree-select-up-node)
+	  ("<right>" . neotree-change-root)
+	  ("<f10>"   . neotree-toggle)))
+  :init
+  (setq neo-keymap-style 'concise)
+  ;; conciseにするとキーバインドをシンプルにできる
+  ;;  C ルートディレクトリ変更
+  ;;  c 作成
+  ;;  + 作成
+  ;;  d 削除
+  ;;  r リネーム
+  ;;  e エンター
+  :config
+  (with-eval-after-load 'doom-themes
+    (doom-themes-neotree-config)) ;; need all-the-icons
+  (setq neo-mode-line-type nil)
+  (setq neo-create-file-auto-open t)
+  ;; (setq neo-theme 'nerd-icons)
+  (defun my:neotree-find ()
+    "Neotree-find with dimmer-off."
+    (interactive)
+    (dimmer-off)
+    (neotree-find))
+
+  (defun neotree-text-scale ()
+    "Neotree text scale.
+  see https://github.com/jaypei/emacs-neotree/issues/218"
+    (interactive)
+    (text-scale-adjust 0)
+    (text-scale-decrease 1)
+    (message nil))
+  (add-hook 'neo-after-create-hook
+	    (lambda (_)
+	      (call-interactively 'neotree-text-scale)))
+
+  (defun neo-open-file-hide (full-path &optional arg)
+    "Open a file node and hides tree."
+    (neo-global--select-mru-window arg)
+    (find-file full-path)
+    (neotree-hide)
+    (dimmer-on))
+
+  (defun neotree-enter-hide (&optional arg)
+    "Enters file and hides neotree directly"
+    (interactive "P")
+    (neo-buffer--execute arg 'neo-open-file-hide 'neo-open-dir)))
+
+
+;;; 40_neotree.el ends here
