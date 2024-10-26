723592b4078124b1876c53b5e49f801963c587d8
Minoru Yamada <minorugh@gmail.com>
2024-10-26 21:24:30 +0900

    Deleted tex

--- tex/dvpd-mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 生成されたPDFをPreview.appで開く
-name=$1
-
-dvipdfmx $1 && evince ${name%.*}.pdf
-
-rm *.aux *.dvi *.log
--- tex/dvpd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 生成されたPDFをevinceで開く
-name=$1
-
-dvipdfmx $1 && evince ${name%.*}.pdf
-
-rm *.au* *.dv* *.lo*
