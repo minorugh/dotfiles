2aa4dca35fc9bdc49576c16e55eae48211dd8a3f
Minoru Yamada <minorugh@gmail.com>
2024-12-01 07:54:01 +0900

    Fix comment

--- .emacs.d/inits/20_misc.el
+++ .emacs.d/inits/20_misc.el
@@ -6,7 +6,6 @@
 (leaf quickrun :ensure t
   :bind ([f6] . quickrun))
 
-
 (leaf key-chord :ensure t
   :hook (after-init-hook . key-chord-mode)
   :chord (("df" . counsel-descbinds)
@@ -15,16 +14,13 @@
   (setq key-chord-two-keys-delay 0.1)
   :hook after-init-hook)
 
-
 (leaf expand-region :ensure t
   :bind ("C-@" . er/expand-region))
-
 
 (leaf undo-fu :ensure t
   :doc "Undo helper with redo"
   :bind (("C-_" . undo-fu-only-undo)
 	 ("C-/" . undo-fu-only-redo)))
-
 
 (leaf undohist :ensure t
   :doc "Persistent undo history"
@@ -33,20 +29,17 @@
   (setq undohist-directory     "~/.emacs.d/tmp/undohist")
   (setq undohist-ignored-files '("/tmp/" "COMMIT_EDITMSG")))
 
-
 (leaf which-key :ensure t
   :doc "Display available keybindings in popup"
   :config
   (setq which-key-max-description-length 40)
   :hook after-init-hook)
 
-
 (leaf projectile :ensure t
   :doc "Manage and navigate projects in Emacs"
   :config
   (setq projectile-known-projects-file "~/.emacs.d/tmp/projectile.eld")
   :hook after-init-hook)
-
 
 (leaf prescient :ensure t
   :doc "Better sorting and filtering"
@@ -57,22 +50,18 @@
   (leaf ivy-prescient :ensure t :global-minor-mode t)
   (leaf company-prescient :ensure t :global-minor-mode t))
 
-
 (leaf popwin :ensure t
   :doc "popup window manager for Emacs"
   :hook after-init-hook)
-
 
 (leaf aggressive-indent :ensure t
   :doc "Minor mode to aggressively keep your code always indented"
   :hook (emacs-lisp-mode-hook css-mode-hook))
 
-
-(leaf atomic-chrome
-  :ensure t
+(leaf atomic-chrome :ensure t
+  :doc "Edit text areas of the browser in Emacs"
   :hook (after-init-hook . atomic-chrome-start-server)
   :custom (atomic-chrome-buffer-open-style . 'full))
-
 
 (leaf iedit :ensure t
   :doc "Edit multiple occurrences in the same way simultaneously"
