70dca6841c6242976d5a1b35cbf0155b68f74a38
Minoru Yamada <minorugh@gmail.com>
2025-05-10 06:46:46 +0900

    Fix keybind

--- .emacs.d/inits/04_swiper.el
+++ .emacs.d/inits/04_swiper.el
@@ -5,7 +5,8 @@
 
 (leaf swiper :ensure t
   :doc "Isearch with an overview"
-  :bind ("C-s" . swiper-region)
+  :bind (("C-s" . swiper-region)
+	 ("C-r" . swiper-thing-at-point))
   :config
   (defun swiper-region ()
     "If region is selected, `swiper-thing-at-point'.
