e104363a51ed74b4b85c414be587dc2bed573967
Minoru Yamada <minorugh@gmail.com>
2025-05-10 06:32:02 +0900

    Add avy

--- .emacs.d/inits/05_counsel.el
+++ .emacs.d/inits/05_counsel.el
@@ -18,6 +18,9 @@
 	 ("C-x C-f" . counsel-find-file)
 	 ("C-x C-r" . counsel-recentf))
   :init
+  (leaf avy :ensure t
+    :doc "Jump to arbitrary positions."
+    :bind ("C-r" . avy-goto-word-1))
   (leaf ivy-rich :ensure t
     :doc "More friendly display transformer for ivy"
     :hook after-init-hook)
