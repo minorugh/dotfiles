28cddb6c6d9375438910ba50b7f3a0894a6f4088
Minoru Yamada <minorugh@gmail.com>
2025-05-10 11:55:21 +0900

    Fix

--- .emacs.d/inits/05_counsel.el
+++ .emacs.d/inits/05_counsel.el
@@ -19,7 +19,7 @@
 	 ("C-x C-r" . counsel-recentf))
   :init
   (leaf avy :ensure t
-    :doc "Jump to arbitrary positions quicly.")
+    :doc "Jump to arbitrary positions quickly.")
   (leaf ivy-rich :ensure t
     :doc "More friendly display transformer for ivy"
     :hook after-init-hook)
