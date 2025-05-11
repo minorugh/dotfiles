8d84f726e49f100eb179e2014d5d0e2e6ee1ebad
Minoru Yamada <minorugh@gmail.com>
2025-05-12 06:41:11 +0900

    Fix

--- .emacs.d/inits/08_dimmer.el
+++ .emacs.d/inits/08_dimmer.el
@@ -11,7 +11,7 @@
 	 (minibuffer-exit-hook  . dimmer-on))
   :config
   (setq dimmer-buffer-exclusion-regexps
-	'("^ \\*compilation*\\|\\*YaTeX-typesetting*\\|\\*Google Translate*\\|\\*gt-result*"))
+	'("^ \\*compilation*\\|\\*YaTeX-typesetting*\\|\\*gt-result*"))
   (setq dimmer-fraction 0.5)
 
   (defun dimmer-excludes ()
