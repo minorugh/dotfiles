a3d149ae9af84d65af21ad4a5539bcc4c7a85415
Minoru Yamada <minorugh@gmail.com>
2025-06-11 07:59:14 +0900

    Fix

--- .emacs.d/inits/06-dimmer.el
+++ .emacs.d/inits/06-dimmer.el
@@ -19,6 +19,7 @@
     "Settings to suppress the dimmer."
     (interactive)
     (dimmer-configure-which-key)
+    (dimmer-configure-magit)
     (dimmer-configure-hydra)
     (dimmer-configure-org))
 
