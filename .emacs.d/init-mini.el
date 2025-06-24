e9ec28f8d74ea36229d4468cfb57e08996b474f8
Minoru Yamada <minorugh@gmail.com>
2025-06-24 22:51:23 +0900

    Fix

--- .emacs.d/init-mini.el
+++ .emacs.d/init-mini.el
@@ -74,11 +74,11 @@
               tab-width        4
               indent-tabs-mode nil)
 
-;; (global-hl-line-mode 1)
+(global-hl-line-mode 1)
 
-;; (if (fboundp 'display-line-numbers-mode)
-;;     (global-display-line-numbers-mode 1)
-;;   (global-linum-mode 1))
+(if (fboundp 'display-line-numbers-mode)
+    (global-display-line-numbers-mode 1))
+;;   (global-linum-mode 1)
 
 ;; Basic modes
 (show-paren-mode 1)
