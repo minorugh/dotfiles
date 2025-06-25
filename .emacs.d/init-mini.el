eaad0de2ae91e34ef693b9ca57b5e4179ec5efda
Minoru Yamada <minorugh@gmail.com>
2025-06-26 08:26:37 +0900

    Fix

--- .emacs.d/init-mini.el
+++ .emacs.d/init-mini.el
@@ -1,113 +1,80 @@
 ;;; init-mini.el --- Emacs minimal configurations.	-*- lexical-binding: t no-byte-compile: t -*-
-;;
 ;;; Commentary:
-;;
 ;; This will start with typing `eq' at shell with minimal Emacs.
 ;; Write below at .zshrc or .bashrc.
 ;; alias eq="emacs -q -l ~/.emacs.d/init-mini.el"
 ;; Use when test of package and my Emacs don't start.
 ;;; Code:
+(package-initialize)
 
-;; UI
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 (set-frame-parameter nil 'fullscreen 'maximized)
 (load-theme 'misterioso t)
 
-;; Load path
-(push (expand-file-name "site-lisp" user-emacs-directory) load-path)
-(push (expand-file-name "lisp" user-emacs-directory) load-path)
+;; Save the file specified code with basic utf-8 if it exists
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(add-to-list 'default-frame-alist '(font . "Cica-18"))
+
+;;; Faster rendering by not corresponding to right-to-left language
+(setq-default bidi-display-reordering nil)
+;; Do not make a backup file like *.~
+(setq make-backup-files nil)
+;; Do not use auto save
+(setq auto-save-default nil)
+;; Do not create lock file
+(setq create-lockfiles nil)
+;; When the mouse cursor is close to the text cursor, the mouse hangs away
+(if (display-mouse-p) (mouse-avoidance-mode 'exile))
+;; Disable automatic save
+(setq auto-save-default nil)
+;; Do not distinguish uppercase and lowercase letters on completion
+(setq completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+;; Make it easy to see when it is the same name file
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+;; Display the character position of the cursor
+(column-number-mode t)
+;; Increase history count
+(setq history-length 10000)
+;; Save history of minibuffer
+(savehist-mode 1)
+;; Make "yes or no" "y or n"
+(fset 'yes-or-no-p 'y-or-n-p)
+;; Turn off warning sound screen flash
+(setq visible-bell nil)
+;; All warning sounds and flash are invalid (note that the warning sound does not sound completely)
+(setq ring-bell-function 'ignore)
+;; Brace the corresponding parentheses
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(setq show-paren-style 'parenthesis)
+;; Highlight the space at the end of the line
+(setq-default show-trailing-whitespace t)
+;; Do not change the position of the cursor on the screen as much as possible when scrolling pages
+(setq scroll-preserve-screen-position t)
+
+;; Use fido-mode
+(fido-mode 1)
+(fido-vertical-mode 1)
+(setq dired-listing-switches "-AFl --group-directories-first")
 (setq default-directory user-emacs-directory)
 
-;; Packages
-(setq package-archives
-      '(("gnu"   . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.org/packages/")))
-
-;; Encoding
-(prefer-coding-system 'utf-8)
-
-;; Better defaults
-(setq uniquify-buffer-name-style 'post-forward-angle-brackets) ; Show path if names are same
-(setq adaptive-fill-regexp "[ t]+|[ t]*([0-9]+.|*+)[ t]*")
-(setq adaptive-fill-first-line-regexp "^* *$")
-(setq delete-by-moving-to-trash t)         ;; Deleting files go to OS's trash folder
-(setq make-backup-files nil)               ;; Forbide to make backup files
-(setq auto-save-default nil)               ;; Disable auto save
-(setq set-mark-command-repeat-pop t)       ;; Repeating C-SPC after popping mark pops it again
-(setq dired-listing-switches "-AFl --group-directories-first") ;; Diectry first
-
-(setq-default major-mode 'text-mode)
-
-(setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
-(setq sentence-end-double-space nil)
-
-(global-hl-line-mode 1)
-;; (global-display-line-numbers-mode 1)
-
-;; Basic modes
-(show-paren-mode 1)
-(delete-selection-mode 1)
-(global-auto-revert-mode 1)
-(recentf-mode 1)
-(setq auto-save-default nil)
-(when (fboundp 'savehist-mode)
-  (savehist-mode 1))
-(if (fboundp 'save-place-mode)
-    (save-place-mode 1)
-  (require 'saveplace)
-  (setq-default save-place t))
-(setq savehist-file "~/.emacs.d/tmp/history")
-(setq save-place-file "~/.emacs.d/tmp/places")
-(setq recentf-save-file "~/.emacs.d/tmp/recentf-test")
-
-(setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
-(electric-pair-mode 1)
-
-(add-hook 'prog-mode-hook #'subword-mode)
-(add-hook 'minibuffer-setup-hook #'subword-mode)
-
-;; Change to short command
-(defalias 'yes-or-no-p 'y-or-n-p)
-(defalias 'exit 'save-buffers-kill-emacs)
-
-;; Completion
-(when (fboundp 'global-completion-preview-mode)
-  (global-completion-preview-mode 1))
-
-;; fido-mode
-(when (fboundp 'fido-mode)
-  (fido-mode 1)
-  (fido-vertical-mode 1)
-  (defun fido-recentf-open ()
-    "Use `completing-read' to find a recent file."
-    (interactive)
-    (if (find-file (completing-read "Find recent file: " recentf-list))
-        (message "Opening file...")
-      (message "Aborting"))))
-(define-key global-map (kbd "C-x r") 'fido-recentf-open)
-
-(defun my:keyboard-quit ()
-  "Hoge."
-  (interactive)
-  (if (not (use-region-p))
-      (minibuffer-keyboard-quit)
-    (keyboard-quit)))
-
+;; Use the X11 clipboard
+(setq select-enable-clipboard  t)
+(define-key global-map (kbd "M-w") 'clipboard-kill-ring-save)
+(define-key global-map (kbd "C-w") 'clipboard-kill-region)
 ;; Key Modifiers
-(define-key global-map (kbd "s-a")   #'mark-whole-buffer)
-(define-key global-map (kbd "s-v")   #'yank)
-(define-key global-map (kbd "s-c")   #'kill-ring-save)
-(define-key global-map (kbd "s-s")   #'save-buffer)
 (define-key global-map (kbd "M-/")   #'kill-buffer)
 (define-key global-map (kbd "C-_")   #'undo)
 (define-key global-map (kbd "C-/")   #'undo-redo)
 (define-key global-map (kbd "C-:")   #'switch-to-buffer)
 (define-key global-map (kbd "C-x f") #'find-file)
 (define-key global-map (kbd "C-x j") #'dired-jump)
-(define-key global-map (kbd "<muhenkan>") #'my:keyboard-quit)
 
 ;;; Init-mini.el ends here
