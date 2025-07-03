0b894b33e65a9d8c94ca928193f3b17db4b69e83
Minoru Yamada <minorugh@gmail.com>
2025-07-04 08:23:54 +0900

    Fix

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -46,7 +46,7 @@
       (setq inhibit-message t)
       (custom-set-faces '(default ((t (:background "#282a36"))))))
   (add-hook 'window-setup-hook
-            (lambda ()
+	        (lambda ()
 	          "Restart Emacs after a clean install of packages."
 	          (delete-file "~/.emacs.d/projects")
 	          (restart-emacs))))
