4ee65169241f84d5d66be942eac6e14438e50bf5
Minoru Yamada <minorugh@gmail.com>
2025-10-27 16:11:30 +0900

    Fix

--- .emacs.d/inits/40-hydra-menu.el
+++ .emacs.d/inits/40-hydra-menu.el
@@ -12,7 +12,7 @@
    "
    Work.menu
   _d_:日記  _m_:毎日  _w_:若鮎  _t_:定例  _[__]_:創作  _e_:hugo^^  _p_rint.r_e_  _r_ainbow  yas._n_._v_._i_  _c_ap._u_p.d_o_wn
-  _a_:合評  _f_:週秀  _s_:吟行  _k_:近詠  _y__,_:年度  _g_ist._B_  Rm_@_point^^  _:_thunar  _b_ackupall^^^^  _+_.scale-adj^^^^
+  _a_:合評  _f_:週秀  _s_:吟行  _k_:近詠  _y__,_:年度  _g_ist._B_  Browse_@_p^^  _:_thunar  _b_ackupall^^^^  _+_.scale-adj^^^^
 "
    ("+" text-scale-adjust)
    ("c" my:capitalize-word)
