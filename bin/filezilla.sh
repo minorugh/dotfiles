8b2e3a854c04238fe749ca6827bfa38db3ea70ce
Minoru Yamada <minorugh@gmail.com>
2026-03-10 08:23:37 +0900

    Fix

--- bin/filezilla.sh
+++ bin/filezilla.sh
@@ -3,6 +3,10 @@
 # Inherit SSH_AUTH_SOCK from keychain
 source ~/.keychain/$(hostname)-sh 2>/dev/null
 
+# Debug
+echo "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" > /tmp/fzilla-debug.log
+ls -la $SSH_AUTH_SOCK >> /tmp/fzilla-debug.log 2>&1
+
 filezilla "$@" &
 
 exit
