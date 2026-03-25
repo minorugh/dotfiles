44de183bf5dc2fbd35dda1d5eae145432a3a076a
Minoru Yamada <minorugh@gmail.com>
2026-03-25 17:27:32 +0900

    Fix

--- bin/keepass.sh
+++ bin/keepass.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-secret-tool lookup type kdb | keepassxc --pw-stdin ~/Dropbox/backup/passwd/keypassX/20191105.kdbx
+# secret-tool lookup type kdb | keepassxc --pw-stdin ~/Dropbox/backup/passwd/keypassX/20191105.kdbx
+secret-tool lookup type kdb | keepassxc --pw-stdin ~/Dropbox/backup/keypassX/20191105.kdbx
 
- exit
+exit
