89e9667f37d89db1f598a24373c2fb29a802f6b8
Minoru Yamada <minorugh@gmail.com>
2026-03-29 21:55:07 +0900

    manual 2026-03-29 21:55:07

--- .emacs.d/inits/30-ui.el
+++ .emacs.d/inits/30-ui.el
@@ -10,6 +10,22 @@
   (setq doom-themes-enable-italic nil)
   (doom-themes-org-config))
 
+(eval-after-load "diff-mode"
+  '(progn
+     ;; 追加された行の色（薄い緑）
+     (set-face-attribute 'diff-added nil
+                         :foreground "green4" :background "white")
+     ;; 削除された行の色（薄い赤）
+     (set-face-attribute 'diff-removed nil
+                         :foreground "red4" :background "white")
+     ;; ヘッダー部分の色
+     (set-face-attribute 'diff-header nil
+                         :foreground "blue" :background "lightgrey")
+     (set-face-attribute 'diff-file-header nil
+                         :foreground "blue" :background "lightgrey")))
+
+;; (setq diff-font-lock-keywords nil)
+
 (leaf hl-line :ensure nil
   :tag "builtin"
   :doc "Highlight the current line."
@@ -19,17 +35,21 @@
   :config
   (custom-set-faces
    '(region  ((t (:background "#6272a4" :extend t))))
-   '(hl-line ((t (:background "#3B4252" :extend t))))))
+   '(hl-line ((t (:background "#3B4252" :extend t))))
+   ;; diff-mode の削除行を見やすく
+   ;; '(diff-removed ((t (:foreground "#ff8080" :background "#3d1f1f"))))
+   ;; '(diff-indicator-removed ((t (:foreground "#ff5555"))))
+   ))
 
-(leaf doom-modeline :ensure t
-  :doc "A minimal and modern mode-line."
-  :hook (after-init-hook . doom-modeline-mode)
-  :config
-  (setq doom-modeline-icon            t)
-  (setq doom-modeline-major-mode-icon nil)
-  (setq doom-modeline-minor-modes     nil)
-  (line-number-mode   0)
-  (column-number-mode 0))
+   (leaf doom-modeline :ensure t
+     :doc "A minimal and modern mode-line."
+     :hook (after-init-hook . doom-modeline-mode)
+     :config
+     (setq doom-modeline-icon            t)
+     (setq doom-modeline-major-mode-icon nil)
+     (setq doom-modeline-minor-modes     nil)
+     (line-number-mode   0)
+     (column-number-mode 0))
 
 (leaf hide-mode-line :ensure t
   :doc "Hides the mode-line in current buffer."
