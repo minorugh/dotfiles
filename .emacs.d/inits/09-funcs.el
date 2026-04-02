dce0a33603b4f9d6d3c109c8a596cbb90e06d611
Minoru Yamada <minorugh@gmail.com>
2026-04-02 19:48:16 +0900

    auto: 2026-04-02 19:48:16

--- .emacs.d/inits/09-funcs.el
+++ .emacs.d/inits/09-funcs.el
@@ -19,49 +19,6 @@
 (setq compilation-always-kill t)
 (setq compilation-finish-functions #'compile-autoclose)
 
-(leaf git-peek
-  :vc (:url "https://github.com/minorugh/git-peek")
-  :config
-  ;; git-peek: preview window height ratio (1.0 = full height)
-  (setq git-peek-preview-height 1.0)
-
-  (defun git-peek-emergency-quit ()
-    "Force quit git-peek session and restore the previous window configuration.
-Restores modeline color, re-enables dimmer-mode if it was active, and kills
-all git-peek buffers."
-    (interactive)
-    (when git-peek--preview-modeline-cookie
-      (set-face-background 'mode-line git-peek--modeline-color-default)
-      (setq git-peek--preview-modeline-cookie nil))
-    (when (and git-peek--dimmer-was-on (fboundp 'dimmer-mode))
-      (dimmer-mode 1))
-    (when git-peek--saved-wconf
-      (set-window-configuration git-peek--saved-wconf)
-      (setq git-peek--saved-wconf nil))
-    (dolist (bname '("*git-peek-commits*" "*git-peek-preview*"))
-      (when (get-buffer bname) (kill-buffer bname)))
-    (message "git-peek: emergency quit")))
-
-
-(defun my-muhenkan ()
-  "Muhenkan key handler: toggle evil state or rescue from any situation.
-In evil normal state: switch to insert state.
-Otherwise: git-peek quit → minibuffer quit (top-level fallback)
-→ deactivate mark → deactivate input method → return to evil normal state."
-  (interactive)
-  (cond
-   ((get-buffer "*git-peek-commits*") (git-peek-emergency-quit))
-   ((minibuffer-window-active-p (selected-window))
-    (minibuffer-keyboard-quit)
-    (when (minibuffer-window-active-p (selected-window))
-      (top-level)))
-   ((evil-normal-state-p) (evil-insert-state))
-   ((use-region-p) (deactivate-mark))
-   (current-input-method (deactivate-input-method))
-   (t (evil-normal-state) (message "-- NORMAL --"))))
-
-(bind-key "<muhenkan>" #'my-muhenkan)
-
 
 ;;; ps-print / ps-mule (builtin)
 ;; https://tam5917.hatenablog.com/entry/20120914/1347600433
@@ -101,13 +58,6 @@ If region isn't selected, post from the buffer."
       (compile (concat "gist -oPd " (gist-description) " -f " (gist-filename)))))
   (delete-other-windows))
 
-(defun my-tig ()
-  "Open tig in the current directory's git repository with gnome-terminal."
-  (interactive)
-  (let ((root (or (locate-dominating-file default-directory ".git")
-                  (error "Git repository not found"))))
-    (start-process "tig" nil "gnome-terminal" "--" "bash" "-c"
-                   (format "cd %s && tig; exec bash" root))))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
