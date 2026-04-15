ee8f21a81f6209c68acd3be89362ef60283fe3ab
Minoru Yamada <minorugh@gmail.com>
2026-04-15 14:14:22 +0900

    auto: 2026-04-15 14:14:22

--- .emacs.d/inits/20-region-action.el
+++ .emacs.d/inits/20-region-action.el
@@ -12,41 +12,48 @@
   :lighter " [SEL]"
   :keymap my-selected-mode-map)
 
-(leaf my-region-actions
-  :doc "Key bindings valid only during region selection"
-  :tag "builtin"
-  :config
-  (bind-key ";" 'comment-dwim     my-selected-mode-map)
-  (bind-key "c" 'kill-ring-save   my-selected-mode-map)
-  (bind-key "g" 'my-google-search my-selected-mode-map)
-  (bind-key "w" 'my-weblio-search my-selected-mode-map)
-  (bind-key "d" 'deepl-translate  my-selected-mode-map)
+(defun my-region-actions-initialize ()
+  "Load region actions on first region selection."
+  (remove-hook 'activate-mark-hook #'my-region-actions-initialize)
 
-  (defun my-get-region-or-word ()
-    (if (use-region-p)
-        (buffer-substring-no-properties (region-beginning) (region-end))
-      (thing-at-point 'word t)))
+  (leaf my-region-actions
+    :doc "Key bindings valid only during region selection"
+    :tag "builtin"
+    :config
+    (bind-key ";" 'comment-dwim     my-selected-mode-map)
+    (bind-key "c" 'kill-ring-save   my-selected-mode-map)
+    (bind-key "g" 'my-google-search my-selected-mode-map)
+    (bind-key "w" 'my-weblio-search my-selected-mode-map)
+    (bind-key "d" 'deepl-translate  my-selected-mode-map)
 
-  (defun my-google-search (str)
-    (interactive (list (my-get-region-or-word)))
-    ;; ?q=%s を追加することで検索ワードがGoogleに渡ります
-    (browse-url (format "https://google.com/search?q=%s" (url-hexify-string str))))
+    (defun my-get-region-or-word ()
+      (if (use-region-p)
+          (buffer-substring-no-properties (region-beginning) (region-end))
+        (thing-at-point 'word t)))
 
-  (defun my-weblio-search (str)
-    (interactive (list (my-get-region-or-word)))
-    (browse-url (format "https://www.weblio.jp/content/%s" (url-hexify-string str))))
+    (defun my-google-search (str)
+      (interactive (list (my-get-region-or-word)))
+      (browse-url (format "https://google.com/search?q=%s" (url-hexify-string str))))
 
-  (defun my-selected-mode-update ()
-    (if (use-region-p)
-        (unless my-selected-mode (my-selected-mode 1))
-      (when my-selected-mode (my-selected-mode -1))))
+    (defun my-weblio-search (str)
+      (interactive (list (my-get-region-or-word)))
+      (browse-url (format "https://www.weblio.jp/content/%s" (url-hexify-string str))))
 
-  ;; Monitor region changes (selection start/stop) and switch modes
-  (add-hook 'post-command-hook    #'my-selected-mode-update)
-  (add-hook 'activate-mark-hook
-	    (lambda () (setq my-ime-flag current-input-method)(deactivate-input-method)))
-  (add-hook 'deactivate-mark-hook
-	    (lambda () (unless (null my-ime-flag)(toggle-input-method)))))
+    (defun my-selected-mode-update ()
+      (if (use-region-p)
+          (unless my-selected-mode (my-selected-mode 1))
+        (when my-selected-mode (my-selected-mode -1))))
+
+    (add-hook 'post-command-hook    #'my-selected-mode-update)
+    (add-hook 'activate-mark-hook
+              (lambda () (setq my-ime-flag current-input-method) (deactivate-input-method)))
+    (add-hook 'deactivate-mark-hook
+              (lambda () (unless (null my-ime-flag) (toggle-input-method)))))
+
+  (setq my-ime-flag current-input-method)
+  (deactivate-input-method))
+
+(add-hook 'activate-mark-hook #'my-region-actions-initialize)
 
 
 ;; Local Variables:
