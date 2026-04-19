71fd32d21215de1739e9bec697b613bf62e07f23
Minoru Yamada <minorugh@gmail.com>
2026-04-20 08:38:14 +0900

    auto: 2026-04-20 08:38:14

--- .emacs.d/inits/50-dired.el
+++ .emacs.d/inits/50-dired.el
@@ -50,45 +50,48 @@
     "In dired, open the file in associated application."
     (interactive)
     (let* ((file (dired-get-filename nil t)))
-      (call-process "xdg-open" nil 0 nil file)))
+      (call-process "xdg-open" nil 0 nil file))))
 
-  (defun my-sudo-reopen ()
-    "Reopen current file with sudo privileges via TRAMP."
-    (interactive)
-    (let ((pos (point)))
-      (find-alternate-file (concat "/sudo:localhost:" (buffer-file-name)))
-      (goto-char pos)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Dired expanded function
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun my-sudo-reopen ()
+  "Reopen current file with sudo privileges via TRAMP."
+  (interactive)
+  (let ((pos (point)))
+    (find-alternate-file (concat "/sudo:localhost:" (buffer-file-name)))
+    (goto-char pos)))
 
-  (defun my-open-tig ()
-    "Run tig for current context in gnome-terminal."
-    (interactive)
-    (let* ((path (or (and (derived-mode-p 'dired-mode)
-                          (dired-get-filename nil t))
-                     (buffer-file-name)
-                     default-directory))
-           (dir  (if (file-directory-p path)
-                     path
-                   (file-name-directory path)))
-           (root (locate-dominating-file dir ".git")))
-      (if root
-          (start-process
-           "tig" nil
-           "gnome-terminal"
-           "--maximize"
-           "--working-directory" dir
-           "--"
-           "bash" "-c"
-           (format "tig %s" (shell-quote-argument path)))
-	(message "Not in a Git repo"))))
+(defun my-open-tig ()
+  "Run tig for current context in gnome-terminal."
+  (interactive)
+  (let* ((path (or (and (derived-mode-p 'dired-mode)
+                        (dired-get-filename nil t))
+                   (buffer-file-name)
+                   default-directory))
+         (dir  (if (file-directory-p path)
+                   path
+                 (file-name-directory path)))
+         (root (locate-dominating-file dir ".git")))
+    (if root
+        (start-process
+         "tig" nil
+         "gnome-terminal"
+         "--maximize"
+         "--working-directory" dir
+         "--"
+         "bash" "-c"
+         (format "tig %s" (shell-quote-argument path)))
+      (message "Not in a Git repo"))))
 
-  (defun my-sxiv ()
-    "Open images in current directory with sxiv (fullscreen)."
-    (interactive)
-    (let* ((files (directory-files default-directory nil
-                                   "\\.\\(jpe?g\\|png\\|gif\\|bmp\\)$"))
-           (cmd (format "sxiv -t -f %s"
-			(mapconcat #'shell-quote-argument files " "))))
-      (start-process-shell-command "sxiv" nil cmd))))
+(defun my-sxiv ()
+  "Open images in current directory with sxiv (fullscreen)."
+  (interactive)
+  (let* ((files (directory-files default-directory nil
+                                 "\\.\\(jpe?g\\|png\\|gif\\|bmp\\)$"))
+         (cmd (format "sxiv -t -f %s"
+		      (mapconcat #'shell-quote-argument files " "))))
+    (start-process-shell-command "sxiv" nil cmd)))
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
