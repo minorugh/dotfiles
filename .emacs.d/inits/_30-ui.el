1cfb5dfe9f1eece4e3149dcb987338406e68477a
Minoru Yamada <minorugh@gmail.com>
2026-05-27 21:15:41 +0900

    auto: 2026-05-27 21:15:41

--- .emacs.d/inits/_30-ui.el
+++ .emacs.d/inits/_30-ui.el
@@ -41,7 +41,53 @@
   (setq doom-modeline-major-mode-icon nil)
   (setq doom-modeline-minor-modes     nil)
   (line-number-mode   0)
-  (column-number-mode 0))
+  (column-number-mode 0)
+
+  (defvar my-modeline-default-bg nil
+    "Default mode-line background color captured after theme load.")
+
+  (defvar my-modeline-default-box nil
+    "Default mode-line box attribute captured after theme load.")
+
+  (defun my-modeline-capture-defaults ()
+    "Capture default mode-line face attributes after theme initialization."
+    (setq my-modeline-default-bg  (face-background 'mode-line nil t))
+    (setq my-modeline-default-box (face-attribute  'mode-line :box nil t)))
+
+  (defun my-modeline-popup-window-p (w)
+    "Return non-nil if W is a popup window that should not count as a real split.
+Excludes minibuffer, hydra, lv, and Flymake diagnostics windows."
+    (or (window-minibuffer-p w)
+        (string-match-p "\\*hydra\\|lv\\|\\*Flymake"
+                        (buffer-name (window-buffer w)))))
+
+  (defun my-update-modeline-for-split ()
+    "Highlight active mode-line when 2 or more real windows are shown.
+Popup windows such as minibuffer, hydra, lv, and Flymake diagnostics
+are excluded from the window count."
+    (run-with-idle-timer 0.1 nil
+      (lambda ()
+        (let ((wins (cl-count-if-not #'my-modeline-popup-window-p
+                                     (window-list))))
+          (if (> wins 1)
+              (progn
+                (set-face-attribute 'mode-line nil
+                                    :background "#44475a"
+                                    :box '(:line-width 2 :color "#bd93f9"))
+                (set-face-attribute 'doom-modeline-bar nil
+                                    :background "#bd93f9"))
+            (when (and my-modeline-default-bg my-modeline-default-box)
+              (set-face-attribute 'mode-line nil
+                                  :background my-modeline-default-bg
+                                  :box my-modeline-default-box)
+              (set-face-attribute 'doom-modeline-bar nil
+                                  :background "#bd93f9")))))))
+
+  (add-hook 'doom-modeline-mode-hook
+            (lambda ()
+              (run-with-idle-timer 1 nil #'my-modeline-capture-defaults)))
+
+  (add-hook 'window-configuration-change-hook #'my-update-modeline-for-split))
 
 (leaf nerd-icons
   :ensure t
