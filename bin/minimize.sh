ce218c8c27e97d5849a2509e6bd18fbe80e5a43b
Minoru Yamada <minorugh@gmail.com>
2026-05-30 08:48:09 +0900

    auto: 2026-05-30 08:48:09

--- bin/minimize.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xdotool search --name "NeoMutt Mail" windowminimize %@ > /dev/null 2>&1
--- bin/neomutt.sh
+++ bin/neomutt.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# 'mail' という名前の tmux セッションがあるか確認
 tmux has-session -t mail 2>/dev/null
 
 if [ $? -ne 0 ]; then
-    # セッションがなければ、~/Downloads に移動してから裏で NeoMutt を起動
     cd ~/Downloads && tmux new-session -d -s mail 'neomutt'
+    tmux set -t mail status off  # ここで直接設定
 fi
 
-# 余計なオプションを全削除。元の確実な起動コマンドに戻す
 gnome-terminal --maximize --title="NeoMutt Mail" -- tmux attach -t mail
 
 exit
+
