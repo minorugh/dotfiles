1cb980a6d5df81a921027c9a84e7f15f4f6763d8
Minoru Yamada <minorugh@gmail.com>
2026-06-01 07:07:16 +0900

    auto: 2026-06-01 07:07:16

--- bin/neomutt-watch.sh
+++ bin/neomutt-watch.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 #######################################################################
 ## neomutt-watch.sh
-## neomutt (tmux セッション) の死活監視スクリプト
+## NeoMutt tmux セッション 強制リフレッシュスクリプト
 ##
-## cron から呼ばれ、tmux セッションが死んでいれば再作成する。
-## 再起動した場合のみ /tmp/cron.log に日時を記録する。
+## cron から呼ばれ、夜間 IMAP 接続切れで固まるのを防ぐため
+## tmux セッションを無条件で再作成する。
+## 実行のたびに /tmp/cron.log に日時を記録する。
 ##
 ## cron 設定：
-##   */5 5,6,7 * * * /usr/local/bin/neomutt-watch.sh >> /tmp/cron.log 2>&1
+##   */5 5,6,7 * * * /usr/local/bin/neomutt-watch.sh
 ##
 #######################################################################
-if ! tmux has-session -t mail 2>/dev/null; then
-    cd ~/Downloads && tmux new-session -d -s mail 'neomutt'
-    tmux set -t mail status off
-    echo "$(date '+%Y-%m-%d %H:%M:%S'): neomutt restarted" >> /tmp/cron.log
-fi
+
+tmux kill-session -t mail 2>/dev/null
+
+cd ~/Downloads && tmux new-session -d -s mail 'neomutt'
+tmux set -t mail status off
+echo "[neomutt-watch] END: $(date '+%Y-%m-%d %H:%M:%S') (OK)" >> /tmp/cron.log
+
+
--- bin/neomutt.sh
+++ bin/neomutt.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+#######################################################################
+## neomutt.sh
+## NeoMutt 起動スクリプト
+##
+## tmux セッション 'mail' がなければ新規作成して neomutt を起動。
+## あれば既存セッションに attach する。
+## xfce4-terminal で最大化表示。
+##
+## 起動方法：
+##   - Xfce キーバインド C-z
+##   - Emacs hydra m キー
+##
+#######################################################################
 
-if ! tmux has-session -t mail 2>/dev/null; then
+tmux has-session -t mail 2>/dev/null
+
+if [ $? -ne 0 ]; then
     cd ~/Downloads && tmux new-session -d -s mail 'neomutt'
     tmux set -t mail status off
 fi
@@ -8,3 +23,4 @@ fi
 xfce4-terminal --maximize --title="NeoMutt Mail" -e "tmux attach -t mail"
 
 exit
+
