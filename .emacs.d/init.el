7a792b27e6a300a336c7d294641f0a5c0f38d954
Minoru Yamada <minorugh@gmail.com>
2026-06-13 07:09:05 +0900

    auto: 2026-06-13 07:09:05

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -57,7 +57,7 @@
 
 (leaf init-loader
   :ensure t
-  :load-path "~/.emacs.d/elisp"   ; local elisp packages
+  :load-path "~/.emacs.d/elisp"   ;; Path to the local packages
   :config
   (setq init-loader-show-log-after-init 'error-only)
   (setq init-loader-byte-compile t)
