441e0fd9216494403fe685c558ede79462bd8b2a
Minoru Yamada <minorugh@gmail.com>
2026-06-24 11:10:53 +0900

    auto: 2026-06-24 11:10:53

--- bin/emacs-start.sh
+++ bin/emacs-start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # /usr/local/bin/emacs-start.sh
-if pgrep -x emacs >/dev/null; then
-    wid=$(xdotool search --class emacs 2>/dev/null | tail -n1)
-    [ -n "$wid" ] && xdotool windowmap --sync "$wid" \
-        windowraise "$wid" windowfocus --sync "$wid" windowactivate --sync "$wid"
-else
-    exec zsh -lc "emacs --maximized"
-fi
+exec zsh -lc "emacs --maximized"
