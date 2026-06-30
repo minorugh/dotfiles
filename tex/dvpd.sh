b31d3f684e205253e42c3865a54caa31b3b5f442
Minoru Yamada <minorugh@gmail.com>
2026-06-30 15:51:17 +0900

    auto: 2026-06-30 15:51:17

--- tex/dvpd.sh
+++ tex/dvpd.sh
@@ -2,7 +2,15 @@
 
 # 生成されたPDFをevinceで開く
 name=$1
+dir=$(dirname "$name")
+base=${name%.*}
 
-dvipdfmx $1 && evince ${name%.*}.pdf
+dvipdfmx "$name" && evince "${base}.pdf"
+
+# 最終ページ数をlastpage.datに記録(rmで中間ファイルを消す前に確保)
+pages=$(pdfinfo "${base}.pdf" 2>/dev/null | awk '/^Pages:/{print $2}')
+if [ -n "$pages" ]; then
+    echo "$pages" > "${dir}/lastpage.dat"
+fi
 
 rm *.au* *.dv* *.lo*
