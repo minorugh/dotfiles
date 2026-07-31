05c7e8e4a01ae17a19ca6550794f8233fff9e4d1
Minoru Yamada <minorugh@gmail.com>
2026-07-31 11:40:04 +0900

    auto: 2026-07-31 11:40:04

--- .emacs.d/inits/00-base.el
+++ .emacs.d/inits/00-base.el
@@ -98,6 +98,46 @@
             "/Dropbox/backup/"
             "/scp:"
             "neomutt-"))
+(add-hook 'after-init-hook #'savehist-mode)
+
+
+;; ============================================================
+;; Recent Files
+;; ============================================================
+
+;; (run-with-idle-timer 0.5 nil #'recentf-mode)
+(setq recentf-max-saved-items 100)
+(setq recentf-auto-cleanup 'never)
+(setq recentf-save-file (locate-user-emacs-file "tmp/recentf"))
+(setq recentf-exclude
+      (list (expand-file-name "elpa/" user-emacs-directory)
+            (expand-file-name "tmp/" user-emacs-directory)
+            "\\.howm-keys"
+            "/session\\."
+            "task.org"
+            "/Dropbox/backup/"
+            "/scp:"
+            "neomutt-"))
+(add-hook 'after-init-hook #'recentf-mode)
+
+
+;; ============================================================
+;; Recent Files
+;; ============================================================
+
+;; (run-with-idle-timer 0.5 nil #'recentf-mode)
+(setq recentf-max-saved-items 100)
+(setq recentf-auto-cleanup 'never)
+(setq recentf-save-file (locate-user-emacs-file "tmp/recentf"))
+(setq recentf-exclude
+      (list (expand-file-name "elpa/" user-emacs-directory)
+            (expand-file-name "tmp/" user-emacs-directory)
+            "\\.howm-keys"
+            "/session\\."
+            "task.org"
+            "/Dropbox/backup/"
+            "/scp:"
+            "neomutt-"))
 
 
 ;; ============================================================
