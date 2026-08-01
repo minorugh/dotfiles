a6d81df81e2e36b7b65dde1f764971ebe12af88f
Minoru Yamada <minorugh@gmail.com>
2026-08-01 10:01:24 +0900

    auto: 2026-08-01 10:01:24

--- machine.mk
+++ machine.mk
@@ -1,8 +1,8 @@
 ### メイン機ホスト名の一元管理
 # author Minoru Yamada. 2026.08.01
 #
-# dotfiles配下にある各所のMakefile に include される共有定義。
-# メイン機の機種を変更する場合は MAIN_HOSTNAME の値だけを書き換えればよい。
-# env-import/Makefile はdotfiles管理外のため独立管理（変更時は両方の修正が必要）。
+# dotfiles配下にある各所の Makefileに includeされる共有定義。
+# メイン機を機種変更する場合は ここの MAIN_HOSTNAMEの値だけを書き換えるだけで各所 Makefileに反映される。
+# env-import/Makefileは dotfiles管理外のため独立管理（変更時は両方の修正が必要）。
 
 MAIN_HOSTNAME := P1
