77ea531d43b84b92f87adc4571db038eae88d950
Minoru Yamada <minorugh@gmail.com>
2026-08-08 17:35:10 +0900

    auto: 2026-08-08 17:35:10

--- bin/dropbox-watch.sh
+++ bin/dropbox-watch.sh
@@ -14,7 +14,7 @@
 # GAP_THRESHOLD は cron間隔を前提にした値。
 # cron間隔を変える場合は平常時の実行間隔の2〜3倍程度を目安に調整すること。
 #
-# Author: Minoru Yamada
+# Author: Minoru Yamada (aodamo)
 # Created: 2026-08-07
 #
 LOCKFILE="/tmp/dropbox-watch.lock"
@@ -23,7 +23,7 @@ flock -n 9 || exit 0
 
 HOME_DIR=$(eval echo "~$USER")
 HEARTBEAT_FILE="$HOME_DIR/.cache/dropbox-watch.heartbeat"
-LOGFILE="/tmp/dropbox-watch.log"
+LOGFILE="$HOME_DIR/.cache/dropbox-watch.log"
 GAP_THRESHOLD=180
 
 export DISPLAY=:0
