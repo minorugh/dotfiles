0fb5ebfb1b037fddaba99740c6ef7ea2609df66d
Minoru Yamada <minorugh@gmail.com>
2026-09-02 20:09:15 +0900

    auto: 2026-09-02 20:09:15

--- .emacs.d/inits/06-mozc.el
+++ .emacs.d/inits/06-mozc.el
@@ -19,7 +19,6 @@
           ("," . (lambda () (interactive) (mozc-insert-str "、")))
           ("." . (lambda () (interactive) (mozc-insert-str "。")))))
   :config
-  (global-set-key (kbd "C-|") nil)
   (setq default-input-method "japanese-mozc")
   (setq mozc-leim-title       "あ")
   (custom-set-faces
